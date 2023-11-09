7f226b0fe24ca6feb3108ce7a9843627ec1fe47c
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-09 21:06:23 +0900

    Title: 작은 구슬의 이동, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/작은 구슬의 이동/small-marble-movement.cpp
+++ 231109/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,2 +1,50 @@
-29 97
-10 9 R
+#include <iostream>
+#include <string>
+
+#define ASCII_NUM 128
+#define DIR_NUM 4
+
+using namespace std;
+
+int n, t;
+int x, y, dir;
+int mapper[ASCII_NUM];
+
+int dirs[DIR_NUM][4]={{0,1},{1,0},{-1,0},{0,-1}};
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+void Simulate(){
+    while(t--){
+        int nx = x+dirs[dir][0], ny = y+dirs[dir][1];
+
+        // 범위 안에 들어온다면 그대로 진행
+        if(InRange(nx,ny))
+            x = nx, y = ny;
+        // 벽에 부딪힌다면, 방향을 바꿈
+        else
+            dir = 3-dir;
+    }
+}
+
+int main(){
+    cin >> n >> t;
+    // 각 알파벳 별 방향 번호를 설정
+    // 이런 식으로 if문 안쓰고 구할 수 있음
+    mapper['R'] = 0;
+    mapper['D'] = 1;
+    mapper['U'] = 2;
+    mapper['L'] = 3;
+
+    char c_dir;
+    cin >> x >> y >> c_dir;
+    x--; y--; dir = mapper[c_dir];
+
+    Simulate();
+
+    cout<<x+1<<' ' <<y+1;
+    return 0;
+
+}
