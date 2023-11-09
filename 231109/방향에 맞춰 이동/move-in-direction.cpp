2fff01ff3f4367cd73f601fc5d5f9a5ec152c2f5
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-09 20:06:32 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/방향에 맞춰 이동/move-in-direction.cpp
+++ 231109/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
+#define DIR_NUM 4
+
 using namespace std;
-int x, y;
-int N;
-int dirs[4][2]={{1,0},{0,-1},{-1,0},{0,1}}; // E, S, W, N 
-int main() {
-    cin>>N;
-    for(int i=0; i<N; i++){
-        char c;
-        int d;
-        cin>>c>>d;
-        if(c == 'E'){
-            x += dirs[0][0]*d;
-            y += dirs[0][1]*d;
-        }
-        else if(c == 'S'){
-            x += dirs[1][0]*d;
-            y += dirs[1][1]*d;
-        }
-        else if(c == 'W'){
-            x += dirs[2][0]*d;
-            y += dirs[2][1]*d;
-        }
-        else{
-            x += dirs[3][0]*d;
-            y += dirs[3][1]*d;
-        }
+
+int n, x, y;
+// 동,서,남,북 순으로 dx, dy 정의
+int dirs[DIR_NUM][2] = {{1,0},{-1,0},{0,-1},{0,1}};
+
+int main(){
+    // 입력
+    cin >> n;
+
+    // 움직이는 것을 진행
+    while(n--){
+        char c_dir; int dist;
+        cin >> c_dir >> dist;
+
+        // 각 방향에 맞는 번호를 붙이기
+        int dir;
+        if(c_dir=='E')
+            dir = 0;
+        else if(c_dir=='W')
+            dir = 1;
+        else if(c_dir == 'S')
+            dir = 2;
+        else
+            dir = 3;
+
+        // 주어진 방향대로 dist 거리만큼 이동했을 경우의 위치를 구하기
+        x += dirs[dir][0] * dist;
+        y += dirs[dir][1] * dist;
     }
-    cout<<x << " " << y;
+    cout<<x<<' ' <<y;
     return 0;
 }
