87e149bb46b7121c157883b8bc0398732d553250
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-09 20:46:15 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231109/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#define MAX_N 50
+using namespace std;
+int maze[MAX_N][MAX_N];
+int n, t;
+int r, c, d;
+int cur_d;
+int dirs[4][2] = {{0,1},{-1,0},{1,0},{0,-1}}; // R,U,D,L
+
+bool InRange(int x, int y){
+    return (0 <= x && x < n && 0 <= y && y < n);
+}
+
+int main() {
+    cin >> n >> t;
+    cin >> r >> c >> d;
+    r--;c--;
+    if(d=='U'){
+        cur_d=1;
+    }
+    else if(d == 'R')
+        cur_d=0;
+    else if(d == 'D')
+        cur_d=2;
+    else
+        cur_d=3;
+    while(t--){
+        int nr=r+dirs[cur_d][0], nc=c+dirs[cur_d][1];
+        if(InRange(nr,nc)){
+            r = nr;
+            c = nc;
+        }
+        else cur_d = 3-cur_d;
+    }
+    cout<<r+1 << ' ' <<c+1;
+    return 0;
+}
