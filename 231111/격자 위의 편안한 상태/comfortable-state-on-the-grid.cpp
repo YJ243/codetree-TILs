9a4c8be7d78261966793c35300a511fb0f4d04d1
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-11 18:34:41 +0900

    Title: 격자 위의 편안한 상태, Time: 17ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231111/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#define MAX_N 100
+using namespace std;
+int N, M;
+int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}}; // 위, 아래, 왼쪽, 오른쪽
+int maze[MAX_N][MAX_N];
+bool InRange(int x, int y){
+    return 0 <= x && x < N && 0 <= y && y <= N;
+}
+
+bool IsComfort(int x, int y){
+    int cnt=0;
+    for(int i=0; i<4; i++){
+        int nx = x+dirs[i][0], ny = y+dirs[i][1];
+        if(InRange(nx,ny) && maze[nx][ny])  cnt++;
+    }
+    if(cnt==3)
+        return true;
+    else    return false;
+}
+int main() {
+    cin >> N >> M;
+    for(int i=0; i<M; i++){
+        int x, y;
+        cin >> x >> y;
+        x--; y--;
+        maze[x][y] = 1;
+        if(IsComfort(x,y)){
+            cout<< 1<<'\n';
+        }
+        else{
+            cout << 0 << '\n';
+        }
+    }
+    return 0;
+}
