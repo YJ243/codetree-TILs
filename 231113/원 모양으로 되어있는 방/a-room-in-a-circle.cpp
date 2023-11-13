959c61f453f7f0e9f6ef9a9f5cae0d97a5bc943d
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-13 14:50:07 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231113/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+#define MAX_N 1003
+using namespace std;
+int N;
+int room[MAX_N]; // 들어가야 하는 사람 명수
+int cur[MAX_N]; // 현재 방에 들어가 있는 사람 명수 상태
+int main() {
+    cin >> N;
+    int min_dist = INT_MAX;
+    int total=0;
+    for(int i=0; i<N; i++){
+        cin >> room[i];
+        total += room[i];
+    }
+    for(int i=1; i<N; i++){
+        // 0번째 방에서 시작하는 경우
+        room[i] = total;    // i번째 방에 전체 인원 넣기
+        int cur_dist = 0;
+        int next_idx = (i+1)%N;
+        int next = i+1;
+        while(true){
+            if(next_idx == i) 
+                break;
+            cur_dist += room[next_idx]*(next-i);
+            next_idx = (next_idx+1)%N;
+            next++;
+        }
+        
+        min_dist = min(cur_dist, min_dist);
+    }
+    cout<<min_dist;
+    return 0;
+}
