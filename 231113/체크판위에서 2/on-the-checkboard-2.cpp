23a2072e4cefc73159d8ff478f3d9cadc8a6c671
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-13 11:49:46 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231113/체크판위에서 2/on-the-checkboard-2.cpp
+++ 231113/체크판위에서 2/on-the-checkboard-2.cpp
@@ -9,6 +9,7 @@ int main(){
     for(int i=0; i<R; i++)
         for(int j=0; j<C; j++)
             cin >>arr[i][j];
+
     char cur_c = arr[0][0];
     if(cur_c == arr[R-1][C-1]){
         cout<<0 << '\n';
@@ -17,6 +18,7 @@ int main(){
     int success_cnt = 0;
     for(int i=1; i<R; i++){
         for(int j=1; j<C; j++){
+            cur_c = arr[0][0];
             if(arr[i][j] != cur_c && i == R-1 && j == C-1 && success_cnt == 0){
                 // 현재 색과 다른데, 맨 마지막에만 있다면
                 cout << 0 << '\n';
@@ -25,11 +27,14 @@ int main(){
         
             else if(arr[i][j] != cur_c){
                 // 이제 여기서 탐색해야 함
+                // (i,j)는 처음 점프한 지점
+  
                 cur_c = arr[i][j];
                 for(int k=i+1; k<R; k++){
                     for(int l=j+1; l<C; l++){
                         if(cur_c != arr[k][l]){
                             //이제 여기서 한 번 점프 했을 때 마지막까지 도달 가능하다면?
+                            //(k,l)은 두번째 점프한 지점
                             for(int x=k+1; x<R; x++){
                                 for(int y=l+1; y<C; y++){
                                     if(x == R-1 && y == C-1 && cur_c == arr[R-1][C-1]){
