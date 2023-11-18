0b8287ae37baed6dddb74cdbf7f554f52e013cba
yj____ <60027712+YJ243@users.noreply.github.com>
2023-11-18 20:11:13 +0900

    Title: G or H 3, Time: 29ms, Memory: 0MB, Status: Passed - CodeTree

--- 231118/G or H 3/G-or-H-3.cpp
+++ 231118/G or H 3/G-or-H-3.cpp
@@ -8,18 +8,22 @@ char loc[MAX_LOC];
 int main(){
     cin >> N >> K;
     int max_idx = 0;
+    int min_idx = MAX_LOC;
     for(int i=0; i<N; i++){
         int idx;
         cin >> idx;
         cin >>  loc[idx];
         max_idx = max(idx, max_idx);
+        min_idx = min(idx, min_idx);
     }
     int ans = 0;
-
-    for(int i=1; i<=max_idx-K; i++){
+    //cout << loc[min_idx] << ' ' << loc[max_idx] << '\n';
+    
+    for(int i=1; i<=MAX_LOC-K; i++){
         int cur = 0;
+    //    cout << i << ' ' ;
+        
         for(int j=i; j<=i+K; j++){
-
             if(loc[j] == 'G'){
                 cur += 1;
             }
@@ -29,6 +33,7 @@ int main(){
 
         }
         ans = max(cur, ans);
+         
     }
     cout << ans;
 }
