8ab7278e126a67d22de1fd89a114fe25398f4537
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-03 16:04:35 +0900

    Title: 두 수의 합, Time: 120ms, Memory: 6MB, Status: Passed - CodeTree

--- 231203/두 수의 합/sum-of-two-num.cpp
+++ 231203/두 수의 합/sum-of-two-num.cpp
@@ -4,20 +4,18 @@
 using namespace std;
 
 int n;      // n: 정수 개수
-double k;   // 두 수의 합
-double arr[MAX_N];
-unordered_map<double, int> m;
+int k;   // 두 수의 합
+int arr[MAX_N];
+unordered_map<long long, int> m;
 
 int main(){
     cin >> n >> k;
     int ans = 0;
     for(int i=0; i<n; i++){
         cin >> arr[i];
-        double tmp = k-arr[i];
-        if(m.find(tmp) != m.end()){
-            // 만약 현재 수와 배열에 있는 수를 더해서 k를 만들 수 있다면
-            ans += m[tmp];
-        }
+        long long diff = (long long)k-arr[i];
+        ans += m[diff];
+
         m[arr[i]]++;
     }
     cout << ans << '\n';
