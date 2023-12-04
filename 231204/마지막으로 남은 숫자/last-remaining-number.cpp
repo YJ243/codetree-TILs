25379f3622c301a8815656526ec4d93a33753ba0
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-04 16:45:29 +0900

    Title: 마지막으로 남은 숫자, Time: 1039ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 231204/마지막으로 남은 숫자/last-remaining-number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n;
+priority_queue<int> pq;
+int main() {
+    cin >> n;
+    while(n--){
+        int x;
+        cin >> x;
+        pq.push(x);
+    }
+    while(pq.size() != 1){
+        int a,b;
+        a = pq.top();
+        pq.pop();
+        b = pq.top();
+        pq.pop();
+        if(a==b) continue;
+        int diff = a-b;
+        pq.push(diff);
+    }
+    if(pq.empty()){
+        cout << -1;
+    }
+    else{
+        cout << pq.top();
+    }
+    return 0;
+}
