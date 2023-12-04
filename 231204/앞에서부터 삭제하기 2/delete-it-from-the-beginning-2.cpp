bc4a295f3ebc5bd2080a816d40cdfbca56e079c4
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-04 17:16:56 +0900

    Title: 앞에서부터 삭제하기 2, Time: 1090ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 231204/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+#define MAX_N 100000
+
+using namespace std;
+int N;
+int arr[MAX_N];
+
+priority_queue<int> pq;
+int main() {
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    int ans = 0;
+    for(int K=1; K<=N-2; K++){
+        for(int i=K; i<N; i++){
+            pq.push(arr[i]);    // 앞에서부터 K개를 삭제하고 pq에 집어넣음
+
+        }
+        int sum_val = 0;
+        while(pq.size() > 1){
+            int val = pq.top();
+            pq.pop();
+            sum_val += val;
+        }
+        int avg_val = sum_val / (N-K-1);
+
+        ans = max(ans, avg_val);
+        pq.pop();
+
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << (double)ans;
+    return 0;
+}
