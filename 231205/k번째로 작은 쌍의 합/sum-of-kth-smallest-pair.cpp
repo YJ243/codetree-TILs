032483f0c1c9a45d37e50d80892b4214acbf918b
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-05 18:49:29 +0900

    Title: k번째로 작은 쌍의 합, Time: 147ms, Memory: 4MB, Status: Passed - CodeTree

--- 231205/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
+++ 231205/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 int n, m, k;
-priority_queue<long long> pq;
+priority_queue<tuple<long long, int, int, int> > pq;
 int arr1[MAX_N];
 int arr2[MAX_N];
 long long ans;
@@ -15,39 +15,26 @@ int main() {
 
     for(int i=0; i<n; i++)
         cin >> arr1[i];
-    //sort(arr1, arr1+n);
+    sort(arr1, arr1+n);
     for(int i=0; i<m; i++)
         cin >> arr2[i]; 
-    //sort(arr2, arr2+m);
-
+    sort(arr2, arr2+m);
+    int cnt = 0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            pq.push(-arr1[i]-arr2[j]);
-        }
+        // 일단 수열 A의 최솟값과 B와의 조합을 넣가
+        pq.push(make_tuple(-arr1[i]-arr2[0], -arr1[i],-arr2[0],0));
+        //cout << arr1[0] << ' ' << arr2[i] << '\n';
     }
-
+    int x=0, y=0, idx=0;
     while(k--){
-        ans = pq.top();
+        //cout << k << ' ';
+        tie(ans, x,y,idx) = pq.top();
+        x = -x; y = -y;
         pq.pop();
+        //cout << x << ' ' << y << '\n';
+        pq.push(make_tuple(-x-arr2[idx+1],-x, -arr2[idx+1],idx+1));
     }
     cout << -ans << '\n';
-    /*
-    if(k%2 == 0){
-        int idx = (k/2)-1;
-        cout << idx << ' ';
-        ans = (long long)arr1[idx]+arr2[idx];
-    }
-    else{
-        int idx1 = (k/2)-1;
-        int idx2 = k-idx1-1;
-        if(arr1[idx1]+arr2[idx2] > arr1[idx2]+arr2[idx1]){
-            ans = arr1[idx2]+arr2[idx1];
-        }
-        else{
-            ans = arr1[idx1]+arr2[idx2];
-        }
-    }
-    cout << ans << '\n';
-    */
+
     return 0;
 }
