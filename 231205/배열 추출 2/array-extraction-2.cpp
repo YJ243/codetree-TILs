bdd4117019d713bc29d7f59a84a63cd1406b19b3
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-05 22:44:09 +0900

    Title: 배열 추출 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231205/배열 추출 2/array-extraction-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+int n;
+using namespace std;
+priority_queue<tuple<long long, long long> > pq;
+
+int main() {
+    cin >> n;
+    while(n--){
+        long long x;
+        cin >> x;
+        if(x==0){
+            if(!pq.empty()){
+                long long val_abs, val;
+                tie(val_abs, val) = pq.top();
+                pq.pop();
+                cout << -val << '\n';
+            }
+            else{
+                cout << "0" << '\n';
+            }
+        }
+        else{
+            pq.push(make_tuple(abs(x),-x));
+        }
+    }
+    return 0;
+}
