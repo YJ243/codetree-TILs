79511ebcc3da66a0d685963a1d097f3b2632b97d
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-06 18:41:27 +0900

    Title: 정원 입장은 선착순, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231206/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
+++ 231206/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
@@ -43,7 +43,9 @@ int main() {
     vector<pair<int, int>> vec(arrive_time.begin(), arrive_time.end()); // map을 vector로 변경
 
     sort(vec.begin(), vec.end(), cmp);
-
+    //for(vector<pair<int,int> >::iterator it = vec.begin(); it != vec.end(); it++){
+    //    cout << it->first << ' ';
+    //}
     // 2. hashmap 1번부터 n번까지 보는데, (도착시간 순으로)
     for (auto elem : vec)
     {
@@ -56,7 +58,7 @@ int main() {
                 // 기다리는 시간 = 0, 정원이 열리는 시간 업데이트해주기
                 waiting_time.push(0);
                 open_time += tmp_arrive+stay_time[tmp_idx];
-
+                //cout << "initial_open" << open_time << '\n';
             }
 
             else{   // 맨 처음이 아니라면
@@ -68,8 +70,11 @@ int main() {
                     tie(candidate_idx, candidate_time) = front_pq.top();
                     candidate_idx *= -1;
                     front_pq.pop();
-                    waiting_time.push(open_time-candidate_time);
+                    open_time = max(open_time, candidate_time);
+                    int wait = max(0,open_time-candidate_time);
+                    waiting_time.push(wait);
                     open_time += stay_time[candidate_idx];
+                    //cout << candidate_idx << ' ' << open_time <<' ' <<candidate_time << ' ' <<wait<<'\n';
                 }
                 // 그 이후에 대기큐에 넣어주기
             }
@@ -79,6 +84,7 @@ int main() {
         else{
         // 번호표의 숫자가 작은 사람부터 들어갈 수 있으니, 번호표의 숫자가 작은 애부터 나오도록 minheap으로 구현
             front_pq.push(make_pair(-tmp_idx, tmp_arrive));        // 정원 앞의 pq
+            //cout << tmp_idx << ' ' << open_time << '\n';
         }
     }
     // 끝나고 남아있다면
