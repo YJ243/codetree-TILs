5f8ca3b17e6932c45faa9186d50cc60d250eb5b4
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-06 18:22:35 +0900

    Title: 정원 입장은 선착순, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231206/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
+++ 231206/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
@@ -20,6 +20,9 @@ unordered_map<int,int> arrive_time; // key: 번호, value = 도착시간
 
 bool cmp(pair<int, int>& a, pair<int, int>& b)
 {
+    if(a.second == b.second){
+        return a.first < b.first;
+    }
     return a.second < b.second;
 }
 
@@ -31,28 +34,34 @@ int main() {
         int arrive = 0;
         cin >> arrive;
         arrive_time[i] = arrive;
+
         // 머무는 시간 저장
         cin >> stay_time[i];
     }
 
     // 1. 도착 시간 정렬 (hahsmap의 value 오름차순으로 정렬)
     vector<pair<int, int>> vec(arrive_time.begin(), arrive_time.end()); // map을 vector로 변경
+
     sort(vec.begin(), vec.end(), cmp);
-    
-    // 2. hashmap 1번부터 n번까지 보는데, 
+
+    // 2. hashmap 1번부터 n번까지 보는데, (도착시간 순으로)
     for (auto elem : vec)
     {
         int tmp_idx=elem.first, tmp_arrive=elem.second;
-        
+
         //  2-1. 도착 시간이 정원이 열리는 현재 시각보다 이후라면
         if(open_time<tmp_arrive){
             if(waiting_time.empty()){    // 맨 처음에 바로 들어갈 수 있는 경우 바로 정원 들어가기
-            // 기다리는 시간 = 0, 정원이 열리는 시간 업데이트해주기
-            waiting_time.push(0);
-            open_time += tmp_arrive+stay_time[tmp_idx];
+
+                // 기다리는 시간 = 0, 정원이 열리는 시간 업데이트해주기
+                waiting_time.push(0);
+                open_time += tmp_arrive+stay_time[tmp_idx];
+
             }
 
             else{   // 맨 처음이 아니라면
+                front_pq.push(make_pair(-tmp_idx, tmp_arrive));
+
                 // front_pq에 들어가 있는 애들 다 빼고 처리한 다음
                 while(!front_pq.empty()){
                     int candidate_idx, candidate_time;
@@ -63,7 +72,6 @@ int main() {
                     open_time += stay_time[candidate_idx];
                 }
                 // 그 이후에 대기큐에 넣어주기
-                front_pq.push(make_pair(-tmp_idx, tmp_arrive));
             }
         }
         
@@ -72,6 +80,15 @@ int main() {
         // 번호표의 숫자가 작은 사람부터 들어갈 수 있으니, 번호표의 숫자가 작은 애부터 나오도록 minheap으로 구현
             front_pq.push(make_pair(-tmp_idx, tmp_arrive));        // 정원 앞의 pq
         }
+    }
+    // 끝나고 남아있다면
+    while(!front_pq.empty()){
+        int candidate_idx, candidate_time;
+        tie(candidate_idx, candidate_time) = front_pq.top();
+        candidate_idx *= -1;
+        front_pq.pop();
+        waiting_time.push(open_time-candidate_time);
+        open_time += stay_time[candidate_idx];
     }
     cout << waiting_time.top();
     return 0;
