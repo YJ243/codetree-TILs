701d5db21324837abbc364f99d3045981dab62bf
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-07 15:10:02 +0900

    Title: 최솟값 3개, Time: 178ms, Memory: 1MB, Status: Passed - CodeTree

--- 231207/최솟값 3개/top-3-smallest-number.cpp
+++ 231207/최솟값 3개/top-3-smallest-number.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 #include <queue>
+#include <cstdlib>
 #define MAX_N 100000
 using namespace std;
 
-priority_queue<int> pq;
-int N;
-int arr[3];
-int a,b,c;
-
+priority_queue<int> pq;     // 가장 작은 숫자 3개를 반환하기 위한 우선순위 큐(minHeap)
+int N;      // N 개의 숫자
+int arr[3]; // 현재까지 가장 작은 숫자 3개
+int input_arr[MAX_N];
 int main() {
+    // 입력:
     cin >> N;
+    //N = 10000;
     for(int i=0; i<N; i++){
         int val;
         cin >> val;
+        //val = rand()%10000+1;
+        //cout << val << ' ';
         pq.push(-val);
-        if(i < 2) 
+        if(i < 2)       // 주어진 숫자의 수가 채 3개가 되지 않는다면 
+            //continue;
             cout << -1 << '\n';
-        else{
-            for(int j=0; j<3; j++){
-                arr[j] = pq.top();
+
+        else{           // 주어진 숫자의 수가 3개 이상이라면
+            for(int j=0; j<3; j++){ // 3개의 숫자를 뽑기
+                arr[j] = pq.top();  
                 pq.pop();
                 arr[j] *= -1;
             }
-            int ans = 1;
+            long long ans = 1;
             for(int j=0; j<3; j++){
-                ans *= arr[j];
-                pq.push(-arr[j]);
+                ans *= arr[j];      // 3개의 숫자의 곱을 구하기
+                pq.push(-arr[j]);   // 다시 큐에 집어넣기
             }
-            cout << ans << '\n';            
+            cout << ans << '\n';    // 해당 곱 출력
         }
     }
     return 0;
