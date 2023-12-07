a89260a8de0f49b31c3fd8066330513e941edcfa
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-07 20:30:30 +0900

    Title: 자리 바꾸기 2, Time: 450ms, Memory: 35MB, Status: Passed - CodeTree

--- 231207/자리 바꾸기 2/changing-seats-2.cpp
+++ 231207/자리 바꾸기 2/changing-seats-2.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <unordered_set>
 #include <tuple>
+using namespace std;
+
 #define MAX_N 100000
 
-using namespace std;
-int arr[MAX_N+1];
-
-unordered_set<int> seats[MAX_N];
+int arr[MAX_N+1];                   // arr[i]: i번 자리에 앉아있는 사람의 번호
+unordered_set<int> seats[MAX_N];    // seats[i]: i가 앉게되는 자리들의 번호
+tuple<int,int> change[MAX_N];       // 입력으로 주어지는 바꿔야할 자리
 int n,k;
-tuple<int,int> change[MAX_N];
+
 int main() {
     cin >> n >> k;
-
     // 처음 자리 삽입
     for(int i=1; i<=n; i++){
         seats[i].insert(i);     // seats[i]: i가 앉게되는 자리
@@ -20,26 +20,28 @@ int main() {
     for(int i=0; i<k; i++){
         int a,b;
         cin >> a >> b;
-        change[i] = make_tuple(a,b);
+        change[i] = make_tuple(a,b);    // 바꿔야할 자리 입력 받기
     }
+
     for(int i=0; i<3; i++){
         for(int j=0; j<k; j++){
-
             int a,b;
             tie(a,b) = change[j];
+            
             int seat_a, seat_b;
             seat_a = arr[a];    // a자리에 앉아 있는 사람의 번호=seat_a
             seat_b = arr[b];    // b자리에 앉아 있는 사람의 번호=seat_b
 
             seats[seat_a].insert(b); // a자리에 앉은 사람이 옮긴 자리는 b번 인덱스
             seats[seat_b].insert(a); // b자리에 앉은 사람이 옮긴 자리는 a번 인덱스 
-            arr[a] = seat_b;
-            arr[b] = seat_a;
+            
+            arr[a] = seat_b;    // a자리에 b자리에 앉아 있던 사람 옮기기
+            arr[b] = seat_a;    // b자리에 a자리에 앉아 있던 사람 옮기기
         }
     }
 
-    for(int i=1; i<=n; i++){
+    // 각 사람마다 앉은 자리 개수 출력
+    for(int i=1; i<=n; i++)
         cout << seats[i].size() << '\n';
-    }
     return 0;
 }
