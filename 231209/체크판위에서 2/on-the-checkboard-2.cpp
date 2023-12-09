93fbcc2e40697d8511646aab525dcd227ded2b4b
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-09 19:04:06 +0900

    Title: 체크판위에서 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231209/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAX_D 15
+
+using namespace std;
+int r, c;
+char arr[MAX_D][MAX_D];
+int ans;
+
+int main(){
+    cin >> r >> c;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cin >> arr[i][j];
+        }
+    }
+    char cur = arr[0][0];
+
+    for(int i=1; i<r-1; i++){
+        for(int j=1; j<c-1; j++){
+            if(cur != arr[i][j]){
+                // 처음으로 바뀌는 위치라면
+                for(int x=i+1; x<r-1; x++){
+                    for(int y=j+1; y<c-1; y++){
+                        if(cur == arr[x][y]){
+                            ans++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ans;
+}
