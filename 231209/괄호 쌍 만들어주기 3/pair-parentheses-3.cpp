fcc33c8144b10ebf8d13c1e15fb633734b0eb443
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-09 18:43:40 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231209/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+string s;
+int ans;
+int main(){
+    cin >> s;
+    for(int i=0; i<s.size(); i++){
+        for(int j=i+1; j<s.size(); j++){
+            if(s[i] == '(' && s[j] == ')'){
+                ans++;
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
