0af4bb02747fb2996e80648c4d8eeba79bf90008
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-11 22:48:19 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 231211/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#define MAX_N 101
+#define MAX_N 100
 using namespace std;
 
 int n, k;
-int arr[MAX_N];
+int arr[MAX_N+1];
+
 int ans;
+
 int main() {
     cin >> n >> k;
 
@@ -20,7 +22,7 @@ int main() {
         // 중심점은 i, 구간 안에서 사탕 개수 구하기 (i-k ~ i+k, 2k+1)
         int cur_cnt=0; 
         for(int j=i-k; j<=i+k; j++){
-            if(0 <= j && j < MAX_N){        // j가 범위 안에 있을 경우에만 더해주기
+            if(0 <= j && j <= MAX_N){        // j가 범위 안에 있을 경우에만 더해주기
                 cur_cnt += arr[j];
             }
         }
