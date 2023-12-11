ea61fa2e0258e15186b0a044b901cfe91d2386bc
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-11 22:46:29 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 231211/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define MAX_N 100
+#define MAX_N 101
 using namespace std;
 
 int n, k;
@@ -7,20 +7,27 @@ int arr[MAX_N];
 int ans;
 int main() {
     cin >> n >> k;
+
     for(int i=0; i<n; i++){
-        int idx, cnt;
+        int cnt, idx;       // cnt: 사탕 개수, idx: 바구니 좌표
         cin >> cnt >> idx;
-        arr[idx] += cnt;
-        
+        arr[idx] += cnt;    // 해당 바구니 좌표에 사탕 넣기
     }
 
-    for(int i=k; i<MAX_N-k; i++){
-        int cur_cnt=0;
+
+    for(int i=0; i<=MAX_N; i++){
+        //cout << i-k << ' '<< i << ' ' << i+k << '\n';
+        // 중심점은 i, 구간 안에서 사탕 개수 구하기 (i-k ~ i+k, 2k+1)
+        int cur_cnt=0; 
         for(int j=i-k; j<=i+k; j++){
-            cur_cnt += arr[j];
+            if(0 <= j && j < MAX_N){        // j가 범위 안에 있을 경우에만 더해주기
+                cur_cnt += arr[j];
+            }
         }
         ans = max(cur_cnt, ans);
     }
+    
     cout << ans << '\n';
+
     return 0;
 }
