224b9d2a532aa4d38c0520568b2aff766cf9bca6
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-11 16:56:38 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 231211/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -10,11 +10,14 @@ int main(){
     for(int i=0; i<n; i++)
         cin >> arr[i];
     
+    // 모든 구간의 시작점을 잡아보기
     for(int i=0; i<=n-k; i++){
+        // 해당 구간 내 원소의 합을 구하기
         int cur_sum = 0;
         for(int j=i; j<i+k; j++){
             cur_sum += arr[j];
         }
+        // 최댓값 구하기
         ans = max(cur_sum, ans);
     }
     cout << ans;
