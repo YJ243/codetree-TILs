162f09e0b21cd5cc5217757ed731aa6093b8eeb2
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-13 13:19:45 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231213/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 231213/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -14,9 +14,10 @@ int main() {
     for(int i=0; i<n; i++){
         for(int j=i+t-1; j<n; j++){
             // (i ~ j) 까지 탐색하기, 구간은 최소 t이상
-
+            // 모든 구간의 시작점을 잡아보기
             int cur_cost = 0;
             for(int k=i; k<=j; k++){
+                // 해당 구간을 고르게 할 때의 비용 구하
                 if(arr[k] < h || arr[k] > h) {
                     cur_cost += abs(arr[k]-h);
                 }
