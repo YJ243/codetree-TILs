1bb2d5abeca9799dd999f5fb9ee409d1eeffbbe5
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-14 17:23:34 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231214/삼각형 만들기/create-triangle.cpp
+++ 231214/삼각형 만들기/create-triangle.cpp
@@ -5,26 +5,29 @@
 
 using namespace std;
 int n;
-int c[MAX_N][2];
+int x[MAX_N];
+int y[MAX_N];
 int ans = INT_MIN;
+
 int main(){
     // 입력
     cin >> n;
     for(int i=0; i<n; i++)
-        cin >> c[i][0] >> c[i][1];
+        cin >> x[i] >> y[i];
 
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
-            bool isX = false, isY = false;
             for(int k=j+1; k<n; k++){
+                bool isX = false, isY = false;
                 // 점은 i, j, k번째 점
                 // x축에 평행(y가 같음)한지 확인
-                if(c[i][1] == c[j][1] || c[j][1] == c[k][1] || c[k][1] == c[i][1])  isX = true;
+                if(y[i] == y[j] || y[j] == y[k] || y[k] == y[i])  isX = true;
                 // y축에 평행한지(x가 같은게 있는) 확인
-                if(c[i][0] == c[j][0] || c[j][0] == c[k][0] || c[k][0] == c[i][0])  isY = true;
+                if(x[i] == x[j] || x[j] == x[k] || x[k] == x[i])  isY = true;
 
-                if(isX && isY){
-                    int cur_area = abs((c[i][0]*c[j][1] + c[j][0]*c[k][1] + c[k][0]*c[j][1]) - (c[j][0]*c[i][1] + c[k][0]*c[j][1] + c[i][0]*c[k][1]));
+                if(isX && isY){ // 만약 둘다 평행한 면을 가지고 있을 경우 계산하기
+                    // (x1y2 + x2y3 + x3y1) - (x2y1 + x3y2 + x1y3) 
+                    int cur_area = abs((x[i]*y[j] + x[j]*y[k] + x[k]*y[i]) - (x[j]*y[i] + x[k]*y[j] + x[i]*y[k]));
                     ans = max(cur_area, ans);
                 }
             }
