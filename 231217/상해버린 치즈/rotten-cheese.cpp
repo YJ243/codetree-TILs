ee237df7dc781ab756268bd6fbd46b14e13e1999
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-17 19:53:34 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231217/상해버린 치즈/rotten-cheese.cpp
+++ 231217/상해버린 치즈/rotten-cheese.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #define MAX_N 50
 using namespace std;
-int n, m, d, s; // n명의 사람이 m개의 치즈를 먹음, 언제 누가 치즈를 먹었는지 d번, 특정 사람이 언제 아팠는지 s번
+int n, m, d, s; 
 vector<pair<int, int> > people[MAX_N+1];     // people[i]: i번 번호의 사람이 <m, t> m번 치즈를 t초에 먹음
-int sick_time[MAX_N+1];
+int sick_time[MAX_N+1];                      // sick_time[i]: i번 사람이 언제 확실히 아팠는지 기록
 int ans;
+
 int main() {
+    // 입
     cin >> n >> m >> d >> s;
     for(int i=0; i<d; i++){
         // (p,c,t): p번째 사람이 c번째 치즈를 t초에 먹음
@@ -24,44 +26,40 @@ int main() {
 
     // 모든 치즈를 하나씩 상했다고 가정하기
     for(int i=1; i<=m; i++){
-        // 해당 번호의 사람이 언제 아팠는지 배열 선언하기
+        // 해당 번호의 사람이 현재 턴에서(i번 치즈가 상함) 언제 아팠는지
         int tmp_sick[n+1] = {};
         for(int j=1; j<=n; j++){
             // j번 사람이 i번 치즈를 먹었다면 해당 초 이후부터 아프기 시작했다 표시하기
             for(int k=0; k<(int) people[j].size(); k++){
                 if(people[j][k].first == i)
                     tmp_sick[j] = people[j][k].second+1;    // +1초 부터 아프기 시작함
-                    //cout << j << "번 사람이 " << tmp_sick[j] << "초 부터 아프기 시작함" << '\n';
             }
         }
         
-        int sick_cnt = 0;
+        int sick_cnt = 0;   // 현재 총 아픈 사람
         bool passed = true; // 아프기 전에 치즈를 먹었는지, sick_time에 들어가 있는 사람이 먹었는지
-        // 그렇다면 이제 sick_time을 보면서 확인하기
         
         for(int j=1; j<=n; j++){
-            // 만약 sick_time이 0보다 크다면 아프기 시작한 시점, 그 시점 이전에 치즈를 먹었는지
+            // 만약 sick_time이 0보다 크다면 아프기 시작한 시점
             if(tmp_sick[j] > 0){
-                // 아파지기 전에 먹었거나 아프다고 표시를 안했을 경우 
+                // 아프기 시작했다고 기록한 시점(sick_time[j]) 전에 해당 치즈를 먹었거나 아프다고 기록을 안했을 경우 
                 if(sick_time[j] > tmp_sick[j]-1 || sick_time[j] == 0)
                     sick_cnt++; // 약 먹어야 하는 사람 수 증가
                 else if(sick_time[j] <= tmp_sick[j]-1){ // 아픈 이후에 해댱 상한 치즈를 먹은 경우
-                    passed = false;
+                    passed = false;                     // 이 경우는 있을 수 없
                     break;
                 }
-            
             }
+            // 배가 아프다고 입력으로 기록(sick_time[j] > 0)했는데 현재의 상한 치즈를 먹지 않은(tmp_sick[j] == 0) 경우
             else if(tmp_sick[j] == 0 && sick_time[j] > 0){
-                // 배가 아프다고 입력이 들어왔는데 현재 상한 치즈를 먹지 않은 경우
                 passed = false; // 해당 경우가 발생할 수 없으니
                 break;
             }
         }
-        if(!passed) continue;
-        ans = max(ans, sick_cnt);
-
+        if(!passed) continue;       // 만약 현재 경우가 발생할 수 없는 경우 패스
+        ans = max(ans, sick_cnt);   // 약을 먹어야 하는 최대 사람수 업데이트
     }
+    // 출력
     cout << ans << '\n';
-
     return 0;
 }
