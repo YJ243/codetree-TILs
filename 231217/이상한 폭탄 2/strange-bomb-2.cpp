2c690595a801898a9635f669be075d39827d2feb
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-17 21:21:30 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231217/이상한 폭탄 2/strange-bomb-2.cpp
+++ 231217/이상한 폭탄 2/strange-bomb-2.cpp
@@ -6,10 +6,13 @@ int n, k;   // 폭탄 개수 n, 특정 거리 k
 int ans = -1;
 int bomb[MAX_N];    // 이상한 폭탄 번호들
 int main() {
+    // 입력
     cin >> n >> k;
     for(int i=0; i<n; i++)
         cin >> bomb[i];
     
+    // 모든 쌍에 대해서 터질 수 있는 폭탄을 찾고
+    // 그 중 번호가 최대인 값을 찾ㄱ
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
             if((bomb[i] == bomb[j]) && (j-i <= k)){
