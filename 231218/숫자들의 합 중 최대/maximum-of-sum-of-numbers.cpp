3723d02c01647e3704a00b3b58b17685dc90ea88
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 14:56:23 +0900

    Title: 숫자들의 합 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ 231218/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -3,15 +3,25 @@ using namespace std;
 
 int x,y;
 int ans = 1;
+
+// 각 자리 숫자의 합을 구하여 반환
+int DigitSum(int n){
+    // 한 자리 숫자라면 그 숫자 그대로 반환
+    if(n < 10)
+        return n;
+
+    // 두자리 이상의 숫자라면,
+    // 맨 끝자리를 제외한 숫자들의 합을 재귀적으로 호출한 뒤,
+    // 마지막 자리수를 계속 더해 반환
+    return DigitSum(n/10) + (n%10);
+}
 int main() {
+    // 입력
     cin >> x >> y;
+    
+    // 각 숫자에 대해 각 자리 숫자의 합을 구해 그 중 최댓값을 계산하기
     for(int i=x; i<=y; i++){
-        int d1 = i/10000;
-        int d2 = i/1000;
-        int d3 = (i/100)%10;
-        int d4 = (i/10)%10;
-        int d5 = i%10;
-        ans = max(ans, d1+d2+d3+d4+d5);
+        ans = max(ans, DigitSum(i));
     }
     cout << ans << '\n';
     return 0;
