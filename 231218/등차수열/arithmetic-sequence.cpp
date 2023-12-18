2a5afc66ecafa29f138a55b4b84330a8c3574cef
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 21:40:23 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231218/등차수열/arithmetic-sequence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int arr[MAX_N];
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    
+    int ans = 0;
+    for(int k=1; k<=MAX_N; k++){
+        // 자연수 k를 적절하게 잡아보기
+        int cur = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if((arr[i]+arr[j]) == 2*k)    cur++;
+            }
+        }
+        ans = max(ans, cur);
+    }
+    cout << ans;
+    return 0;
+}
