faa1b0ccd72ef66240fecabd8647f7fb4b227e73
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 21:32:48 +0900

    Title: 정보에 따른 숫자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/정보에 따른 숫자 2/number-based-on-information-2.cpp
+++ 231218/정보에 따른 숫자 2/number-based-on-information-2.cpp
@@ -21,25 +21,22 @@ int main() {
             S.push_back(loc);
         else
             N.push_back(loc);
+        
+        // 만약 배열로 선언했다면
+        // if(x == 'S')      s[cnt_s++] = y; // 이렇게 하면 편 
     }
 
-    // 1부터 1000까지 탐색하면서 (k-d1) <= (k-d2): k는 특별한 위치
+    // 각 숫자에 대해 s에 더 가까운지 n에 더 가까운지 판단
+    // (k-d1) <= (k-d2): k는 특별한 위치
     for(int i=a; i<=b; i++){
         // 제일 가까운 S까지의 거리 찾기
         int d1 = 1000, d2 = 1000;
-        int s_index, n_index;
         for(int j=0; j<(int)S.size(); j++){
-            if(d1 > abs(S[j]-i)){
-                d1 = min(d1, abs(S[j]-i));
-                s_index = S[j];
-            }
+            d1 = min(d1, abs(S[j]-i));
         }
         // 제일 가까운 N까지의 거리 찾기
         for(int j=0; j<(int)N.size(); j++){
-            if(d1 > abs(N[j]-i)){
-                d2 = min(d2,abs(N[j]-i));
-                n_index = N[j];
-            }
+            d2 = min(d2,abs(N[j]-i));
         }
         
         if(d1 <= d2)    ans++;
