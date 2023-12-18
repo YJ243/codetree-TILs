e180407c84208b4839b8f20d74c0994102262930
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 14:36:27 +0900

    Title: 스승의 은혜 3, Time: 42ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 231218/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -3,49 +3,45 @@
 #define MAX_N 1000
 
 using namespace std;
-int n, b;       // n명의 학생에게 b만큼의 예산으로 선물을 줌
+int n, b;                       // n명의 학생에게 b만큼의 예산으로 선물 주기
+pair<int, int> students[MAX_N]; // <학생이 원하는 선물 가격, 배송비>
+int ans;                        // 선생님이 선물 가능한 학생의 최대 명수
 
-pair<int, int> students[MAX_N]; // <학생이 원하는 선물의 가격, 배송비>
-int ans;
-
-int main() {
+int main(){
     // 입력
     cin >> n >> b;
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
         cin >> students[i].first >> students[i].second;
-    }
-
+    
     for(int i=0; i<n; i++){
-        // Step 1. 먼저 pair를 복사하기
+        // Step1. 먼저 pair를 복사하기
         pair<int, int> tmp[n];
-        for(int i=0; i<n; i++){
-            tmp[i].first = students[i].first;
-            tmp[i].second = students[i].second;
+        for(int j=0; j<n; j++){
+            tmp[j].first = students[j].first;
+            tmp[j].second = students[j].second;
         }
 
         // Step2. i번 학생의 선물을 반값으로 할인하기
         tmp[i].first /= 2;
         
-        // Step3. 가격을 기준으로 오름차순 정렬, 그 안에서 배송비를 기준으로 오름차순 정하기
+        // Step3. 가격+배송비 합을 기준으로 오름차순 정렬, 만약 같다면 가격 기준으로 오름차순 정렬
         sort(tmp, tmp+n, [](const pair<int, int> &a, const pair<int, int> &b){
             if(a.first + a.second == b.first + b.second)
                 return a.first < b.first;
             return a.first + a.second < b.first + b.second;
         });
+
+        // Step4. 예산 범위 내에서 최대 명수 구하기
         
-        // Step4. 예산범위 내에서 최대 명수 구하기
-        int cost = 0, idx = 0;
-        while(cost <= b && idx < n){
-            cost += tmp[idx].first + tmp[idx].second;
-            idx++;
+        int cost = 0, cnt = 0;
+        for(int j=0; j<n; j++){
+            if(cost + tmp[j].first + tmp[j].second > b)
+                break;
+            cost += tmp[j].first + tmp[j].second;
+            cnt++;
         }
-        idx--;
-        /*
-        if(cost <= b && idx == n-1){
-            idx++;
-        }
-        */
-        ans = max(ans, idx);
+
+        ans = max(ans, cnt);
     }
     cout << ans;
     return 0;
