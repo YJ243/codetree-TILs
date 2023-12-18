9cf35951e99145a358a4d48b1fe9c8dc2da9dfd2
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 17:17:17 +0900

    Title: 흥미로운 숫자 2, Time: 22ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 231218/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 int x, y;
 int ans;
 
-vector<int> tmp;
-void DigitOfNumber(int num, vector<int>& arr){
-    if(num < 10){
-        arr.push_back(num);
-        return;
-    }
-    arr.push_back(num%10);
-    DigitOfNumber(num/10, arr);
-
-}
-
 bool IsInteresting(int number){
-    tmp.clear();
-    DigitOfNumber(number, tmp);
-    int cnt = 0, cur = tmp[0];
+    int cur = number;
     int number_cnt[10] = {};
-    for(int j=0; j<(int) tmp.size(); j++){
-        // 각 숫자별 나온 자릿수 증가시키기
-        number_cnt[tmp[j]]++;
+    while(cur){
+        number_cnt[cur%10]++;
+        cur /= 10;
     }
     // 1개만 나오는 숫자 수, 1개 초과해서 나오는 수
     int one_cnt = 0, second_cnt = 0;
