1846439c0b72f3687e78582d2aa4fb8448d90d8c
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 14:53:16 +0900

    Title: 각 자리 숫자의 제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/각 자리 숫자의 제곱/square-of-each-digit.cpp
+++ 231218/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -1,15 +1,25 @@
+/*
+ * 2023.12.18
+ * 마지막 자리수를 제외하고 남은 숫자들을 함수에 넘기는 재귀함수 만들어 해결
+*/
 #include <iostream>
 
 using namespace std;
 int n;
 
 int f(int n){
+    // 한 자리 숫자라면 제곱한 값이 결과가 됨
     if(n < 10)
         return n*n;
+
+    // 마지막 자리 숫자의 제곱에
+    // 남은 숫자들의 합을 계산한 결과를 더해서 반
     return f(n/10) + (n%10)*(n%10);
 }
 int main() {
+    // 변수 입력:
     cin >> n;
+    
     cout << f(n);
     return 0;
 }
