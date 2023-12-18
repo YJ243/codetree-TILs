301ac45b0c204fb63f62967e4965f708f8705989
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-18 13:19:48 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231218/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 231218/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -27,13 +27,20 @@ int main() {
         tmp[i].first /= 2;
         
         // Step3. 가격을 기준으로 오름차순 정렬, 그 안에서 배송비를 기준으로 오름차순 정하기
-        sort(students, students+n, [](const pair<int, int> &a, const pair<int, int> &b){
+        sort(tmp, tmp+n, [](const pair<int, int> &a, const pair<int, int> &b){
             if(a.first == b.first)
                 return a.second < b.second;
             return a.first < b.first;
         });
+        /*
+        cout << i << "번째 가격을 반값으로" << '\n';
+        for(int k=0; k<n; k++){
+            cout << students[k].first << ' ' << students[k].second << '\n';
+        }
 
-        // Step5. 예산범위 내에서 최대 명수 구하기
+        cout << "**************"<<'\n';
+        */
+        // Step4. 예산범위 내에서 최대 명수 구하기
         int cost = 0, idx = 0;
         while(cost <= b && idx < n){
             cost += tmp[idx].first + tmp[idx].second;
@@ -44,7 +51,7 @@ int main() {
             idx++;
         }
         ans = max(ans, idx);
-
+/*
         // Step6. 배송비를 기준으로 오름차순 정렬, 그 안에서 가격을 기준으로 정렬
         sort(students, students+n, [](const pair<int, int> &a, const pair<int, int> &b){
             if(a.second == b.second)
@@ -63,7 +70,7 @@ int main() {
             idx++;
         }
         ans = max(ans, idx);
-
+*/
     }
     cout << ans;
     return 0;
