c593e4b6d3b008a8c146e493ca61ad0532d22529
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-21 14:24:01 +0900

    Title: 3개의 선 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231221/3개의 선 2/three-lines-2.cpp
+++ 231221/3개의 선 2/three-lines-2.cpp
@@ -12,15 +12,13 @@ int main(){
     for(int i=0; i<n; i++)
         cin >> x[i] >> y[i];
 
-    // 모든 직선에 대해 전부 시도해 보기
     for(int i=0; i<=MAX_XY; i++){
         for(int j=0; j<=MAX_XY; j++){
             for(int k=0; k<=MAX_XY; k++){
                 // i,j,k가 각각 x축 혹은 y축에 평행한 직선의 좌표가 됨
                 // (x,y) == (0,3)
-                bool isPassed = true;   // 직선 3개로 모든 점을 지나게 할 수 있으면 true
+                bool isPassed = true;
                 for(int a=0; a<n; a++){
-                    // 해당 점이 직선에 닿으면 넘어가기
                     if(y[a] == i || y[a] == j || y[a] == k)
                         continue;
                     isPassed = false;
