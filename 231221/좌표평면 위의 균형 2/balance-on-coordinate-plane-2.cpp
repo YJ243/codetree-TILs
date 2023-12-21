ea45ab20c175b60c0b30e919ec09de3e4c338412
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-21 14:51:50 +0900

    Title: 좌표평면 위의 균형 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231221/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.cpp
+++ 231221/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.cpp
@@ -14,11 +14,14 @@ int main() {
     for(int i=0; i<n; i++){
         cin >> x[i] >> y[i];
     }
-    for(int i=0; i<=100; i++){
-        for(int j=0; j<=100; j++){
-            if((i % 2 != 0) || (j % 2 != 0)) continue;
+    // 모든 직선에 대해 전부 시도해보
+    for(int i=0; i<=100; i += 2){
+        for(int j=0; j<=100; j += 2){
+            //if((i % 2 != 0) || (j % 2 != 0)) continue;
             // x=i, y=j의 평행한 직선들
+            // dots: x=i, y=j를 기준으로 1~4분면의 점의 개수
             int dots[4] = {};
+            // k번째 점이 몇사분면인지 확인하고 해당 위치의 segment를 1 증가시
             for(int k=0; k<n; k++){
                 // 1사분면
                 if(x[k] > i && y[k] > j)
@@ -32,7 +35,9 @@ int main() {
                 // 4사분면
                 else    dots[3]++;
             }
+            // x=i, y=j로 나눴을 때의 m을 구하기
             int max_dots = 0;
+            // 1,2,3,4,를 두개씩 나눠서 max함수를 이용해도 됨
             for(int k=0; k<4; k++){
                 max_dots = max(max_dots, dots[k]);
             }
