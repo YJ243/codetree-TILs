661e25b86b7e3ac8020970e790522eed1d7af529
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-21 14:45:02 +0900

    Title: 좌표평면 위의 균형 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231221/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100
+using namespace std;
+int n;
+int x[MAX_N];
+int y[MAX_N];
+int ans;
+
+int main() {
+    // 입력
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> x[i] >> y[i];
+    }
+    for(int i=1; i<=100; i++){
+        for(int j=1; j<=100; j++){
+            // x=i, y=j의 평행한 직선들
+            int dots[4] = {};
+            for(int k=0; k<n; k++){
+                // 1사분면
+                if(x[k] > i && y[k] > j)
+                    dots[0]++;
+                // 2사분면
+                else if(x[k] < i && y[k] > j)
+                    dots[1]++;
+                // 3사분면
+                else if(x[k] < i && y[k]< j)
+                    dots[2]++;
+                // 4사분면
+                else    dots[3]++;
+            }
+            int max_dots = 0;
+            for(int k=0; k<4; k++){
+                max_dots = max(max_dots, dots[k]);
+            }
+            if(ans == 0)
+                ans = max_dots;
+            else    ans = min(ans, max_dots);
+        }
+    }
+    cout << ans;
+    return 0;
+}
