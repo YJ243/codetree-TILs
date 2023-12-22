604ea7d7f031952d2f175971cf1b58d5bbed08e6
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-22 18:31:10 +0900

    Title: 원소 값들의 최대 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231222/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
+++ 231222/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -10,12 +10,16 @@ int arr[MAX_N+1];
 int ans;
 
 int main() {
+    // 입력
     cin >> n >> m;
     for(int i=1; i<=n; i++)
         cin >> arr[i];
     
     // 시작위치를 0부터 끝까지 확인해보는데 합이 최대가 된다면?
+    // 어느 지점에서 시작할 지 전부 시도해보기
+    // 모든 경우의 수에 대해 최대가 되도록 하는 수를 계산
     for(int i=1; i<=n; i++){
+        // 시작점은 i
         int curCnt = m-1, curSum = arr[i], cur_idx = i;
         while(curCnt--){
             curSum += arr[arr[cur_idx]];
