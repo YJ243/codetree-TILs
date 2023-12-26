d296a3c6c61f431e5c753bb9b830582b289046a8
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-26 20:04:53 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+#define MAX_N 100
+using namespace std;
+
+
+int n, k;
+int arr[MAX_N];
+bool bombed[MAX_N];
+unordered_map<int, int> cnt;
+int main() {
+    cin >> n >> k;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<=min(i+3, n-1); j++){
+            if(arr[i] == arr[j])
+                bombed[i] = true;
+                bombed[j] = true;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(bombed[i])
+            cnt[arr[i]]++;
+    }
+    int max_cnt = 0;
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        if(bombed[i]){
+            if(max_cnt < cnt[arr[i]]){
+                max_cnt = cnt[arr[i]];
+                ans = arr[i];
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
