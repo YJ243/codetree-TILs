869f07a57e6efaf302c6923a5c3609ef1fd0d17a
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-26 19:06:12 +0900

    Title: A, B, C, D 찾기 2, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 231226/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
+++ 231226/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
@@ -3,37 +3,29 @@
 using namespace std;
 int arr[15];
 int main() {
+    // 입력
     for(int i=0; i<15; i++)
         cin >> arr[i];
     
+    // 모든 a,b,c,d를 확인해서 이 합들이 arr과 같은지 여부를 확인하기
     for(int a=1; a<=40; a++){
         for(int b=a; b<=40; b++){
             for(int c=b; c<=40; c++){
                 for(int d=c; d <= 40; d++){
-                    int tmp[15] = {};
-                    tmp[0] = a;
-                    tmp[1] = b;
-                    tmp[2] = c;
-                    tmp[3] = d;
-                    tmp[4] = a+b;
-                    tmp[5] = b+c;
-                    tmp[6] = c+d;
-                    tmp[7] = d+a;
-                    tmp[8] = a+c;
-                    tmp[9] = b+d;
-                    tmp[10] = a+b+c;
-                    tmp[11] = a+b+d;
-                    tmp[12] = a+c+d;
-                    tmp[13] = b+c+d;
-                    tmp[14] = a+b+c+d;
+                    int tmp[15] = {a, b, c, d, a + b, b + c, c + d, d + a,
+                    a + c, b + d, a + b + c, a + b + d, a + c + d, b + c + d,
+                    a + b + c + d};
+
                     sort(tmp, tmp+15);
                     sort(arr, arr+15);
-                    bool passed = true;
+
+                    bool passed = true; // arr 배열과 tmp 배열이 완전히 같을 경우 true
                     for(int i=0; i<15; i++){
                         if(tmp[i] != arr[i]){
                             passed = false;
                         }
                     }
+                    // 만약 두 배열이 완전히 같다면 a,b,c,d 조합을 찾아냄
                     if(passed){
                         cout << a << ' ' << b << ' ' << c << ' ' << d;
                     }
