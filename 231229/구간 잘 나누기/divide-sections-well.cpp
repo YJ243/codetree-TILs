2d8e72b4e07f9273b93785ce01b646110805589d
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-29 15:40:48 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231229/구간 잘 나누기/divide-sections-well.cpp
+++ 231229/구간 잘 나누기/divide-sections-well.cpp
@@ -1,65 +1,44 @@
 #include <iostream>
 #include <algorithm>
 
-#define MAX_A 10000
+#define MAX_A MAX_N*MAX_N   // 100이 100번 나오는 경우
 #define MAX_N 100
 
 using namespace std;
-
 int n, m;
-int a[MAX_N];
-
-int main() {
-    // 입력:
+int arr[MAX_N];
+int ans;
+int main(){
     cin >> n >> m;
-    int cur_max = 0;
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-        cur_max = max(cur_max, a[i]);
+    int max_num = 0;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        max_num = max(max_num, arr[i]);
     }
-    
-    // 100개의 숫자를 여러 개의 구간으로 나누는 경우의 수는 굉장히 많습니다.
-    // 숫자들을 모든 구간으로 나누어 완전탐색하기 위해서는
-    // 최악의 경우 99C50 = 약 5 * 10^28개의 경우를 세야 합니다.
-    // 이는 아무리 컴퓨터라 해도 너무 많은 경우의 수를 세야 합니다.
-    // 따라서 보다 효율적인 방법을 찾아야 합니다.
-
-    // 잘 생각해 보면, 구간의 합의 최댓값을 결정한다면
-    // 구간을 몇 개로 나눠야 하는지 손쉽게 찾을 수 있습니다.
-    int ans = MAX_A;
-    for(int i = cur_max; i <= MAX_A; i++) {
-        // 구간의 합의 최댓값이 i일 때
-
-        // possible : 구간을 나눌 수 있으면 true
-        // section : 나뉘어져야 하는 구간의 개수
-        bool possible = true;
-        int section = 1;
-
-        int cnt = 0;
-        for(int j = 0; j < n; j++) {
-            // 숫자 하나가 i보다 크면 구간을 나눌 수 없습니다.
-            if(a[j] > i) {
-                possible = false;
-                break;
+    // max_num ~ 전제 합까지 보기
+    for(int cur_max = max_num; cur_max <= MAX_A; cur_max++){
+        bool possible = true;   // 현재 구간 합들 중 최댓값이 cur_max가 되면서, m개의 구간으로 나눌 수 있는가?
+        int section = 1;        // 구간 개수
+        int cur_sum = 0;        // 구간에서의 합
+        int tmp_max = 0;
+        for(int i=0; i<n; i++){
+            // 배열의 첫번째부터 볼 것인데,
+            if(cur_sum + arr[i] > cur_max){
+                // 지금 숫자를 더했을 때 구간 합의 최대값보다 커진다면
+                section++;
+                cur_sum = 0;
             }
             
-            // j번째 숫자가 들어갔을 때 i보다 커지면
-            // j번째 숫자부터 다음 구간으로 만듭니다.
-            if(cnt + a[j] > i) {
-                cnt = 0;
-                section++;
-            }
-
-            // 이번 구간에 j번째 숫자를 집어넣습니다.
-            cnt += a[j];
+            cur_sum += arr[i];  // 현재 값 더하기
+            tmp_max = max(tmp_max, cur_sum);
+        }
+        if(tmp_max == cur_max && section <= m){
+            //cout << tmp_max << ' ' << section << '\n';
+            ans = cur_max;
+            break;
         }
 
-        if(possible && section <= m) {
-            ans = min(ans, i);
-        }
     }
-
     cout << ans;
-
     return 0;
 }
