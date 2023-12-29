31dd3ad4851d1d12834bbd507efff04c20b8d5b0
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-29 17:59:58 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231229/두 선분/two-lines.cpp
+++ 231229/두 선분/two-lines.cpp
@@ -1,3 +1,8 @@
+/*
+ * 2023.12.29
+ * 내 풀이
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -11,3 +16,34 @@ int main() {
         cout << "nonintersecting";
     return 0;
 }
+
+/*
+ * 해설 풀이
+
+#include <iostream>
+
+using namespace std;
+
+int x1, x2, x3, x4;
+
+bool Intersecting(int x1, int x2, int x3, int x4){
+    // 겹치지 않는 경우에 대한 처리를 먼저 진행하기
+    if(x2 < x3 || x4 < x1)
+        return false;
+    // 나머지는 전부 겹치는 경우라고 볼 수 있음
+    else
+        return true;
+}
+
+int main(){
+    // 입력:
+    cin >> x1 >> x2 >> x3 >> x4;
+
+    // 겹치는지를 확인하기
+    if(Intersecting(x1, x2, x3, x4))
+        cout << "intersecting";
+    else
+        cout << "nonintersecting";
+    return 0;
+}
+*/
