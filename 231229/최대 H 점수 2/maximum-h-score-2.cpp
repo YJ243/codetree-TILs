5cb80f82d1e7a43d5b8891b8dbc63983e99d5195
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-29 16:11:59 +0900

    Title: 최대 H 점수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231229/최대 H 점수 2/maximum-h-score-2.cpp
+++ 231229/최대 H 점수 2/maximum-h-score-2.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int n, l;   // n개의 숫자, 최대 l개의 서로 다른 원소 값을 1씩 올리기
 int h;      // h점수를 최대로 만들어야 함(h이상인 숫자의 수가 h개 이상인 것을 만족하는 h중 최댓값)
 int a[MAX_N];
+int ans;
 int main() {
     // 입력:
     cin >> n >> l;
@@ -12,6 +13,7 @@ int main() {
     for(int i=0; i<n; i++){
         cin >> a[i];
     }
+    ans = n;
     // 기준을 새로 설정: h가 답이 되는지 안되는지로 판단하기
     for(int h=1; h<=n; h++){
         int cnt = 0, plus_cnt = 0;
@@ -27,8 +29,10 @@ int main() {
                 //countable.push_back(i);
             }
         }
-        if(cnt >= h)
+        if(cnt >= h){
+            //cout << "h: " << h << ' ' << "cnt: " << cnt << ' ' << "plus: " << plus_cnt << '\n';
             continue;
+        }
         else{
             // 이제 L개 올려서 만들 수 있는지 보기
             if(cnt + l >= h && plus_cnt >= l){
@@ -36,10 +40,11 @@ int main() {
                 continue;
             }
             else{
-                cout << h-1;
+                ans = h-1;
                 break;
             }
         }
     }
+    cout << ans;
     return 0;
 }
