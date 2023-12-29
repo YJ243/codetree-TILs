a514a8a94bce8d4819e2e6cdc35f3ad74b0047f6
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-29 17:57:20 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231229/두 선분/two-lines.cpp
+++ 231229/두 선분/two-lines.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int x1, x2, x3, x4;
 int main() {
     cin >> x1 >> x2 >> x3 >> x4;
-    if(x3 <= x2 || x2 <= x4){
+    if((x1 <= x3 && x3 <= x2) || (x3 <= x1 && x1 <= x4)){
         cout << "intersecting";
     }
     else
