bdafdc8b04e640ccf035870c697f6f2b18154468
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-30 16:50:10 +0900

    Title: 전부 겹치는 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 231230/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -1,3 +1,8 @@
+/*
+ * 2023.12.30
+ * 내 풀이
+*/
+
 #include <iostream>
 #define MAX_N 100
 using namespace std;
@@ -25,3 +30,41 @@ int main() {
     else    cout << "No";
     return 0;
 }
+
+/*
+ * 해설
+ * 어느 한 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 온다면
+ * 선분이 전부 겹칠 수 없음
+ * 시작점 중 가장 뒤에 있는 좌표와, 끝점 중 가장 앞에 있는 점의 좌표를 확인
+ * 어느 한 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 온다면 선분이 겹치는 지점이 생길 수 없음
+
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int n;
+int x1, x2;
+int max_x1;
+int min_x2 = INT_MAX;
+
+int main(){
+    // 입력:
+    cin >> n;
+    // 시작점 중 가장 뒤에 있는 좌표와 끝점 중 가장 앞에 있는 점의 좌표를 확인
+    for(int i=0; i<n; i++){
+        cin >> x1 >> x2;
+        max_x1 = max(max_x1, x1);
+        min_x2 = min(min_X2, x2);
+    }
+
+    // 만약 어느 한 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 온다면
+    // 선분이 전부 겹칠 수 없음
+    if(min_x2 >= max_x1)
+        cout << "Yes";
+    else    cout << "No";
+    return 0;
+}
+*/
