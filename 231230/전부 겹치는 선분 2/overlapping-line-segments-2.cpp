70ccca02e9f9053bf18268bc4ad8bc58b1188c66
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-30 17:20:37 +0900

    Title: 전부 겹치는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231230/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 231230/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -7,22 +7,35 @@ using namespace std;
 
 int n;
 int a[MAX_N][2];
-int minX1 = INT_MAX, minX2 = INT_MAX;
-int maxX1, maxX2;
+
+int minX1[2] = {INT_MAX,0}, minX2[2] = {INT_MAX,0};
+int maxX1[2] = {0,-1}, maxX2[2] = {0,-1};
 int main() {
     cin >> n;
     for(int i=0; i<n; i++){
         cin >> a[i][0] >> a[i][1];
-        maxX1 = max(maxX1, a[i][0]);
-        minX1 = min(minX1, a[i][1]);
+        if(maxX1[0] < a[i][0]){
+            maxX1[0] = a[i][0];
+            maxX1[1] = i;
+        }
+        if(minX1[0] > a[i][1]){
+            minX1[0] = a[i][1];
+            minX1[1] = i;
+        }
     }
 
     for(int i=0; i<n; i++){
-        if(maxX2 < a[i][0] && a[i][0] != maxX1)   maxX2 = a[i][0];
-        if(minX2 > a[i][1] && a[i][1] != minX1)   minX2 = a[i][1];
+        if(maxX2[0] < a[i][0] && i != maxX1[1]){
+            maxX2[0] = a[i][0];
+            maxX2[1] = i;
+        }
+        if(minX2[0] > a[i][1] && i != minX1[1]){
+            minX2[0] = a[i][1];
+            minX2[1] = i;
+        }
     }
     //cout << minX1 << ' ' << minX2 << ' ' << maxX1 << ' ' <<maxX2 << '\n';
-    if(maxX2 >= minX1 || minX2 >= maxX1)  cout << "Yes";
+    if(maxX2[0] >= minX1[0] || minX2[0] >= maxX1[0])  cout << "Yes";
     else    cout << "No";
 
 
