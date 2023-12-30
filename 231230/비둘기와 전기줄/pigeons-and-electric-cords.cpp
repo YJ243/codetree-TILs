3d153a3111731fad6daec941314826cfa9e2d10b
yj____ <60027712+YJ243@users.noreply.github.com>
2023-12-30 18:30:05 +0900

    Title: 비둘기와 전기줄, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231230/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#define MAX_N 100
+
+using namespace std;
+int n;
+//vector<vector<int
+unordered_map<int, vector<int>> count;
+int ans;
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int number, loc;
+        cin >> number >> loc;
+        count[number].push_back(loc);
+    }
+
+    for(int i=0; i<n; i++){
+        if(count[i].size() <= 1) continue;
+        int cur = count[i][0];
+        for(int j=1; j<count[i].size(); j++){
+            if(cur != count[i][j]){
+                ans++;
+                cur = count[i][j];
+            }
+        }
+    }
+    cout << ans;
+
+    
+    return 0;
+}
