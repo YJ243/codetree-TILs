8dcb6139f0e09fdfd2e14e6f7adac108162cc4fd
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-01 20:55:58 +0900

    Title: 연속된 숫자 만들기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 240101/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -9,16 +9,21 @@ int loc[MAX_N];
 int ans;
 
 int main() {
+    // 입력:
     for(int i=0; i<MAX_N; i++)
         cin >> loc[i];
     
-    // step 0. 먼저 정렬하기
+    // step 0. 주어진 값들을 먼저 정렬하기
     sort(loc, loc+MAX_N);   
 
     // step 1. 만약 주어진 경우가 연속인 세 개 수라면
+    // 이동할 필요가 없으므로 최대 이동 횟수는 0
     if(loc[1]-loc[0] == 1 && loc[2]-loc[1] == 1)
         ans = 0;
+
     // step 2. 연속된 수가 아니라면
+    // 각각 거리를 한칸씩 좁혀가면서 이동하는 방식이 최대 이동횟수를 만들기 때문에
+    // 두 간격중 큰 것을 선택해, 해당간격-1이 답이 됨.
     else{
         int cur = max(loc[1]-loc[0], loc[2]-loc[1]);
         ans = cur-1;
