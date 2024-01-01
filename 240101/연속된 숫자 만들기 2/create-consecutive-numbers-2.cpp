d94a793bafb32fe0e997bab158f88097fda4a7c2
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-01 17:29:40 +0900

    Title: 연속된 숫자 만들기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240101/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240101/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 long long arr[3];
 int ans;
 int main() {
+    // 입력:
     for(int i=0; i<3; i++)
         cin >> arr[i];
     
+    // 주어진 값들을 정렬
+    sort(a, a+n);
+
     // Step 1. 시작부터 연속인 경우
     if(arr[1]-arr[0] == 1 && arr[2]-arr[1] == 1)
         ans = 0;
