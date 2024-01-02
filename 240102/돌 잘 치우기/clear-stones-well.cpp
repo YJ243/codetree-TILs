bceda2b40228a8abee97b629de1d79250aeacb56
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-02 20:33:04 +0900

    Title: 돌 잘 치우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/돌 잘 치우기/clear-stones-well.cpp
+++ 240102/돌 잘 치우기/clear-stones-well.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
 #define MAX_N 100
 
 using namespace std;
@@ -13,7 +14,7 @@ int start[MAX_N*MAX_N][2];          // 모든 시작점
 vector<pair<int, int> > stones;     // 돌이 들어있는 좌표들 저장
 vector<pair<int, int> > removed;     // 현재 턴에서 치워야할 돌
 int ans;
-
+int different_stone;
 bool InRange(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < n;
 }
@@ -24,39 +25,79 @@ bool CanGo(int x, int y){
 
 void do_remove(){
     for(int i=0; i<(int)removed.size(); i++){
-        int cx = removed[i][0], cy = removed[i][1];
+        int cx = removed[i].first, cy = removed[i].second;
         grid[cx][cy] = 0;
     }
 }
 
 void do_recover(){
     for(int i=0; i<(int)removed.size(); i++){
-        int cx = removed[i][0], cy = removed[i][1];
+        int cx = removed[i].first, cy = removed[i].second;
         grid[cx][cy] = 1;
     }
 }
+
+void initialize(){
+    // 방문 배열 초기화
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            visited[i][j] = false;
+
+    // 시작점들 큐에 넣기
+    for(int i=0; i<k; i++){
+        int sx = start[i][0], sy = start[i][1];
+        visited[sx][sy] = true;
+        q.push(make_pair(sx, sy));
+    }
+    // 현재 턴에서의 갈 수 있는 서로 다른 돌 개수 초기화
+    different_stone = k;
+}
+
+void bfs(){
+    int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+    while(!q.empty()){
+        pair<int,int> curr = q.front();
+        q.pop();
+        for(int d=0; d<4; d++){
+            int nx = curr.first+dirs[d][0];
+            int ny = curr.second+dirs[d][1];
+            if(CanGo(nx,ny)){
+                different_stone++;
+                visited[nx][ny] = true;
+                q.push(make_pair(nx,ny));
+            }
+        }
+    }
+
+    // 탐색이 끝난후 서로 다른 칸의 최댓값 갱신
+    ans = max(ans, different_stone);
+}
+
+void Move(){
+    initialize();
+    bfs();
+}
+
+
 void choose_stone(int t, int index){   // t번째 돌을 고르는 함수, index는 현재 stones 벡터에서 선택할 시작
     if(t == m){
         // 만약 m개를 다 골랐다면
         // 그 돌들 치우고 bfs 탐색하여 이동한 칸 수 세기
         do_remove();
-        for(int i=0; i<(int)removed.size(); i++)
-            cout << removed[i][0] << ' ' << removed[i][1] << '\n';
-        bfs();
+        Move();
         do_recover();
         return;
     }
 
     for(int i=index; i<(int) stones.size(); i++){
         // 돌을 하나 선택
-        removed.push_back(make_pair(stones[i][0], stones[i][1]));
+        removed.push_back(make_pair(stones[i].first, stones[i].second));
         // 다음 돌 선택
         choose_stone(t+1, i+1);
         // 선택 취소
         removed.pop_back();
     }
-
-
 }
 
 int main() {
@@ -77,9 +118,8 @@ int main() {
 
     // stones 중에서 m개 돌을 backtracking으로 고르기
     choose_stone(0,0);    // 0번째 stone 고르는 함수
-    for(int i=0; i<(int)stones.size(); i++){
-    // 2. 그리고 
-    // 3. ans와 비교해서 업데이트 하기
-    }
+    
+    // 출력
+    cout << ans;
     return 0;
 }
