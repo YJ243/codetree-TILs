2ddb1f80273370d032ec07de552389cbd5c56305
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-02 13:48:11 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240102/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -8,16 +8,19 @@ int graph[MAX_N][MAX_N];
 bool visited[MAX_N][MAX_N];
 int ans;
 
+// 탐색하는 위치가 격자 범위 내에 있는지 여부를 반환
 bool InRange(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < m;
 }
 
+// 탐색하는 위치로 움직일 수 있는지 여부를 반
 bool CanGo(int x, int y){
     // 범위 안에 있고, 방문하지 않았으며, 빈칸일 경우 갈 수 있음
     return InRange(x,y) && !visited[x][y] && graph[x][y];
 }
 
 void DFS(int x, int y){
+    // 아래쪽과 오른쪽 각각에 대하여 DFS 탐색
     for(int i=0; i<2; i++){
         int nx = x+dirs[i][0], ny = y+dirs[i][1];
         if(CanGo(nx,ny)){
