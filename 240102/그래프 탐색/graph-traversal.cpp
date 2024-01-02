ba43c331e5a64609609e938fb2eb9aafb9f0c5d3
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-02 12:03:33 +0900

    Title: 그래프 탐색, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/그래프 탐색/graph-traversal.cpp
+++ 240102/그래프 탐색/graph-traversal.cpp
@@ -3,13 +3,17 @@
 #define MAX_N 1000
 using namespace std;
 int n, m;
+
+// index를 1번부터 사용하기 위해 MAX_N+1만큼 할당하기
 vector<int> graph[MAX_N+1];
 bool visited[MAX_N+1];
 int ans;
 
 void DFS(int v){
+    // 해당 정점에서 이어져있는 모든 정점을 탐색해주기
     for(int i=0; i<graph[v].size(); i++){
         if(!visited[graph[v][i]]){
+            // 아직 방문한 적이 없는 정점에 대해서만 탐색 진행
             visited[graph[v][i]] = true;
             DFS(graph[v][i]);
         }
@@ -19,6 +23,8 @@ void DFS(int v){
 int main(){
     cin >> n >> m;
     for(int i=0; i<m; i++){
+        // 각 정점이 서로 이동이 가능한 양방향 그래프이기 때문에
+        // 각 정점에 대한 간선을 각각 저장해주
         int s, e;
         cin >> s >> e;
         graph[s].push_back(e);
@@ -32,7 +38,7 @@ int main(){
     for(int i=2; i<=n; i++){
         if(visited[i]) ans++; 
     }
-    
+
     cout << ans;
     return 0;
 }
