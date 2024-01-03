381afe29eb353ad6afb5f7148952529eca012905
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-03 23:14:43 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 59ms, Memory: 1MB, Status: Passed - CodeTree

--- 240103/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
+++ 240103/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -1,3 +1,10 @@
+/*
+ * 2023.1.3
+ * Backtracking을 활용해 가능한 모든 중복 순열 탐색
+ * 특정 시점에 뽑힌 숫자의 개수를 인자로 설정, n개의 숫자를 모두 뽑았을 때마다 출력
+ * 사전 순으로 앞선 것부터 순서쌍이 만들어지기 하기 위해 사용 가능한 숫자 중 가장 작은 숫자부터 순회
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,7 +17,7 @@ void Print(){
     cout << '\n';
 }
 
-void Choose(int num){   // num 번째 숫자 고르기
+void Choose(int num){   // 지금까지 num개의 숫자를 뽑았을 때 그 다음 어떤 숫자를 뽑을지 선
     if(num == n){       // 만약 n개 숫자 모두 고르고 다음으로 넘어왔다면
         Print();        // 출력
         return;
