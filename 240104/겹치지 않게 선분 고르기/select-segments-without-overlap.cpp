a90018bd230f3ee2ce2071e98b5b5476ca49d5fb
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-04 22:28:25 +0900

    Title: 겹치지 않게 선분 고르기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240104/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 240104/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,6 +1,12 @@
+/*
+ * 2024.1.4
+ * 1. 잦은 실수가 많아서 푸는데 오래 걸림
+ * 2. 조합을 백트래킹으로 처음 풀어봐서 풀이를 만들지 못함
+*/
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <algorithm>
 #define MAX_N 15
 using namespace std;
 
@@ -52,3 +58,73 @@ int main(){
     cout << ans;
     return 0;
 }
+
+/*
+ * 해설 풀이
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+
+#define MAX_N 15
+
+using namespace std;
+
+int n;
+pair<int, int> segments[MAX_N];
+
+int ans;
+vector<pair<int, int> > selected_segs;
+
+bool Overlapped(pair<int, int> seg1, pair<int, int> seg2){
+    int ax1, ax2;
+    tie(ax1, ax2) = seg1;
+
+    int bx1, bx2;
+    tie(bx1, bx2) = segs2;
+
+    // 두 선분이 겹치는지 여부는 한 점이 다른 선분에 포함되는 경우로 판단 가능
+    return (ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) ||
+           (bx1 <= ax1 && ax1 <= bx2) || (bx1 <= ax2 && ax2 <= bx2);
+}
+
+bool Possible(){
+    // 단 한쌍이라도 선분끼리 겹치면 안됨
+    for(int i=0; i<(int)selected_segs.size(); i++)
+        for(int j=i+1; j<(int)selected_segs.size(); j++)
+            if(Overlapped(selected_segs[i], selected_segs[i]))
+                return false;
+    
+    return true;
+}
+
+void FindMaxSegments(int cnt){
+    if(cnt == n){
+        if(Possible())
+            ans = max(ans, (int) selected_segs.size());
+        return;
+    }
+
+    selected_segs.push_back(segments[cnt]);
+    FindMaxSegments(cnt + 1);
+    selected_segs.pop_back();
+
+    FindMaxSegments(cnt+1);
+}
+
+int main(){
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int x1, x2;
+        cin >> x1 >> x2;
+        segments[i] = make_pair(x1, x2);
+    }
+    FindMaxSegments(0);
+
+    cout << ans;
+    return 0;
+}
+*/
