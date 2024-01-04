03fe1c5f23b536160b0c165b75734ce16a5d9db1
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-04 22:17:25 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240104/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 240104/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,59 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#include <algorithm>
 #define MAX_N 15
 using namespace std;
 
 int n;
-vector<pair<int, int> > lines;
-vector<pair<int, int> > selected_lines;
+pair<int, int> lines[MAX_N]; 
+vector<int> selected_idx;
 int ans;
 
-void CheckOverlapping(int total){            // 현재까지 선택된 선분들이 겹치는 게 있는지 확인
-    bool isOverlapped = false;
-    
-    for(int i=0; i<total; i++){
-        for(int j=0; j<total; j++){
-            if(i == j) continue;
-            if((selected_lines[i].first <= selected_lines[j].first && selected_lines[j].first <= selected_lines[i].second) || 
-                selected_lines[j].first <= selected_lines[i].second && selected_lines[i].second <= selected_lines[j].first) 
-                isOverlapped = true;
+bool isOverlapped(){
+    for(int i=0; i<(int)selected_idx.size(); i++){
+        for(int j=0; j<(int)selected_idx.size(); j++){
+            if(i==j) continue;
+            int x1, x2, x3, x4;
+            tie(x1,x2) = lines[i];
+            tie(x3,x4) = lines[j];
+            if((x1 <= x3 && x3 <= x2) || (x3 <= x1 && x1 <= x4)){
+                cout << x1 << ' ' << x2 << ' ' << x3 << ' ' << x4 << '\n';
+                return true;
+            }
         }
-    }    
-    if(!isOverlapped){
-        ans = max(ans, total);
     }
-    
+    return false;
 }
 
-void Choose(int cnt, int start, int total){    // 현재까지 cnt개를 뽑았고, 총 total개의 선분을 고르는 함수
-    if(cnt == total){
-        CheckOverlapping(total);
+void SelectLine(int num){   // num번째 선분 선택을 결정하는 함수
+    if(num == n){
+        if(!isOverlapped()){
+            // 만약 선분 조합이 겹치지 않으면
+            int cur = (int)selected_idx.size();
+            ans = max(ans, cur);
+        }
         return;
     }
-    // 중복순열이여서 시간초과가 뜸 => 시간초과가 안뜨게 중복 없애줘야 하는데...
-    for(int i=start; i<n-total+cnt; i++){
-        selected_lines.push_back(make_pair(lines[i].first, lines[i].second));
-        Choose(cnt+1, start+1, total);
-        selected_lines.pop_back();
-    }
-    
+
+    selected_idx.push_back(num);
+    SelectLine(num+1);
+    selected_idx.pop_back();
+
+    SelectLine(num+1);
 }
 
-int main() {
+int main(){
+    // 입력:
     cin >> n;
     for(int i=0; i<n; i++){
-        int x1, x2;
-        cin >> x1 >> x2;
-        lines.push_back(make_pair(x1, x2));
+        cin >> lines[i].first >> lines[i].second;
     }
-    for(int i=2; i<=n; i++){
-        //총 i개를 뽑아서 확인하기
-        //cout << i << '\n';
-        Choose(0,0, i);
-    }
-    //cout << ans;
-    cout << ans+1;
+
+    SelectLine(0);  // 0번째 선분을 확인하러 호출
+    cout << ans;
     return 0;
 }
