e36a68caed3273ed0f9fa49f0f8a58620b8f89c7
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-05 16:22:51 +0900

    Title: 사다리 타기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/사다리 타기/ladder-game.cpp
+++ 240105/사다리 타기/ladder-game.cpp
@@ -1,3 +1,10 @@
+/*
+ * 2024.1.5
+ * 현재 입력으로 주어진 가로줄 m개 중에서 조합을 만들어 보면 되는데
+ * 아예 처음부터 m개 외에도 나올 수 있는 모든 경우의 수를 생각해서 틀림: (n-1)*m
+ * 또한 사다리게임에서 진행이 위에서부터 아래로 내려오면서 가로선이 있을 때 
+ * 해당 위치에 있는 두 값을 바꾸는 것으로 시뮬레이션이 가능하다는 것을 몰랐음
+*/
 #include <iostream>
 #include <algorithm>
 #include <climits>
@@ -13,14 +20,18 @@ vector<pair<int, int> > selected_lines;
 
 int ans = INT_MAX;
 
+// 처음 상황과, 선택한 가로줄만 사용했을 때의 상황을 시뮬레이션
+// 둘의 결과가 같은지 확인
 bool IsSame(){
+    // Step1. 시작 숫자를 세팅
     int originArr[n] = {};
     int finalArr[n] = {};
     
     for(int i=0; i<n; i++)
         originArr[i] = finalArr[i] = i;
     
-    // 이제 주어진 가로줄을 차례대로 보면서 확인하기
+    // Step2. 위에서부터 순서대로 적혀있는 가로줄에 대해 양쪽 번호에 해당하는 숫자를 바꿔줌
+    // 주어진 가로줄을 차례대로 보면서 확인하기
     for(int i=0; i<(int)lines.size(); i++){
         int cur = lines[i].second;
         swap(originArr[cur], originArr[cur+1]);
@@ -29,6 +40,7 @@ bool IsSame(){
         int cur = selected_lines[i].second;
         swap(finalArr[cur], finalArr[cur+1]);
     }
+    // Step3. 두 상황의 결과가 동일한지 확
     for(int i=0; i<n; i++){
         if(originArr[i] != finalArr[i])
             return false;
