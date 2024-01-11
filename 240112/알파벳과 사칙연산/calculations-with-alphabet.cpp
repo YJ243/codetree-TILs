9669f7c78570b35923d3303e7ca02c35cb74480c
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-12 08:16:29 +0900

    Title: 알파벳과 사칙연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240112/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 240112/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -7,48 +7,72 @@ using namespace std;
 string s;
 int n;
 int ans;
-vector<int> numbers;
+vector<int> alpa_to_numbers;    // numbers[]
 char operations[MAX_N];
 
 void update(){
-    int cur_sum = numbers[0];
-    for(int i=1; i<(int)numbers.size(); i++){
-        if(operations[i-1]=='+'){
-            cur_sum += numbers[i];
+    int cur_sum = 0;
+
+    for(int i=0; i<(n+1)/2; i++){
+        //cout << i << ':' ;
+        int cur=0;
+        if(s[i*2] == 'a'){
+            cur=alpa_to_numbers[0];
         }
-        else if(operations[i-1]=='-'){
-            cur_sum -= numbers[i];
+        else if(s[i*2] == 'b'){
+            cur=alpa_to_numbers[1];
+        }
+        else if(s[i*2] == 'c'){
+            cur=alpa_to_numbers[2];
+        }
+        else if(s[i*2] == 'd'){
+            cur=alpa_to_numbers[3];
+        }
+        else if(s[i*2] == 'e'){
+            cur=alpa_to_numbers[4];
         }
         else{
-            cur_sum *=numbers[i];
+            cur=alpa_to_numbers[5];
         }
+        //cout << cur << '\n';
+        if(i==0)   cur_sum = cur;
+        else{
+           // cout << s[i*2-1] << '\n';
+            if(s[i*2-1]=='+'){
+                cur_sum += cur;
+            }
+            else if(s[i*2-1]=='-'){
+                cur_sum -= cur;
+            }
+            else{
+                cur_sum *=cur;
+            }
+        }
+       // cout << "cur_sum: " << cur_sum << '\n';
     }
-    if(cur_sum==12){
-        cout << numbers[0] << ' ' << numbers[1] << ' ' << numbers[2] << '\n';
-    }
+   // cout << '\n';
     ans = max(ans, cur_sum);
 }
 
 void choose(int num){   //numbers 배열의 num번째를 선택하러 가는 함수
-    if(num == (n+1)/2){
+    if(num == 6){
         update();
         return;
     }
 
     for(int i=1; i<=4; i++){
-        numbers.push_back(i);
+        alpa_to_numbers.push_back(i);
         choose(num+1);
-        numbers.pop_back();
+        alpa_to_numbers.pop_back();
     }
 
 }
 int main() {
     cin >> s;
     n = (int)s.size();
-    for(int i=0; i<(n+1)/2; i++){
-        operations[i] = s[i*2+1];
-    }
-    //choose(0);
+
+    choose(0);
+    /*
     int cur_sum = 4;
     for(int i=1; i<(n+1)/2; i++){
         if(operations[i-1]=='+'){
@@ -61,6 +85,7 @@ int main() {
             cur_sum *=4;
         }
     }
-    cout << cur_sum;
+    */
+    cout << ans;
     return 0;
 }
