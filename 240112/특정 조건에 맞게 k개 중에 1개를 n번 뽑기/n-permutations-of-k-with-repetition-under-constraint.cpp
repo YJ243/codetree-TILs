6a85af6dcc227c6a40e46ef5f8c17fce67f24a14
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-12 09:14:57 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 56ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240112/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int k, n;
+vector<int> a;
+
+void Print(){
+    for(int i=0; i<(int)a.size(); i++)
+        cout << a[i] << ' ';
+    cout << '\n';
+}
+void Choose(int num){   // num번째 숫자를 고르는 함수
+    if(num == n){
+        Print();
+        return;
+    }
+    for(int i=1; i<=k; i++){
+        if(num <= 1){
+            a.push_back(i);
+            Choose(num+1);
+            a.pop_back();
+        }
+        else if(a[num-1] != i || a[num-2] != i){
+            a.push_back(i);
+            Choose(num+1);
+            a.pop_back();
+        }
+        
+    }
+}
+int main() {
+    cin >> k >> n;
+    Choose(0);
+    return 0;
+}
