38772a53010716e9c92a91327bb0d71e5dd51be5
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-12 13:55:48 +0900

    Title: 1차원 윷놀이, Time: 117ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/1차원 윷놀이/yutnori-1d.cpp
+++ 240112/1차원 윷놀이/yutnori-1d.cpp
@@ -6,7 +6,7 @@
 #define MAX_K 4
 
 using namespace std;
-
+// 테스트케이스가 길어지니까 시간초과가 나
 int n, m, k;            // n: 턴수, m: 윷놀이 판 상태, k: 말의 수
 int jump[MAX_N];        // 각 턴마다 나아갈 수 있는 거리
 int locations[MAX_K+1];   // 각 말의 현재 위치
@@ -21,21 +21,18 @@ void Verify(){
     for(int i=0; i<n; i++){
         // i번째 턴을 확인
         int cur_jump = jump[i];
-        //cout << cur_jump << '\n';
-        while(cur_jump--){
-            //cout << cur_jump<< " adsfa"<<'\n';
-            if(locations[pieces[i]] + 1 <= m)
-                locations[pieces[i]]++;
-        }
+        locations[pieces[i]] += jump[i];
+          
     }
+    
     int cur_pass = 0;
     for(int i=1; i<=k; i++){
-        if(locations[i] == m){
+        if(locations[i] >= m){
             cur_pass++;
         }
     }
-
     ans = max(ans, cur_pass);
+
 }
 
 void Choose(int num){  // num번째 턴에 움직일 말을 선택하는 함수
