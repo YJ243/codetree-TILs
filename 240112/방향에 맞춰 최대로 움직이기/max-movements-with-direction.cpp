6abe7fc34ed63037b02a51cd8155a14d164c10c2
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-12 15:37:11 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
+++ 240112/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
@@ -13,25 +13,18 @@ int r, c;                           // 시작점 좌표
 int moving_dist;                    // 움직인 거리
 int ans;                            // 최대 이동할 수 있는 거리
 
-bool InRange(int x, int y){
+bool InRange(int x, int y){         // 해당 좌표가 범위 안에 있는지 인
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void Move(int x, int y){
-    /*
-    // 이동 가능할 때에만 Move를 부르
-    if(!InRange(x,y)){
-        // 해당 위치로 왔을 때 더이상 이동하지 못한다면
-        ans = max(ans, moving_dist);
-        return;
-    }
-    */
+bool CanGo(int x, int y, int prior_num){    // 해당 좌표가 범위 안에 있고, 이전 숫자보다 크다면 이동 가능
+    return InRange(x,y) && numbers[x][y] > prior_num;
+}
 
-    //cout << x << ' ' << y << ' ' << moving_dist << '\n';
-    // 1. directions[x][y]방향에 숫자가 현재보다 작은게 있다면 한칸 이동
-    for(int i=1; i<4; i++){
+void Move(int x, int y){
+    for(int i=1; i<n; i++){
         int nx = x+dx[directions[x][y]]*i, ny = y+dy[directions[x][y]]*i;
-        if(InRange(nx,ny) && numbers[nx][ny] > numbers[x][y]){
+        if(CanGo(nx,ny,numbers[x][y])){
             moving_dist++;
             ans = max(ans, moving_dist);    // 움직일 때마다 정답 업데이트
             Move(nx,ny);
