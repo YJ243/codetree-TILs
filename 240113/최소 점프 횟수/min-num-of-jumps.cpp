046fc2cf94983eccd464e5ea63d3cd6b353b2677
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-13 15:05:19 +0900

    Title: 최소 점프 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/최소 점프 횟수/min-num-of-jumps.cpp
+++ 240113/최소 점프 횟수/min-num-of-jumps.cpp
@@ -39,3 +39,44 @@ int main() {
     cout << ans;
     return 0;
 }
+/*
+해설 코드
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+#define MAX_N 10
+
+using namespace std;
+
+int n;
+int num[MAX_N];
+
+int ans = INT_MAX;
+
+void FindMin(int idx, int cnt){
+    // 마지막 위치를 넘었을 때 그 중 최소 이동 횟수를 갱신
+    if(idx >= n-1){
+        ans = min(ans, cnt);
+        return;
+    }
+    for(int dist = 1; dist <= num[idx]; dist++)
+        FindMin(idx+dist, cnt+1);
+}
+
+int main(){
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+        cin >> num[i];
+
+    FindMin(0,0);
+
+    if(ans == INT_MAX)
+        ans = -1;
+    cout << ans;
+    return 0;
+}
+
+*/
