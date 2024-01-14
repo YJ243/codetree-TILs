1aa4f0f58d8dd7c89697448b3fc6d2250da3ee64
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-15 01:28:16 +0900

    Title: 크기가 n인 순열, Time: 40ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/크기가 n인 순열/n-permutation.cpp
+++ 240115/크기가 n인 순열/n-permutation.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #define MAX_N 8
 using namespace std;
+
+// 변수 선언
 int n;
 vector<int> selected;
 bool visited[MAX_N];
@@ -13,11 +15,15 @@ void Print(){
     cout << '\n';
     
 }
+
+// num번째 출력할 원소를 고르는 함수, 계속 탐색을 이어서 진행 
 void Choose(int num){
+    // 모든 원소를 선택했을 때, 해당 순열을 출력 
     if(num == n){
         Print();
         return;
     }
+    // 앞에서부터 하나씩 원소를 선택
     for(int i=1; i<=n; i++){
         if(visited[i]) continue;
         visited[i] = true;
