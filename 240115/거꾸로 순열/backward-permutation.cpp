f83deab0d6846539116177f067082a94d9281166
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-15 01:33:37 +0900

    Title: 거꾸로 순열, Time: 42ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/거꾸로 순열/backward-permutation.cpp
+++ 240115/거꾸로 순열/backward-permutation.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #define MAX_N 8
 using namespace std;
-
+// 변수 선언
 int n;
 vector<int> picked;
 bool visited[MAX_N];
@@ -12,25 +12,28 @@ void Print(){
     cout << '\n';
 }
 
+// num번째 숫자를 선택하는 함수, 계속 탐색을 이어서 진행 
 void Choose(int num){
-    if((int)picked.size() == n){
+    // 모든 원소를 선택했을 때, 해당 순열을 출력 
+    if(num == n){
         Print();
         return;
     }
-
+    // 뒤에서부터 하나씩 원소를 선택 
     for(int i=n; i>=1; i--){
         if(visited[i]) continue;
 
         picked.push_back(i);
         visited[i] = true;
 
-        Choose(num-1);
+        Choose(num+1);
 
         visited[i] = false;
         picked.pop_back();
     }
 }
 int main() {
+    // 입력:
     cin >> n;
     Choose(0);
     return 0;
