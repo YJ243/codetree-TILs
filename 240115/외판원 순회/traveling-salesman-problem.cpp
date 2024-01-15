2bc486ddd14192eebdb888850eb89125c897262b
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-15 15:42:50 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240115/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+#define MAX_N 10
+
+using namespace std;
+
+vector<int> selected;
+int n;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N];
+int ans = INT_MAX;
+void FindMinCost(){
+    int currCost = 0, curNum = 0;
+    for(int i=0; i<(int)selected.size(); i++){
+        currCost += grid[curNum][selected[i]];
+        curNum = selected[i];
+    }
+    currCost += grid[curNum][0];
+    ans = min(ans, currCost);
+}
+
+void Choose(int num){
+    if(num == n){
+        FindMinCost();
+        return;
+    }
+
+    for(int i=1; i<n; i++){
+        if(visited[i]) continue;
+        visited[i] = true;
+        selected.push_back(i);
+
+        Choose(num+1);
+
+        visited[i] = false;
+        selected.pop_back();
+    }
+}
+
+int main() {
+    // 입력:
+    cin >> n;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin >> grid[i][j];
+    
+    Choose(1);      // 1다음 첫 번째로 이동할 번호를 고르는 함수    
+    cout << ans;
+    return 0;
+}
