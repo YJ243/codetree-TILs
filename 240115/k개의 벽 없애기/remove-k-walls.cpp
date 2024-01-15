0edf8fe8535faca6f6e82cdcf8a38c27c60187aa
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-15 19:28:30 +0900

    Title: k개의 벽 없애기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/k개의 벽 없애기/remove-k-walls.cpp
+++ 240115/k개의 벽 없애기/remove-k-walls.cpp
@@ -7,14 +7,18 @@
 #define MAX_N 100
 using namespace std;
 
+// 전역 변수 선언:
 int n, k;                       // n: 격자 크기, k: 없애야 할 벽의 개수
 int r1, c1, r2, c2;             // 시작점과 도착점
+int grid[MAX_N][MAX_N];         // 1: 벽으로 이동 불가, 0: 이동 가능
+
 vector<pair<int, int> > walls;  // 벽의 위치
-vector<int> selected_walls;         // 선택된 벽의 위치
-int grid[MAX_N][MAX_N]; // 1: 벽으로 이동 불가, 0: 이동 가능
+vector<int> selected_walls;     // 선택된 벽의 위치
+
+// bfs에 필요한 변수들
+queue<pair<int, int> > q;
 bool visited[MAX_N][MAX_N];
 int step[MAX_N][MAX_N];
-queue<pair<int, int> > q;
 
 int ans = INT_MAX;
 int wallCnt;
@@ -28,6 +32,9 @@ void Initialize(){
     }
 }
 
+// queue에 새로운 위치를 추가하고
+// 방문 여부를 표시해줍니다.
+// 시작점으로 부터의 최단거리 값도 갱신해줍니다.
 void Push(int x, int y, int d){
     visited[x][y] = true;
     q.push(make_pair(x,y));
@@ -38,19 +45,29 @@ bool InRange(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
+// 격자를 벗어나지 않으면서, 벽도 없고, 아직 방문한 적이 없는 곳이라면
+// 지금 이동하는 것이 최단거리임을 보장할 수 있으므로 가야만 합니다. 
 bool CanGo(int x, int y){
     return InRange(x,y) && grid[x][y] == 0 && !visited[x][y];
 }
 
+// bfs를 통해 최소 이동 횟수를 구합니다.
 void bfs(){
+    // queue에 남은 것이 없을때까지 반복합니다.
     while(!q.empty()){
+        // queue에서 가장 먼저 들어온 원소를 뺍니다.
         pair<int, int> curr = q.front();
         q.pop();
 
         int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+        // queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
         for(int d=0; d<4; d++){
             int nx = curr.first + dirs[d][0], ny = curr.second + dirs[d][1];
+            
+            // 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+            // 새로 queue에 넣어줍니다. 
             if(CanGo(nx,ny)){
+                // 최단 거리는 이전 최단거리에 1이 증가하게 됩니다. 
                 Push(nx,ny,step[curr.first][curr.second]+1);
             }
         }
@@ -98,7 +115,36 @@ void Choose(int num){
         selected_walls.pop_back();
     }
 }
+
+/*
+해설에서의 Backtracking 함수
+
+void FindMin(int idx, int cnt){
+    if(idx == (int)stone_pos.size()){
+        if(cnt == k){
+            // visited, step 값을 초기화해주기
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
+                    visited[i][j] = step[i][j] = 0;
+            
+            // bfs를 이용해 최소 이동 횟수를 구하기
+            // 시작점을 queue에 넣고 시작하기
+            Push(r1, c1, 0);
+            int min_dist = bfs();
+            ans = min(min_dist, ans);
+        }
+        return;
+    }
+
+    a[stone_pos[idx].first][stone_pos[idx].second] = 0;
+    FindMin(idx+1, cnt+1);
+    a[stone_pos[idx].first][stone_pos[idx].second] = 1;
+
+    FindMin(idx+1, cnt);
+}
+*/
 int main() {
+    // 입력:
     cin >> n >> k;
     for(int i=0; i<n; i++)
         for(int j=0; j<n; j++){
@@ -113,7 +159,9 @@ int main() {
 
     wallCnt = (int)walls.size();
     Choose(0);  // 없앨 첫 번째 벽 구하기
-    if(ans == INT_MAX)
+
+    // 출력:
+    if(ans == INT_MAX)  // 불가능한 경우라면 -1을 답으로 넣어주기
         ans = -1;
     cout << ans;
     return 0;
