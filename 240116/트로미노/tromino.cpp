de8af848ad7b8584c77df3fd379a32d2bf94ccbb
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-16 11:55:54 +0900

    Title: 트로미노, Time: 16ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/트로미노/tromino.cpp
+++ 240116/트로미노/tromino.cpp
@@ -80,3 +80,88 @@ int main() {
     cout << ans;
     return 0;
 }
+
+/*
+ * 해설 풀이
+ * 배열을 활용하여 가능한 모든 모양을 적어준 뒤, 이를 활용해 격자를 순회하며 최대 합을 구하
+ * 가능한 6개의 모든 배열을 표현할 수 있는 공통된 크기의 이차원 배열을 사용해야 깔끔함
+ * 3x3 크기의 배열을 활용해 각 모양을 정의해주기
+ * 이 때 각 모양이 3x3 격자를 벗어나지 않으면서 최대한 왼쪽 상단으로 밀착되게 만들어야 함
+ 9 9 5
+ 9 9 5
+ 5 5 5 를 생각하면 됨
+
+#include <iostream>
+#include <algorithm>
+
+#define MAX_NUM 200
+
+using namespace std;
+
+int n, m;
+int grid[MAX_NUM][MAX_NUM];
+
+// 가능한 모든 모양을 전부 적어줍니다.
+int shapes[6][3][3] = {
+    {{1, 1, 0},
+    {1, 0, 0},
+    {0, 0, 0}},
+
+    {{1, 1, 0},
+    {0, 1, 0},
+    {0, 0, 0}},
+
+    {{1, 0, 0},
+    {1, 1, 0},
+    {0, 0, 0}},
+
+    {{0, 1, 0},
+    {1, 1, 0},
+    {0, 0, 0}},
+
+    {{1, 1, 1},
+    {0, 0, 0},
+    {0, 0, 0}},
+
+    {{1, 0, 0},
+    {1, 0, 0},
+    {1, 0, 0}},
+};
+
+// 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환하기
+int GetMaxSum(int x, int y){
+    int max_sum = 0;
+    for(int i=0; i<6; i++){
+        bool is_possible = true;
+        int sum = 0;
+        for(int dx = 0; dx < 3; dx++)
+            for(int dy = 0; dy < 3; dy++){
+                if(shapes[i][dx][dy] == 0) continue;
+                if(x+dx >= n || y+dy >= m) is_possible = false;
+                else sum += grid[x+dx][y+dy];
+            }
+        if(is_possible)
+            max_sum = max(max_sum, sum);
+    }
+    return max_sum;
+}
+int main() {
+	
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			cin >> grid[i][j];
+	
+	int ans = 0;
+	
+    // 격자의 각 위치에 대하여 탐색하여줍니다.
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			ans = max(ans, GetMaxSum(i, j));
+	
+	cout << ans;
+	
+	return 0;
+}
+*/
