ca43201644dc0259edfcf627e469d23adbb5bc34
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-16 10:30:40 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/최고의 33위치/best-place-of-33.cpp
+++ 240116/최고의 33위치/best-place-of-33.cpp
@@ -22,6 +22,21 @@ int main(){
         for(int j=0; j<n; j++)
             cin >> grid[i][j];
     
+    /*
+    해설에서는 0부터 n까지 보고 범위 벗어나면 continue로 처리
+    for(int row=0; row<n; row++){
+        for(int col=0; col<n; col++){
+            // 3*3 격자가 n*n 격자를 벗어나는 경우는 계산하지 않음
+            if(row+2 >= n || col+2 >= n) continue;
+            
+            // 금의 개수를 계산
+            int num_of_gold = GetNumOfGold(row, col, row+2, col+2);
+
+            // 최대 금의 개수를 저장
+            max_gold = max(max_gold, num_og_gold);
+        }
+    }
+    */
     for(int i=0; i<=n-3; i++){
         for(int j=0; j<=n-3; j++){
             int cur = GetCoin(i,j);
