701d3cc700f8b26445fda410cea9b66c73b3dc25
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-16 10:59:12 +0900

    Title: 행복한 수열의 개수, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240116/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -65,3 +65,66 @@ int main() {
     cout << ans;
     return 0;
 }
+
+/*
+ * 해설 풀이
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 100
+
+int n, m;
+int grid[MAX_N][MAX_N];
+
+int seq[MAX_N];
+
+bool IsHappySequence(){
+    // 주어진 seq가 행복한 수열인지 판단하는 하뭇
+    int consecutive_count = 1, max_ccnt = 1;
+
+    for(int i=1; i<n; i++){
+        if(seq[i-1] == seq[i])
+            consecutive_count++;
+        else
+            consecutive_count = 1;
+        max_ccnt = max(max_ccnt, consecutive_count);
+    }
+
+    // 최대로 연속한 횟수가 m이상이면 true를 반환
+    return max_ccnt >= m;
+}
+
+int main(){
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+    int num_happy = 0;
+
+    // 먼저 가로로 행복한 수열의 수를 세기
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            seq[j] = grid[i][j];
+        }
+        if(IsHappySequence())
+            num_happy++;
+    }
+
+    // 세로로 행복한 수열의 수를 세기
+    for(int j=0; j<n; j++){
+        // 세로로 숫자들을 모아 새로운 수열을 만들기
+        for(int i=0; i<n; i++)
+            seq[i] = grid[i][j];
+        if(IsHappySequence())
+            num_happy++;
+    }
+
+    cout << num_happy;
+
+    return 0;
+}
+*/
