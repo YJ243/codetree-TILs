2c240f1846f0bbb0a39815786e681c76bc30ba22
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-16 17:21:41 +0900

    Title: 금 채굴하기, Time: 17ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/금 채굴하기/gold-mining.cpp
+++ 240116/금 채굴하기/gold-mining.cpp
@@ -8,6 +8,7 @@ int n, m;
 int grid[MAX_N][MAX_N];
 int ans;
 
+// 주어진 k에 대하여 채굴 가능한 금의 개수를 반환하
 int GetGold(int x, int y, int d){
     int curr = 0;
     for(int i=0; i<n; i++){
@@ -27,12 +28,14 @@ int main(){
         for(int j=0; j<n; j++)
             cin >> grid[i][j];
 
-    
+    // 격자의 각 위치가 마름모의 중앙일 때 채굴 가능한 금의 개수를 구하
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             // (i,j)가 마름모의 중심
             for(int k=0; k<=(n-1)*2; k++){
                 int curGold = GetGold(i,j,k);
+
+                // 손해를 보지 않으면서 채굴할 수 있는 최대 금의 개수 업데이
                 if((k*k)+(k+1)*(k+1) <= m*curGold){
                     ans = max(ans, curGold);
                 }
