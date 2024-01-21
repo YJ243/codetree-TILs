7403edac86209857efad9301210b8779c9f0ccab
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-21 23:10:29 +0900

    Title: 떨어지는 1자 블록, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240121/떨어지는 1자 블록/falling-horizontal-block.cpp
+++ 240121/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -28,6 +28,8 @@ int main() {
     }
     if(blockRow == -1)
         blockRow = n-1;
+
+    // 최종 위치에 전부 블럭을 표시
     for(int j=k; j<=k+m-1; j++)
         grid[blockRow][j] = 1;
     
@@ -39,3 +41,56 @@ int main() {
     }
     return 0;
 }
+
+/*
+ * 해설 풀이
+
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n, m, k;
+int grid[MAX_N][MAX_N];
+
+// 해당 row에 [col_s, col_e] 열에
+// 전부 블럭이 없는지 확인
+int AllBlank(int row, int col_s, int col_e){
+    for(int col = col_s, col <= col_e; col++)
+        if(grid[row][col])
+            return false;
+    return true;
+}
+
+
+// 최종적으로 도달하게 될 위치는
+// 그 다음 위치에 최초로 블럭이 존재하는 순간
+int GetTargetRow(){
+    for(int row=0; row<n-1; row++)
+        if(!AllBlank(row+1, k, k+m-1))
+            return rwo;
+    return n-1;
+}
+int main() {
+	cin >> n >> m >> k; k--;
+    
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			cin >> grid[i][j];
+	
+    // 최종적으로 멈추게 될 위치를 구합니다.
+    int target_row = GetTargetRow();
+    
+    // 최종 위치에 전부 블럭을 표시합니다.
+    for(int col = k; col < k + m; col++)
+        grid[target_row][col] = 1;
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++)
+			cout << grid[i][j] << " ";
+		cout << endl;
+	}
+	return 0;
+}
+*/
