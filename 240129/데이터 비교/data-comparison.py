a0017bbf842c1b836b5f50f8294f64977ff84ac0
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-29 22:04:24 +0900

    Title: 데이터 비교, Time: 210ms, Memory: 38MB, Status: Passed - CodeTree

---
+++ 240129/데이터 비교/data-comparison.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr1 = list(map(int, input().split()))
+set1 = set(arr1)
+
+n = int(input())
+arr2 = list(map(int, input().split()))
+
+for elem2 in arr2:
+    if elem2 not in set1:
+        print(0, end=' ')
+    else:
+        print(1, end=' ')
