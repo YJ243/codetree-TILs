1f32d54f541eb0126a4dbb094edfa47c52a5ff46
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-29 20:36:05 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 372ms, Memory: 26MB, Status: Passed - CodeTree

--- 240129/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 240129/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -1,34 +1,17 @@
 n = int(input())
 
-arr = [list(input()) for _ in range(n)]
-
+arr = [input() for _ in range(n)]
 
 ans = 0
+count = dict()
 for i in range(n):
-    freq_first = dict()
-    for k in arr[i]:
-        if k not in freq_first:
-            freq_first[k] = 1
-        else:
-            freq_first[k] += 1
-
-    cur = 1
-    first_list = [[key, value] for key, value in freq_first.items()]
-    for j in range(i+1,n):
-        freq_second = dict()
-        for k in arr[j]:
-            if k not in freq_second:
-                freq_second[k] = 1
-            else:
-                freq_second[k] += 1
-        IsSame = True
-        for i in range(len(first_list)):
-            if first_list[i][0] not in freq_second:
-                IsSame = False
-            elif first_list[i][1] != freq_second[first_list[i][0]]:
-                IsSame = False
-        if IsSame:
-            cur += 1
-    ans = max(ans, cur)
+    sorted_elem = ''.join(sorted(arr[i]))
+    arr[i] = sorted_elem
+    
+    if arr[i] not in count:
+        count[arr[i]] = 1
+    else:
+        count[arr[i]] += 1
+    ans = max(ans, count[arr[i]])
 
 print(ans)
