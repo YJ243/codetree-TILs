379cab1e378e01b23a31cef9796ff135020209d5
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-29 19:48:44 +0900

    Title: 원소의 합이 0, Time: 409ms, Memory: 52MB, Status: Passed - CodeTree

--- 240129/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240129/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -24,3 +24,39 @@ for i in range(len(freqAB_arr)):
         ans += freqCD[-freqAB_arr[i][0]] * freqAB[freqAB_arr[i][0]]
 
 print(ans)
+
+''' 해설 '''
+'''
+# 변수 선언 및 입력:
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+
+count = dict()
+
+# A 수열에서 숫자 하나, B수열에서 숫자 하나를 골랐을 때
+# 나올 수 있는 두 숫자의 합들을 hashmap에 기록하기
+for i in range(n):
+    for j in range(n):
+        sum_val = A[i] + B[j]
+        if sum_val in count:
+            count[sum_val] += 1
+        else:
+            count[sum_val] = 1
+        
+# C, D 수열을 순회하며 쌍을 만들어 주기
+# 앞서 계산한 hashmap을 이용하면
+# C, D 수열에서 고른 값으로 A, B와 쌍을 만들 때
+# 총합이 0이 되는 쌍의 개수를 쉽게 구할 수 있음
+for i in range(n):
+    for j in range(n):
+        diff = -C[i] - D[j]
+
+        if diff in count:
+            ans += count[diff]
+
+# 출력:
+print(ans)
+'''
