885e21422f821d5a4edc1189eef33e6478d23522
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-29 21:02:26 +0900

    Title: 낮은 지점들, Time: 328ms, Memory: 31MB, Status: Passed - CodeTree

--- 240129/낮은 지점들/lowest-points.py
+++ 240129/낮은 지점들/lowest-points.py
@@ -15,3 +15,31 @@ for key, value in x_to_min_y.items():
     ans += value
     
 print(ans)
+
+''' 해설 '''
+'''
+# 변수 선언 및 입력:
+n = int(input())
+min_y = dict()
+
+# 각 x마다 최소 y만 저장해주기
+for _ in range(n):
+    x, y = tuple(map(int, input().split()))
+
+    # 아직 map에 해당 x가 없다면 y값을 그대로 넣어주기
+    if x not in min_y:
+        min_y[x] = y
+    # 그렇지 않다면, 최소 y를 넣어주기
+    else:
+        min_y[x] = min(min_y[x], y)
+
+# 답을 저장하기
+sum_val = 0
+
+# map에 들어있는 값들을 순회
+for value in min_y.values():
+    # value에 해당하는 y값을 더해주기
+    sum_val += value
+
+print(sum_val)
+'''
