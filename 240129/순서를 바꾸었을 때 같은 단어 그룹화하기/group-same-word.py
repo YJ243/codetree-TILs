5b401f3c610b8d7d01493c649da4e2109d5c6780
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-29 20:49:02 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 446ms, Memory: 26MB, Status: Passed - CodeTree

--- 240129/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 240129/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -15,3 +15,37 @@ for i in range(n):
     ans = max(ans, count[arr[i]])
 
 print(ans)
+
+''' 해설 '''
+'''
+# 같은 그룹에 속하는 단어들의 문자를 정렬하면 모두 같아지는 특징 이용하기!!!
+
+# 변수 선언 및 입력:
+n = int(input())
+
+count = dict()
+
+# 입력 문자열을 정렬한 뒤 map에 저장
+for _ in range(n):
+    inp = input()
+    # 정렬을 하면 자동으로 타입이 배열으로 변경된다는 점을 주의
+    inp = sorted(inp)
+
+    # 배열을 다시 문자열으로 돌려놓기
+    string = ""
+    for elem in inp:
+        string += elem
+    if string in count:
+        count[string] += 1
+    else:
+        count[string] = 1
+
+# map을 순회하며 최대인 경우를 출력해주기
+ans = 0
+
+for key, value in count.items():
+    if ans < value:
+        ans = value
+
+print(ans)
+'''
