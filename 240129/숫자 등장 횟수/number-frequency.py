51646a569eb4b2e49cd5ea8ed04fc94f643e71fb
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-29 16:07:18 +0900

    Title: 숫자 등장 횟수, Time: 191ms, Memory: 34MB, Status: Passed - CodeTree

--- 240129/숫자 등장 횟수/number-frequency.py
+++ 240129/숫자 등장 횟수/number-frequency.py
@@ -1,15 +1,22 @@
+# 변수 선언 및 입력:
 n, m = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
-num_to_index = dict()
-for i, num in enumerate(arr):
-    if num in num_to_index:
-        num_to_index[num] += 1
-    else:
-        num_to_index[num] = 1
 
+freq = dict()
+
+# 각 숫자가 몇 번씩 나왔는지르 hashmap에 기록
+for num in arr:
+    # 처음 나온 ㅅ수자라면 1을 직접 적어주기
+    if num in freq:
+        freq[num] += 1
+    # 이미 나와있던 숫자라면 1을 더하기
+    else:
+        freq[num] = 1
+
+# m개의 질의에 대해 몇 번씩 나왔는지를 출력
 key_num = list(map(int, input().split()))
 for num in key_num:
-    if num in num_to_index:
-        print(num_to_index[num], end=' ')
+    if num in freq:
+        print(freq[num], end=' ')
     else:
         print(0, end=' ')
