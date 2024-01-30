b68e9df76c04cd9d42f95645d480ae7da940aefe
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-30 17:58:57 +0900

    Title: DateTime to DateTime, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/DateTime to DateTime/datetime-to-datetime.py
+++ 240130/DateTime to DateTime/datetime-to-datetime.py
@@ -14,3 +14,18 @@ if days == 0 and mins < 0:
 total += mins
 
 print(total)
+
+''' 해설 '''
+'''
+# 변수 선언 및 입력
+a, b, c = tuple(map(int, input().split()))
+
+# 차이를 계산하기
+diff = (a*24*60 + b*60+c) - (11*24*60+11*60+11)
+
+# 출력
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
+    '''
