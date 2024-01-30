216f83c28221ad79b7fb87fb2165b2770006b627
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-30 20:07:52 +0900

    Title: 연속되는 수 4, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/연속되는 수 4/continuous-number4.py
+++ 240130/연속되는 수 4/continuous-number4.py
@@ -2,8 +2,9 @@ n = int(input())
 
 arr = [int(input()) for _ in range(n)]
 
+# 연속하여 동일한 숫자가 나오는 횟수를 구해보며,
+# 그 중 최댓값을 갱신
 ans, cnt = 0,0
-
 for i in range(len(arr)):
     if i >= 1 and arr[i-1] < arr[i]:
         cnt += 1
