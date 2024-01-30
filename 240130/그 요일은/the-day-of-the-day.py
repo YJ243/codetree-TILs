ff600d11a21d1cf4bbdd13a679a99d3752cb6444
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-30 18:18:21 +0900

    Title: 그 요일은, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240130/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+day = input()
+
+day_of_week = {"Mon":0, "Tue":1, "Wed":2, "Thu":3, "Fri":4, "Sat":5, "Sun":6}
+
+def count_days(m, d):
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    for i in range(1, m):
+        total_days += days[i]
+    total_days += d
+
+    return total_days
+
+diff = count_days(m2, d2) - count_days(m1, d1)
+
+ans = diff // 7
+if diff % 7 <= day_of_week[day]:
+    ans += 1
+
+print(ans)
