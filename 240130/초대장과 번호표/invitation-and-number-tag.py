886a86aa443d17e8b48a41a0202658f9b6e430cb
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-30 13:37:40 +0900

    Title: 초대장과 번호표, Time: 378ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240130/초대장과 번호표/invitation-and-number-tag.py
+++ 240130/초대장과 번호표/invitation-and-number-tag.py
@@ -10,7 +10,7 @@ people.sort(key=lambda x: (x[0], x[1]))
 
 total_invited.add(1)
 
-for _ in range(n//2):
+for _ in range(10):
     for group in people:
         total_cnt = group[0]
         invited_cnt = 0
@@ -18,15 +18,15 @@ for _ in range(n//2):
         for i in range(1, len(group)):
             if group[i] in total_invited:   # 만약 해당 번호를 가진 사람이 초대받았다면
                 invited_cnt += 1            # 초대받은 명수 추가
-    
+
         # 한번더 확인하면서 개수가 k-1인지 확인
         for i in range(1, len(group)):
             if group[i] not in total_invited:
                 if invited_cnt == total_cnt-1:
                     total_invited.add(group[i])
                     invited_cnt += 1
-    
-    
+
+
 
 
 print(len(total_invited))
