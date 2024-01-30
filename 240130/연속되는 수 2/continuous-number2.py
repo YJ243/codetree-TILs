5522f3db234d40a4268261331a95731ed9311d9f
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-30 19:53:59 +0900

    Title: 연속되는 수 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240130/연속되는 수 2/continuous-number2.py
+++ 240130/연속되는 수 2/continuous-number2.py
@@ -1,9 +1,10 @@
+# 변수 선언 및 입력
 n = int(input())
 arr=[int(input()) for _ in range(n)]
 
-
 ans = 0
 cur = 0
+# 연속하여 동일한 숫자가 나오는 횟수를 구해보면서, 그 중 최댓값 갱신
 for i in range(len(arr)):
     if i == 0 or arr[i-1] != arr[i]:
         cur = 1
