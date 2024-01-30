2ea7b5c5eaf68172ca0cb6b97bd7b3d3ded40d32
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-30 17:33:04 +0900

    Title: Date to Date, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/Date to Date/date-to-date.py
+++ 240130/Date to Date/date-to-date.py
@@ -12,3 +12,27 @@ for i in range(1, m2):
 second += d2
 
 print(second - first + 1)
+
+''' 해설 풀이 '''
+'''
+# 변수 선언 및 입력
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+
+def num_of_days(m, d):
+    # 계산 편의를 위해 월마다 몇 일이 있는지를 적어주기
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+
+    # 1월부터 (m-1)월까지는 전부 꽉 채워져 있음
+    for i in range(1, m):
+        total_days += days[i]
+    
+    # m월의 경우에는 정확히 d일만 있음
+    total_days += d
+
+    return total_days
+
+# 출력
+total_days = num_of_days(m2, d2) - num_of_days(m1, d1) + 1
+print(total_days)
+'''
