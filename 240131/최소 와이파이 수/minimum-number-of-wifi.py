215e8a58cf932dddc56121f2dd0b8f87c676c434
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-31 22:04:30 +0900

    Title: 최소 와이파이 수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240131/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,6 +1,10 @@
 n, m = tuple(map(int, input().split())) # n개의 숫자, 거리 m이내
 arr = list(map(int, input().split()))
 
+
+# 사람이 살고 있는 곳이 나오면
+# 와이파이를 해당 위치로부터 오른족으로 m만큼 떨어진 곳에 놓은 뒤,
+# 2m만큼 떨어진 곳에서 시작하여 다시 탐색을 진행
 ans, cur = 0,0
 
 while cur < n:
