c758fd5d49bf8b78e552cb8f31b9f0c4332608bd
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-31 21:22:11 +0900

    Title: 최소 와이파이 수, Time: 5033ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240131/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,21 @@
+n, m = tuple(map(int, input().split())) # n개의 숫자, 거리 m이내
+arr = list(map(int, input().split()))
+
+ans = 0
+cur = m
+while True:
+    CanbeSetup = False
+    for i in range(cur-m, cur):
+        if arr[i]:
+            CanbeSetup = True
+
+    if CanbeSetup or cur + m >= n:
+        ans += 1
+        cur += 2*m+1
+    else:
+        cur += m
+
+    if cur >= n:
+        break
+
+print(ans)
