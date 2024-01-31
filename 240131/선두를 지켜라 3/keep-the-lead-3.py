058ead226fc1df5119b2a0cae1a0d8de77c90b4d
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-31 14:27:58 +0900

    Title: 선두를 지켜라 3, Time: 202ms, Memory: 45MB, Status: Passed - CodeTree

--- 240131/선두를 지켜라 3/keep-the-lead-3.py
+++ 240131/선두를 지켜라 3/keep-the-lead-3.py
@@ -34,3 +34,53 @@ for i in range(1, len(A_loc)):
 
 
 print(ans)
+
+''' 해설 '''
+'''
+MAX_T = 1000000
+
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
+
+# A가 매 초마다 서있는 위치를 기록
+time_a = 1
+for _ in range(n):
+    v, t = tuple(map(int, input().split()))
+    for _ in range(t):
+        pos_a[time_a] = pos_a[time_a-1]+v
+        time_a += 1
+
+# B가 매 초마다 서있는 위치를 기록
+time_b = 1
+for _ in range(m):
+    v, t = tuple(map(int, input().split()))
+    for _ in range(t):
+        pos_b[time_b] = pos_b[time_b - 1] + v
+        time_b += 1
+
+# A와 B 중 더 앞서 있는 경우를 확인하기
+# A가 리더면 1, B가 리더면 2, 둘다 리더면 3으로 관리하기
+leader, ans = 0, 0
+for i in range(1, time_a):
+    if pos_a[i] > pos_b[i]:
+        # 조합이 바뀌었다면 답을 갱신
+        if leader != 1:
+            ans += 1
+        # 리더를 A로 변경
+        leader = 1
+    
+    elif pos_a[i] < pos_b[i]:
+        # 조합이 바뀌었다면 답을 갱신
+        if leader != 2:
+            ans += 1
+        # 리더를 B로 변경
+        leader = 2
+    else:
+        # 조합이 바뀌었다면 답을 갱신
+        if leader != 3:
+            ans += 1
+        # 리더를 둘 다로 변경
+        leader = 3
+print(ans)
+'''
