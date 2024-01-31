506622c8c8cf72c2d0924412e877a091dbff25a7
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-31 20:48:31 +0900

    Title: 움직이는 블록, Time: 405ms, Memory: 26MB, Status: Passed - CodeTree

--- 240131/움직이는 블록/moving-block.py
+++ 240131/움직이는 블록/moving-block.py
@@ -35,3 +35,28 @@ while True:
     ans += moved_len
 
 print(ans)
+
+''' 해설 '''
+'''
+항상 평균 블록 수보다 많은 곳에서 적은 곳으로 옮기는 것이 최적임
+평균 블럭 수보다 더 큰 블럭에 대해서만 그 차이만큼 옮겨주면 됨
+
+# 변수 선언 및 입력:
+n = int(input())
+blocks = [
+    int(input())
+    for _ in range(n)
+]
+
+# 전체 블럭 수를 세기
+sum_of_blocks = sum(blocks)
+
+# 평균 블럭 수보다 더 큰 블럭에 대해서만 그 차이만큼 옮겨주면 됨
+avg = sum_of_blocks // n
+ans = 0
+for block_num in blocks:
+    if block_num > avg:
+        ans += block_num - avg
+
+print(ans)
+'''
