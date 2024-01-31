0f40498478e3670fab291a66c5d095cbec745aab
yj____ <60027712+YJ243@users.noreply.github.com>
2024-01-31 22:03:54 +0900

    Title: 최소 와이파이 수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240131/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,37 +1,13 @@
 n, m = tuple(map(int, input().split())) # n개의 숫자, 거리 m이내
 arr = list(map(int, input().split()))
 
-ans = 0
-cur = m
-if m == 0:
-    print(sum(arr))
-    exit(0)
+ans, cur = 0,0
 
-if n < m:
-    print(1)
-    exit(0)
-
-
-if sum(arr) == 0:
-    print(0)
-    exit(0)
-
-while True:
-    CanbeSetup = False
-    for i in range(cur-m, cur):
-        if arr[i]:
-            CanbeSetup = True
-
-    if CanbeSetup or cur + m >= n:
+while cur < n:
+    if arr[cur] == 1:
         ans += 1
-        if cur+2*m+1 < n:
-            cur += 2*m+1
-        else:
-            cur += m+1
+        cur += 2*m+1
     else:
-        cur += m
-
-    if cur >= n:
-        break
+        cur += 1
 
 print(ans)
