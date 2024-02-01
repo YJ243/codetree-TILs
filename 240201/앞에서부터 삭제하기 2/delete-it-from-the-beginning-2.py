f1e820827979fd6b572865a3657c7e69ae8f94e7
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-01 19:50:54 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5048ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

---
+++ 240201/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -0,0 +1,24 @@
+import heapq
+n = int(input())
+arr = list(map(int, input().split()))
+
+total = sum(arr)
+
+ans = 0
+for k in range(1, n-1):
+    # k개 만큼 앞에서부터 삭제하기
+    tmp_total = total
+    for i in range(k):
+        tmp_total -= arr[i]
+
+    pq = []
+    # 삭제하고 남은 나머지들을 pq에 넣기
+    for i in range(k, n):
+        heapq.heappush(pq, arr[i])
+    tmp_total -= heapq.heappop(pq)
+    
+    # 평균 구하기
+    avg = tmp_total / len(pq)
+    ans = max(ans, avg)
+
+print(f"{ans:.2f}")
