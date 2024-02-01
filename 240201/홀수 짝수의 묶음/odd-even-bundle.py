ccf1d52827cf4de31afab6d4236216132310b1d5
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-01 15:42:54 +0900

    Title: 홀수 짝수의 묶음, Time: 5011ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240201/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240201/홀수 짝수의 묶음/odd-even-bundle.py
@@ -1,7 +1,6 @@
 n = int(input())
 arr = list(map(int, input().split()))
-odd = 0     # 홀수
-even = 0    # 짝수
+odd, even = 0,0
 
 for num in arr:
     if num % 2 == 0:
@@ -9,38 +8,31 @@ for num in arr:
     else:
         odd += 1
 
-# 일단 짝수를 먼저 없애놓기
-ans = 0
-if odd > even:
-    # 가능한 입력 중에서 짝수가 홀수보다 많거나 같을 경우
-    ans += even*2 # 그 짝수 개수만큼 답 증가(짝/홀/짝/홀...이렇게 짝수 개수만큼 뽑기)
-    # 그럼 이제 홀수는 총 개수-짝수개만큼 남음
-    odd -= even
-    if odd > 0:
+group_num = 0
 
-        # 이제 계산 시작, 홀수가 odd개인 배열에서 적당히 묶어서 짝/홀/짝/홀 번갈아 나오도록 하기!
-        tmp = [0 for _ in range(odd)]     # 0: 묶어서 짝수, 1: 묶어서 홀수
-        for i in range(odd):
-            if i % 3 == 2:
-                tmp[i] = 1
-
-        if tmp[odd-1] == 1 and tmp[odd-2] == 1:
-            ans += 1
-            odd = odd-4
-            ans += (odd // 3)*2
+while True:
+    if group_num % 2 == 0:
+        # 짝수를 만드는 차례
+        if even:
+            even -= 1
+            group_num += 1
+        elif odd >= 2:
+            odd -= 2
+            group_num += 1
         else:
-            if tmp[odd-1] == 1 and odd[odd-2] == 0:
-                odd += 2
-                odd = odd-3
-                ans += (odd//3)*2
-            elif tmp[odd-1] == 0:
-
-                ans += 1
-                odd = odd-2
-                ans += (odd // 3 )*2
-else:
-    if odd == even:
-        ans = odd*2
+            # 짝수를 만들어야 하는데 만들 숫자가 없는 상황
+            # ex) ... + 짝 + 홀 + ( )
+            if odd > 0: # 만약 홀수가 한개 남아 있다면 짝+홀+홀수1개 합쳐서 짝 만들어야 함
+                group_num -= 1
+                break
     else:
-        ans = odd*2+1
-print(ans)
+        # 홀수를 만드는 차례
+        if odd:
+            odd -= 1
+            group_num += 1
+        else:
+            # 만약 홀수를 만들어야 하는데 홀수가 없는 상황
+            # ex) ... + 짝 + 홀 + 짝 + (  )
+            # 그럼 여기서 뭘 더 할 수가 없으니 나가기
+            break
+print(group_num)
