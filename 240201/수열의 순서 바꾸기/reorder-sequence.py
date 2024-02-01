99b43e1ffd6b01416755a70a1aaac3bbd7aae4dc
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-01 17:54:52 +0900

    Title: 수열의 순서 바꾸기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/수열의 순서 바꾸기/reorder-sequence.py
+++ 240201/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,40 +1,12 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-ans = 0
+sorted_idx = n-2
 
-def Finish():
-    tmp = [0 for _ in range(n)]
-    for i in range(n):
-        tmp[i] = arr[i]
-    tmp.sort()
-    for i in range(n):
-        if tmp[i] != arr[i]:
-            return False
-    return True
-
-while True:
-    if Finish():
-        break
-    moved = arr[0]
-
-    next_idx = n-1
-    for i in range(n-1, 0, -1):
-        if arr[i] - 1 == arr[i-1]:
-            next_idx = i-1
-        else:
-            break
-
-
-
-    if moved < arr[next_idx]:
-        for i in range(1, next_idx):
-            arr[i-1] = arr[i]
-        arr[next_idx-1] = moved
+while sorted_idx >= 0:
+    if arr[sorted_idx] < arr[sorted_idx + 1]:
+        # 만약 정렬이 되어있다면
+        sorted_idx -= 1
     else:
-        for i in range(1, n):
-            arr[i-1] = arr[i]
-        arr[n-1] = moved    
-    ans += 1
-
-print(ans)
+        break
+print(sorted_idx+1)
