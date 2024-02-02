6d58d3bfd029ef7a327892b2ed1bc0ef71c14e3a
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-02 17:01:47 +0900

    Title: 아름다운 수, Time: 367ms, Memory: 29MB, Status: Passed - CodeTree

--- 240202/아름다운 수/beautiful-number.py
+++ 240202/아름다운 수/beautiful-number.py
@@ -32,3 +32,46 @@ def Choose(idx):    # idx번째 수를 선택하는 함수
 
 Choose(0)
 print(ans)
+
+
+''' 해설 풀이 '''
+'''
+# 변수 선언 및 입력:
+n = int(input())
+ans = 0
+seq = list()
+
+def is_beautiful():
+    # 연달아 같은 숫자가 나오는 시작 위치를 잡기
+    i = 0:
+    while i < n:
+        # 만약 연속하여 해당 숫자만큼 나올 수 없다면
+        # 아름다운 수가 아님
+        if i + seq[i] - 1 >= n:
+            return False
+        
+        # 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인
+        # 하나라도 다른 숫자가 있다면 아름다운 수가 아님
+        for j in range(i, i+seq[i]):
+            if seq[j] != seq[i]:
+                return False
+        
+        i += seq[i]
+
+    return True
+
+def count_beautiful_seq(cnt):
+    global ans
+
+    if cnt == n:
+        if is_beautiful():
+            ans += 1
+        return
+    
+    for i in range(1,5):
+        seq.append(i)
+        count_beautiful_seq(cnt+1)
+        seq.pop()
+count_beautiful_seq(0)
+print(ans)
+'''
