e3a37c5a6c3c4316ec7f6628505a3bd50f4ce881
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-02 15:05:15 +0900

    Title: 최솟값 3개, Time: 363ms, Memory: 32MB, Status: Passed - CodeTree

--- 240202/최솟값 3개/top-3-smallest-number.py
+++ 240202/최솟값 3개/top-3-smallest-number.py
@@ -18,3 +18,36 @@ for i in range(n):
         print(curr)
         for j in range(3):
             heapq.heappush(pq, tmp[j])
+
+''' 해설 '''
+'''
+import heapq
+
+# 입력:
+n = int(input())
+arr = list(map(int, input().split()))
+
+# 변수 선언
+pq = [] 
+
+# priority queue를 이용하여 진행합니다.
+for elem in arr:
+    # 해당 숫자를 priority queue에 넣어주기
+    heapq.heappush(pq, elem)
+
+    if len(pq) >= 3:
+        # 주어진 수가 3개 이상이라면 가장 작은 숫자 3개의 곱을 출력
+        x = heapq.heappop(pq)
+        y = heapq.heappop(pq)
+        z = heapq.heappop(pq)
+
+        print(x * y * z)
+
+        heapq.heappush(pq, x)
+        heapq.heappush(pq, y)
+        heapq.heappush(pq, z)
+    
+    else:
+        # 아직 주어진 숫자의 수가 3개가 되지 않으면 -1을 출력합니다.
+        print(-1)
+'''
