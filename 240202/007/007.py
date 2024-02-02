1c09a99dc82df19ba8bed733ac913e41d987e729
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-02 19:27:00 +0900

    Title: 007, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/007/007.py
+++ 240202/007/007.py
@@ -1,12 +1,17 @@
+# 클래스 선언
 class secret:
     def __init__(self, code, loc, time):
         self.code = code
         self.loc = loc
         self.time = time
 
+# 변수 선언 및 입력
 code, point, time =  input().split()
 
+# 객체 생성
 secrect1 = secret(code, point, time)
-print("secret code :", secrect1.code)
-print("meeting point :", secrect1.loc)
-print("time :", secrect1.time)
+
+# 출력
+print(f"secret code : {secrect1.code}")
+print(f"meeting point : {secrect1.loc}")
+print(f"time : {secrect1.time}")
