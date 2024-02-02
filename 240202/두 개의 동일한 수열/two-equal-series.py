426032f2e8fb655b13f4030757d43f2815e78e9d
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-02 15:22:49 +0900

    Title: 두 개의 동일한 수열, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/두 개의 동일한 수열/two-equal-series.py
+++ 240202/두 개의 동일한 수열/two-equal-series.py
@@ -9,3 +9,30 @@ for i in range(n):
         print("No")
         exit(0)
 print("Yes")
+
+''' 해설 '''
+'''
+# 변수 선언 및 입력
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+def equal():
+    # n개의 원소를 순선대로 봤을 때
+    # 전부 동일한 경우에만 일치함
+    # 단 하나라도 다르다면, false
+    for elem1, elem2 in zip(a,b):
+        if elem1 != elem2:
+            return False
+    return True
+
+# 정렬
+a.sort()
+b.sort()
+
+# 수열이 일치하는지 확인합니다.
+if equal():
+    print("Yes")
+else:
+    print("No")
+'''
