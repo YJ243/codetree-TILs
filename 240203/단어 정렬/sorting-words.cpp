50766b94585f3394292c08cadc2c96cb31112612
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-03 15:23:00 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/단어 정렬/sorting-words.cpp
+++ 240203/단어 정렬/sorting-words.cpp
@@ -3,16 +3,19 @@
 #include <algorithm>
 
 using namespace std;
+
+// 변수 선언
 int n;
 vector<string> arr;
 int main() {
+    // 입력:
     cin >> n;
     for(int i=0; i<n; i++){
         string str;
         cin >> str;
         arr.push_back(str);
     }
-
+    // 문자열 사전순 정렬
     sort(arr.begin(), arr.end());
 
     for(int i=0; i<n; i++){
