2169bf7110bad983004b6f085016a588950bd5ee
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-03 15:24:28 +0900

    Title: Top K 숫자 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 1000
+using namespace std;
+
+int k, n;
+int arr[MAX_N];
+int main() {
+    cin >> n >> k;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    sort(arr, arr+n);
+    cout << arr[k-1];
+    return 0;
+}
