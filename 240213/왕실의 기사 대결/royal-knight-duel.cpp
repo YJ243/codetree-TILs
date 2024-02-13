d682c9dc6677a6b179ff6e8fc62e08b542955e1e
yj____ <60027712+YJ243@users.noreply.github.com>
2024-02-13 17:34:46 +0900

    Title: 왕실의 기사 대결, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240213/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240213/왕실의 기사 대결/royal-knight-duel.cpp
@@ -1,121 +1,147 @@
 #include <iostream>
 #include <queue>
+#define MAX_L 40
+#define MAX_N 30
 using namespace std;
-int N, M, Q;  // 지도의 크기, 기사의 수, 명령의 수
-int original_map[55][55];  // 벽, 함정 등을 표현하는 지도
-int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-struct Knight {
-    int idx;            // 이 기사의 번호
-    int r, c, h, w, k;  // 위치, 크기, 체력
-    int damage;         // 받은 총 데미지
-    void fill(int map[][55]) {
-        if (k <= 0)
-            return;
-        // 기사가 차지하는 공간에 기사의 번호 채우기
-        for (int i = r; i < r + h; i++) {
-            for (int j = c; j < c + w; j++) {
+int l, n, q;        // l: 체스판 크기, n: 기사들 명수, q: 왕 명령 개수    
+int original_map[MAX_L][MAX_L];
+int dirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};    // 위, 오른쪽, 아래쪽, 왼쪽
+struct Knight{
+    int idx;    // knight 번호
+    int r, c;   // knight 위치
+    int h, w;   // knight 영역 크기
+    int k;      // knight 체력
+    int damage; // 입은 데미지
+
+    void fill(int map[MAX_L][MAX_L]){
+        for(int i=r; i<r+h; i++)
+            for(int j=c; j<c+w; j++)
                 map[i][j] = idx;
-            }
-        }
     }
-    void move(int d) {
-        // 기사를 d번 방향으로 한 칸 이동시키기
-        r += dirs[d][0];
-        c += dirs[d][1];
-    }
-    void hurt() {
-        for (int i = r; i < r + h; i++) {
-            for (int j = c; j < c + w; j++) {
-                if (original_map[i][j] == 1) {
-                    k --;
-                    damage ++;
-                }
-            }
-        }
-    }
-}knights[40];
-void input() {
-    cin >> N >> M >> Q;
-    for (int i=1;i<=N;i++) {
-        for (int j=1;j<=N;j++){
+
+
+}knights[MAX_N+1];
+
+void Input(){
+    cin >> l >> n >> q;
+    for(int i=0; i<l; i++){
+        for(int j=0; j<l; j++){
             cin >> original_map[i][j];
         }
     }
-    for (int i=1;i<=M;i++){
+    for(int i=1; i<=n; i++){
         int r, c, h, w, k;
         cin >> r >> c >> h >> w >> k;
-        knights[i] = {i, r, c, h, w, k, 0};
+        knights[i] = {i, r-1, c-1, h, w, k, 0}; // knight 정보 넣기
     }
 }
-void move(int k, int d) {  // k번 기사를 d번 방향으로 이동시켜주는 함수. 벽을 만나는 이동이라면 무효 처리
-    int knights_map[55][55] = {0, };
-    // 각 기사가 차지하는 공간을 새로이 그려냄
-    for (int i = 1; i <= M; i++) {
-        knights[i].fill(knights_map);
+
+bool InRange(int x, int y){
+    return 0 <= x && x < l && 0 <= y && y < l;
+}
+
+void do_Command(int start_id, int d){   // id번 기사를 d번 방향으로 연쇄적으로 미는 함수
+
+    // 한 번 명령을 수행할 때마다 새로 기사들 맵 설정
+    int knight_map[MAX_L][MAX_L] = {0,};
+    for(int i=1; i<=n; i++){
+        knights[i].fill(knight_map);    // 기사들 번호로 채우기
     }
-    // 이동할 친구들을 찾자
-    bool moving[55] = {0, };  // moving[i] := i번 기사가 이동 여부
-    queue<int> Q;  // 이동하는 친구들을 담는 큐
-    Q.push(k);
-    moving[k] = true;
-    while (!Q.empty()) {
-        int k = Q.front(); Q.pop();
-        
-        for (int i = knights[k].r; i < knights[k].r + knights[k].h; i++) {
-            for (int j = knights[k].c; j < knights[k].c + knights[k].w; j++) {
-                // (r, c)는 기사에 포함된 격자 중 하나
+
+
+    bool moving[MAX_N+1] = {false,};
+    queue<int> q;   // start_id를 포함하여 밀리는 기사를 담는 큐
+    moving[start_id] = true;
+    q.push(start_id);
+
+    while(!q.empty()){
+        int cur_id = q.front(); q.pop();
+        for(int i = knights[cur_id].r; i < knights[cur_id].r + knights[cur_id].h; i++){
+            for(int j = knights[cur_id].c; j < knights[cur_id].c + knights[cur_id].w; j++){
+                // (i,j)는 cur_id 기사의 영역
                 int r = i, c = j;
-                for (;;) {
-                    r += dirs[d][0];
-                    c += dirs[d][1];
-                    if (r < 1 || c < 1 || r > N || c > N)  // 격자를 벗어나도 벽임
+                while(true){
+                    r += dirs[d][0]; c += dirs[d][1];
+
+                    if(!InRange(r,c)){  // 만약 범위를 벗어나면 벽이므로 움직이지 못함
                         return;
-                    if (knights_map[r][c] == 0){  // d 번 방향으로 이동했더니, 기사가 존재하지 않음
-                        if (original_map[r][c] == 2) {  // 근데 벽이 존재한다.
-                            return;
-                        }
-                        break;
                     }
-                    // 기사가 존재하는 칸을 의미하므로, 이동 여부 갱신
-                    if (!moving[knights_map[r][c]]) {  // 새롭게 이동하는 친구를 찾음!
-                        moving[knights_map[r][c]] = true;
-                        Q.push(knights_map[r][c]);
+                    if(knight_map[r][c] == 0){  // 만약 빈 곳이라면
+                        if(original_map[r][c] == 2){
+                            // 그런데 만약 그곳이 벽이라면
+                            return;     // 더이상 움직이지 못함
+                        }
+                        // 벽이 아니라면
+                        break; // 해당 방향으로 이제 갈 수 있으니 탐색 마침
+                    }
+                    // 이제 범위안에 있으면서 기사가 있는 곳임
+                    if(!moving[knight_map[r][c]]){
+                        // 처음 만난 기사라면
+
+                        moving[knight_map[r][c]] = true;
+                        q.push(knight_map[r][c]);   // 큐에 넣어주기
                     }
                 }
             }
         }
     }
-    // 이동해야 하는 친구들 이동 시키고, 체력 감소
-    for (int i = 1;i <= M; i++){
-        if (!moving[i]) continue;  // 이동하지 않는 친구는 무시하기
-        knights[i].move(d);
-        if (i != k) {  // 명령을 받지 않은 기사가 이동
-            knights[i].hurt();
+
+    // 이제 데미지 입히기
+    // 1. 먼저 이동시키기
+    for(int i=1; i<=n; i++){
+        if(moving[i]){
+            knights[i].r += dirs[d][0];
+            knights[i].c += dirs[d][1];
+        }
+    }
+
+    // 2. 기사들 모두 밀린 이후에 데미지 입음
+    for(int i=1; i<=n; i++){
+        if(i == start_id) continue;     // 처음 명령 받은 기사는 데미지를 입지 않음
+        if(moving[i]){
+            for(int x=knights[i].r; x < knights[i].r + knights[i].h; x++){
+                for(int y=knights[i].c; y < knights[i].c + knights[i].w; y++){
+
+                    if(original_map[x][y] == 1){
+                        // 만약 함정이 있다면
+                        knights[i].k--;         // 체력 감소
+                        knights[i].damage++;    // 데미지 증가 
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+void Progress(){
+    while(q--){
+        int i, d;
+        cin >> i >> d;
+        // 체스판에 있는 기사는 명령을 수행
+        if(knights[i].k > knights[i].damage){
+            do_Command(i, d);
         }
     }
 }
-void pro() {
-    while (Q--){
-        // 1. 입력 받기 (기사 k 가 d 번 방향으로 이동한다.)
-        int k, d;
-        cin >> k >> d;
-        // 2. 기사의 생존 여부 확인
-        if (knights[k].k <= 0){
-            continue;
-        }
-        // 3. 기사를 이동시키는 명령 수행
-        move(k, d);
-    }
-    int ans = 0;
-    for (int i=1;i<=M;i++){
-        if (knights[i].k >= 1){
-            ans += knights[i].damage;
+
+void Output(){
+    int result = 0;
+    for(int i=1; i<=n; i++){
+        if(knights[i].k > knights[i].damage){
+            result += knights[i].damage;
         }
     }
-    cout << ans;
+    cout << result;
 }
 int main() {
-    input();
-    pro();
+    // 입력받기
+    Input();
+
+    // 왕의 명령 수행하기
+    Progress();
+
+    // 출력하기
+    Output();
     return 0;
 }
