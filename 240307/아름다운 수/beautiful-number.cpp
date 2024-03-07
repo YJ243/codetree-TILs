8a5b27046604c127d3949054872bb914c423615d
yj____ <60027712+YJ243@users.noreply.github.com>
2024-03-07 17:54:30 +0900

    Title: 아름다운 수, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 240307/아름다운 수/beautiful-number.cpp
+++ 240307/아름다운 수/beautiful-number.cpp
@@ -50,3 +50,56 @@ int main() {
     cout << ans;
     return 0;
 }
+
+/*
+ *해설 풀이
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+int ans;
+vector<int> seq;
+
+bool IsBeautiful(){
+    // 연달아 같은 숫자가 나오는 시작 위치를 잡는다
+    for(int i=0; i<n; i += seq[i]){
+        // 만약 연속하여 해당 숫자만큼 나올 수 없다면 아름다운 수가 아니다
+        if(i + seq[i] - 1 >= n)
+            return false;
+
+        // 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인한다.
+        // 하나라도 다른 숫자가 있다면 아름다운 수가 아니다
+        for(int j=i; j < i + seq[i]; j++)
+            if(seq[j] != seq[i])
+                return false;
+    }
+    return true;
+}
+
+void CountBeautifulSeq(int cnt){
+    if(cnt == n){
+        if(IsBeautiful())
+            ans++;
+        return;
+    }
+
+    for(int i=1; i<=4; i++){
+        seq.push_back(i);
+        CountBeautifulSeq(cnt+1);
+        seq.pop_back();
+    }
+}
+
+int main(){
+    cin >> n;
+
+    CountBeautifulSeq(0);
+
+    cout << ans;
+    return 0;
+}
+*/
