305ef3e2de3d64dcca5a4aa8498a75d6c7718291
yj____ <60027712+YJ243@users.noreply.github.com>
2024-03-20 13:44:04 +0900

    Title: 아름다운 수, Time: 17ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/아름다운 수/beautiful-number.cpp
+++ 240320/아름다운 수/beautiful-number.cpp
@@ -7,13 +7,15 @@ vector<int> numbers;
 int ans;
 
 bool Isbeautiful(){
+    // 연달아 같은 숫자가 나오는 시작 위치를 잡는다.
     for(int i=0; i<(int)numbers.size(); i += numbers[i]){
+        // 만약 연속하여 해당 숫자만큼 나올 수 없다면
         if(numbers[i] + i-1 >= n)   // 만약 아름다운 수를 만들 길이가 모자른다면 false
             return false;
         int val = numbers[i];
         for(int j=i; j<i+val; j++){
             if(val != numbers[j])   // 만약 해당 숫자만큼 연달아 같은 숫자가 나오지 않는다면
-                return false;
+                return false;       // 하나라도 다른 숫자가 있다면 아름다운 수가 아니다
         }        
     }
     return true;
