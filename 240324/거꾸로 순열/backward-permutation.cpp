062574262f3411aaf10fe6ae10511d02a0b6737d
yj____ <60027712+YJ243@users.noreply.github.com>
2024-03-24 21:12:05 +0900

    Title: 거꾸로 순열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240324/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#define MAX_N 8
+
+using namespace std;
+int n;
+
+vector<int> selected;
+bool visited[MAX_N];
+
+void Print(){
+    for(int i=0; i<(int)selected.size(); i++)
+        cout << selected[i] << ' ';
+    cout << '\n';
+}
+
+void Choose(int idx){
+    if(idx == n){
+        Print();
+        return;
+    }
+
+    for(int i=3; i >= 1; i--){
+        if(visited[i])
+            continue;
+
+        visited[i] = true;
+        selected.push_back(i);
+        Choose(idx+1);
+        selected.pop_back();
+        visited[i] = false;
+    }
+}
+
+int main() {
+    cin >> n;
+    Choose(0);      // 0번째 숫자를 선택하는 함수
+    return 0;
+}
