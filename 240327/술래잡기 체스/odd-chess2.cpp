59e9bb8a6c3243346635d8896ef753b54e508286
yj____ <60027712+YJ243@users.noreply.github.com>
2024-03-27 18:44:42 +0900

    Title: 술래잡기 체스, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240327/술래잡기 체스/odd-chess2.cpp
+++ 240327/술래잡기 체스/odd-chess2.cpp
@@ -30,10 +30,10 @@ bool InRange(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-bool IsFinish(int x, int y, int d){        // 술래가 이동할 수 있는 곳에 도둑말이 더이상 존재하지 않는지 확인하기
+bool IsFinish(){        // 술래가 이동할 수 있는 곳에 도둑말이 더이상 존재하지 않는지 확인하기
     bool IsExist = false;
-    for(int i=1; i<=n; i++){
-        int nx = x + dirs[d][0]*i, ny = y + dirs[d][1]*i;
+    for(int i=1; i<n; i++){
+        int nx = sx + dirs[sd][0], ny = sy + dirs[sd][1];
         if(InRange(nx, ny) && grid[nx][ny] != NOT_EXIST){
             IsExist = true;
         }
@@ -41,40 +41,42 @@ bool IsFinish(int x, int y, int d){        // 술래가 이동할 수 있는 곳
     return !IsExist;
 }
 
-void CatchTheif(int x, int y){
-    //int priorX = sx, priorY = sy, priorD = sd;
-    //int priorS = curr;
-    curr += grid[x][y].first;       // 점수 증가
-    sd = grid[x][y].second;         // 술래 방향 바꿔주기
+tuple<int, int, int, int> CatchTheif(int x, int y){
+    int priorX = sx, priorY = sy, priorD = sd;
+    int priorS = curr;
+    curr += grid[x][y].first;        // 점수 증가
+    sd = grid[x][y].second;
     sx = x, sy = y;                 // 술래 위치 바꿔주기
     grid[x][y] = NOT_EXIST;         // 해당 칸에 아무도 존재하지 않는다고 표시
-    //return make_tuple(priorX, priorY, priorD, priorS);
+    return make_tuple(priorX, priorY, priorD, priorS);
 }
 
 void MoveThief(){
-    for(int num=1; num<=16; num++){                     // 1번부터 16번까지 번호를 차례대로 보기
-        bool IsMoved = false, cannotMovable = false;    // 해당 번호를 가진 도둑이 움직였는지, 움직일 수 없는지 확인하기
+    for(int num=1; num<=16; num++){
+        bool IsMoved = false, cannotMovable = false;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                int curN = grid[i][j].first, curD = grid[i][j].second;      // 현재 보고 있는 칸의 번호와 방향
-                if(curN == num){                                            // 만약 번호가 같다면
+                int curN = grid[i][j].first, curD = grid[i][j].second;
+                if(curN == num){
+                    //cout << curN << ' ' << i << ' ' << j << ' ' << curD << '\n';
                     // 이제 (i,j)에 있는 도둑 옮기기
-                    int nx = i + dirs[curD][0], ny = j + dirs[curD][1];     // 다음에 이동할 위치
-                    int startX = nx, startY = ny;                           // 위를 처음 이동할 위치 좌표로 받아놓기
-                    if(!InRange(nx, ny) || (nx == sx && ny == sy)){         // 만약 범위를 벗어나거나 술래가 있다면
-                        for(int d=1; d<=8; d++){                            //  이동할 수 있을 때까지 45도 반시계 방향으로 회전하기
+                    int nx = i + dirs[curD][0], ny = j + dirs[curD][1];
+                    int startX = nx, startY = ny;
+                    if(!InRange(nx, ny) || (nx == sx && ny == sy)){   // 만약 범위를 벗어나거나 술래가 있다면
+                        for(int d=1; d<=8; d++){    //  이동할 수 있을 때까지 45도 반시계 방향으로 회전하기
                             nx = i + dirs[(curD+d)%MAX_DIR][0], ny = j + dirs[(curD+d)%MAX_DIR][1];
-                            if(InRange(nx,ny) && (nx != sx || ny != sy)){   // 이동 가능한 곳을 찾았다면 방향 저장하고 나가기
+                            if(InRange(nx,ny) && (nx != sx || ny != sy)){
                                 curD = (curD + d) % MAX_DIR;
                                 break;
                             }
                         }
-                        if(nx == startX && ny == startY){       // 만약 다시 처음으로 돌아왔다면 이동불가능하다는 의미
+                        if(nx == startX && ny == startY){
                             cannotMovable = true;
-                            break;       // 이동할 수 없다면 움직이지 않기
+                            continue;       // 이동할 수 없다면 움직이지 않기
                         }
                     }
                     // 이제 (i,j)에서 (nx,ny)로 바꾸기
+                    //cout << i << ' ' << j << "에서 " << nx << ' ' << ny << ' ' << curD << "로 바꾸기\n";
                     int movingNum = grid[nx][ny].first, movingDir = grid[nx][ny].second;
                     grid[nx][ny] = make_pair(num, curD);
                     grid[i][j] = make_pair(movingNum, movingDir);
@@ -86,44 +88,64 @@ void MoveThief(){
             if(IsMoved || cannotMovable)
                 break;
         }
+        /*
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cout << grid[i][j].first << ' ';
+            }
+            cout << '\n';
+        }
+        cout << '\n';
+        */
     }
 }
 
-void ChooseNext(int x, int y, int d, int score){
-    if(IsFinish(x, y, d)){
+void ChooseNext(int cnt){
+    if(IsFinish()){
         // 만약 끝났다면 점수 업데이트하기
-        ans = max(ans, score);
+        ans = max(ans, curr);
         return;
     }
-
-    for(int i=1; i<=n; i++){
+    for(int i=1; i<n; i++){
         //cout << cnt << "번째: " << '\n';
-        int nx = x + dirs[d][0]*i, ny = y + dirs[d][1]*i;
-        // 만약 술래가 이동할 곳이 범위 안이고, 도둑이 존재한다면
-        if(!InRange(nx, ny) || grid[nx][ny] == NOT_EXIST)
-            continue;
+        int nx = sx + dirs[sd][0]*i, ny = sy + dirs[sd][1]*i;
+        if(InRange(nx, ny) && grid[nx][ny] != NOT_EXIST){
+            // 만약 술래가 이동할 곳이 범위 안이고, 도둑이 존재한다면
+            //cout << "술래 원래 위치: " << sx << ' ' << sy << '\n';
+            pair<int, int> tmp[MAX_N][MAX_N];
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
+                    tmp[i][j] = grid[i][j];
+            int priorX, priorY, priorD, priorS;
+            tie(priorX, priorY, priorD, priorS) = CatchTheif(nx, ny);
+            //cout << "술래 이동한 위치: " << sx << ' ' << sy << '\n';
+            MoveThief();
+            
+            ChooseNext(cnt+1);
 
-        pair<int, int> tmp[MAX_N][MAX_N];
-
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-                tmp[i][j] = grid[i][j];
-
-        int extra_score, next_dir;
-        tie(extra_score, next_dir) = grid[nx][ny];
-        CatchTheif(nx, ny);
-  
-        // 2. 도둑 이동하기
-        MoveThief();
-        // 3. 술래가 이동할 다음 위치 찾기
-        ChooseNext(nx, ny, next_dir, score + extra_score);
-        // 4. 도둑 움직이기 이전으로 되돌리기
-
-        // catchTheif도 되돌리기
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-                grid[i][j] = tmp[i][j];
+            // 도둑 움직이기 이전으로 되돌리기
+            sx = priorX, sy = priorY, sd = priorD, curr = priorS;
+            // catchTheif도 되돌리기
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
+                    grid[i][j] = tmp[i][j];
+            //cout << "ㅁㅇㄻㄴㅇㄹ" << cnt << '\n';
+        }
     }
+}
+
+int Simulate(){
+    // Step 1. 먼저 번호가 작은 순서대로 이동시키기
+    MoveThief();
+    ans = curr;
+    // Step 2. 이후 술래말 이동하기
+    if(IsFinish())
+        return 0;
+    
+    // backtracking으로 술래가 이동할 곳 정하기
+    ChooseNext(1);      // 술래가 (0,0)에서 1번째 이동할 곳을 정하러 가기
+
+    return 0;
 }
 
 int main() {
@@ -132,13 +154,9 @@ int main() {
 
     // 시뮬레이션 시작
     // 먼저 (0,0)에 있는 도둑을 잡고 시작
-    int init_score, init_dir;
-    tie(init_score, init_dir) = grid[0][0];
-
     CatchTheif(0,0);        
-    MoveThief();
 
-    ChooseNext(0, 0, init_dir, init_score);      // 술래가 (0,0)에서 1번째 이동할 곳을 정하러 가기
+    Simulate();
 
     cout << ans;
     return 0;
