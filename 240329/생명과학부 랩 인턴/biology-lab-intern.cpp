5a9f476221b709086d0fa6c62f9c0600a5e72a41
yj____ <60027712+YJ243@users.noreply.github.com>
2024-03-29 17:17:16 +0900

    Title: 생명과학부 랩 인턴, Time: 54ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/생명과학부 랩 인턴/biology-lab-intern.cpp
+++ 240329/생명과학부 랩 인턴/biology-lab-intern.cpp
@@ -1,115 +1,137 @@
 #include <iostream>
 #include <tuple>
-#define MAX_NM 100
-#define EMPTY make_tuple(-1, -1, -1)
+
+#define MAX_NUM 100
+#define DIR_NUM 4
+#define BLANK make_tuple(-1, -1, -1)
+
 using namespace std;
 
-int n, m, k;            // n, m: 격자판 크기, k: 곰팡이 수
-tuple<int, int, int> grid[MAX_NM][MAX_NM];        // grid[i][j]: (i,j)에 있는 곰팡이 크기, 방향, 속력  
-tuple<int, int, int> next_grid[MAX_NM][MAX_NM];   // 곰팡이 이동한 다음 격자판 
-int dirs[4][2] = {{-1,0},{1,0},{0,1},{0,-1}};    // 위, 아래, 오, 왼
-int collected_mold;
-void Init(){
-    cin >> n >> m >> k;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            grid[i][j] = EMPTY;
-        }
-    }
-    for(int i=0; i<k; i++){
-        int x, y, s, d, b;      // (x,y) 곰팡이 위치, s: 1초동안 움직이는 거리, d: 이동 방향, b: 곰팡이 크기
-        cin >> x >> y >> s >> d >> b;
-        grid[x-1][y-1] = make_tuple(b, d-1, s);
-    }
+int n, m, k;
 
-}
+tuple<int, int, int> mold[MAX_NUM][MAX_NUM];
+tuple<int, int, int> next_mold[MAX_NUM][MAX_NUM];
 
-void CollectMold(int col_idx){
-    for(int i=0; i<n; i++){     // 해당 열을 위에서 아래로 내려가며 탐색 진행
-        if(grid[i][col_idx] != EMPTY){      // 만약 곰팡이를 만났다면
-            int mold_size;
-            tie(mold_size, ignore, ignore) = grid[i][col_idx];
-            collected_mold += mold_size;
-            grid[i][col_idx] = EMPTY;
-            return;
-        }
-    }
-}
+int ans;
 
-void Initialize_next_grid(){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++)
-            next_grid[i][j] = EMPTY;
-    }
-}
-
-bool InRange(int x, int y){
+bool InRange(int x, int y) {
     return 0 <= x && x < n && 0 <= y && y < m;
 }
 
-void MoveMold(){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(grid[i][j] == EMPTY) continue;
-            int size, dir, len;
-            tie(size, dir, len) = grid[i][j];
-            int x = i, y = j;
-            for(int k=0; k<len; k++){
-                int nx = x + dirs[dir][0], ny = y + dirs[dir][1];
-                if(!InRange(nx, ny)){
-                    dir = (dir % 2 == 0) ? dir+1: dir-1;
-                    nx = x + dirs[dir][0], ny = y + dirs[dir][1];
-                }
-                x = nx, y = ny;
-            }
-            if(next_grid[x][y] == EMPTY){
-                next_grid[x][y] = make_tuple(size, dir, len);
-            }
-            else{
-                int origin_size;
-                tie(origin_size, ignore, ignore) = next_grid[x][y];
-                if(origin_size > size)
-                    continue;
-                else
-                    next_grid[x][y] = make_tuple(size, dir, len);
-            }
+void Collect(int col) {
+    for(int row = 0; row < n; row++)
+        if(mold[row][col] != BLANK) {
+            int mold_size;
+            tie(mold_size, ignore, ignore) = mold[row][col];
+            
+            ans += mold_size;
+            mold[row][col] = BLANK;
+            break;
         }
-    }
 }
 
-void Simulate(int col_idx){
-    // Step 1. 곰팡이 채취하기
-    CollectMold(col_idx);
-
-    // Step 2. 곰팡이 이동 시작
-    Initialize_next_grid();
-
-    MoveMold();
-
-    // Step 3. next_grid에서 grid로 바꿔주기
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            grid[i][j] = next_grid[i][j];
+tuple<int, int, int> GetNextPos(int x, int y, int dist, int move_dir) {
+    // 문제에서 주어진 순서인 위, 아래, 오른쪽, 왼쪽 순으로 적어줍니다. 
+    int dx[DIR_NUM] = {-1, 1, 0, 0};
+    int dy[DIR_NUM] = {0, 0, 1, -1};
+    
+    // dist번 한 칸씩 이동하면 됩니다.
+    while(dist--) {
+        int next_x = x + dx[move_dir], next_y = y + dy[move_dir];
+        // 현재 방향으로 이동했다 했을 때
+        // 만약 격자를 벗어나지 않는다면, 그대로 이동합니다.
+        if(InRange(next_x, next_y))
+            x = next_x, y = next_y;
+        // 만약 격자를 벗어나게 된다면
+        // 방향을 반대로 바꾸고 한 칸 이동하면 됩니다.
+        else {
+            move_dir = (move_dir % 2 == 0) ? (move_dir + 1) : (move_dir - 1);
+            x = x + dx[move_dir]; y = y + dy[move_dir];
         }
     }
+    
+    return make_tuple(x, y, move_dir);
+}
 
+// (x, y) 위치에 있는 곰팡이를 이동시킵니다.
+void Move(int x, int y) {
+    int mold_size, dist, move_dir;
+    tie(mold_size, dist, move_dir) = mold[x][y];
+    
+    int next_x, next_y, next_dir;
+    tie(next_x, next_y, next_dir) = GetNextPos(x, y, dist, move_dir);
+    
+    tuple<int, int, int> new_mold = make_tuple(mold_size, dist, next_dir);
+    
+    // 현재 곰팡이의 크기가 해당 위치에 있던 것 보다 더 큰 경우에만
+    // 곰팡이 정보를 적어줍니다.
+    // 그렇지 않은 경우라면 충돌시 사라지게 될 곰팡이이므로
+    // 무시하면 됩니다.
+    if(new_mold > next_mold[next_x][next_y])
+        next_mold[next_x][next_y] = new_mold;
+}
 
+void MoveAll() {
+    // next_mold 값을 전부 빈칸으로 초기화합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            next_mold[i][j] = BLANK;
+    
+    // 곰팡이를 한번씩 이동시킵니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            if(mold[i][j] != BLANK)
+                Move(i, j);
+    
+    // next_mold 값을 mold에 옮겨줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            mold[i][j] = next_mold[i][j];
+}
+
+void Simulate(int col) {
+    // 해당 열에 있는 곰팡이를 채취합니다.
+    Collect(col);
+    
+    // 곰팡이들을 전부 움직입니다.
+    MoveAll();
 }
 
 int main() {
-    // 입력 받기:
-    Init();
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            int b, d, s;
-            tie(b, d, s) = grid[i][j];
-        }
-    }
-    // 시뮬레이션
-    for(int i=0; i<m; i++){ // m개의 열을 차례대로 보면서 검사하기
-        Simulate(i);
+    cin >> n >> m >> k;
+    
+    // 처음 맵 초기 상태를 전부 빈 공간으로 초기화합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            mold[i][j] = BLANK;
+    
+    for(int i = 0; i < k; i++) {
+        int x, y, s, d, b;
+        cin >> x >> y >> s >> d >> b;
+        
+        // 위, 아래 방향으로 움직이는 경우
+        // 2n - 2번 움직이면 다시 제자리로 돌아오게 되므로
+        // 움직여야 할 거리를 2n - 2로 나눴을 때의 나머지 만큼만
+        // 움직이게 하면 최적화가 가능합니다.
+        if(d <= 2)
+            s %= (2 * n - 2);
+        // 왼쪽, 오른쪽 방향으로 움직이는 경우
+        // 2m - 2번 움직이면 다시 제자리로 돌아오게 되므로
+        // 움직여야 할 거리를 2m - 2로 나눴을 때의 나머지 만큼만
+        // 움직이게 하면 최적화가 가능합니다.
+        else
+            s %= (2 * m - 2);
+        
+        // tuple에 넣을 때
+        // 곰팡이 크기 정보를 먼저 넣어, 후에 곰팡이끼리 충돌이 일어날 경우
+        // 크기부터 비교하여 최대인 곰팡이를 쉽게 판단할 수 있도록 합니다.
+        mold[x - 1][y - 1] = make_tuple(b, s, d - 1);
     }
     
-    cout << collected_mold;
+    // 한 칸씩 이동하면서 곰팡이를 채취합니다.
+    for(int col = 0; col < m; col++)
+        Simulate(col);
+    
+    cout << ans;
     return 0;
 }
