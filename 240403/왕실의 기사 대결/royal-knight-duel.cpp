0750ee62d74b5e799902b21377454c8a1c5a420d
yj____ <60027712+YJ243@users.noreply.github.com>
2024-04-03 02:33:59 +0900

    Title: 왕실의 기사 대결, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240403/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240403/왕실의 기사 대결/royal-knight-duel.cpp
@@ -1,176 +1,144 @@
 #include <iostream>
 #include <queue>
 #include <tuple>
-#define MAX_L 50
-#define MAX_N 30
+#define MAX_L 55
+#define MAX_N 40
 
 using namespace std;
 
-int L, N, Q;        // L: 체스판 크기, N: 기사들 수, Q: 명령 수
-int grid[MAX_L][MAX_L];
-tuple<int, int, int, int, int, int> knights[MAX_N+1];    // 기사들 정보(r, c, h, w, k, damage);
-bool IsMoving[MAX_N+1];
-bool IsDead[MAX_N+1];
-queue<int> q;
-int dirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+int L, N, Q;                                                // L: 체스판 크기, N: 기사들 수, Q: 명령 수
+int grid[MAX_L][MAX_L];                                     // 0: 빈칸, 1: 함정, 2: 벽
+int dirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};       // 상, 우, 하, 좌
+tuple<int, int, int, int, int, int> knights[MAX_N+1];       // 기사들 정보(r, c, h, w, k, damage)
+                                    // 탐색을 위한 큐
 
-void Input(){               // 입력을 받는 함수
-    cin >> L >> N >> Q;
-    for(int i=0; i<L; i++){
+void Input(){                       // 입력을 받는 함수
+    cin >> L >> N >> Q;             // L: 체스판 크기, N: 기사들 수, Q: 명령 수
+    for(int i=0; i<L; i++){         // 체스판 입력받기
         for(int j=0; j<L; j++){
             cin >> grid[i][j];
         }
     }
 
-    for(int i=1; i<=N; i++){
-        int r, c, h, w, k;
+    for(int i=1; i<=N; i++){            // N명의 기사들 입력받기
+        int r, c, h, w, k;              // 기사들 정보(r, c, h, w, k, damage)
         cin >> r >> c >> h >> w >> k;
-        r--; c--;
+        r--; c--;                       // (0,0)부터 시작하도록 함
         knights[i] = make_tuple(r, c, h, w, k, 0);
-        IsDead[i] = false;
     }
-    //for(int i=1; i<=N; i++)
-
 }
 
-bool InRange(int x, int y){
+bool InRange(int x, int y){         // 범위 안에 있는지 확인하는 함수
     return 0 <= x && x < L && 0 <= y && y < L;
 }
 
 void Move(int id, int d){
-    // 1. 먼저 grid에 기사들 표시하기 & IsMoving 초기화
-    int tmp_grid[L][L] = {0,};
+    // 1. 먼저 tmp_grid에 기사들 표시하기 & IsMoving 초기화
+    bool IsMoving[MAX_N+1];                                     // 현재 턴에 움직이고 있는지 확인
+
+    for(int i=1; i<=N; i++)
+        IsMoving[i] = false;
+    
+    int tmp_grid[MAX_L][MAX_L] = {0,};
     for(int i=0; i<L; i++){
         for(int j=0; j<L; j++){
             tmp_grid[i][j] = 0;
         }
     }
     for(int i=1; i<=N; i++){
-        if(!IsDead[i]){
-            int r, c, h, w;
-            tie(r, c, h, w, ignore, ignore) = knights[i];
-            for(int x=r; x<r+h; x++){
-                for(int y=c; y<c+w; y++){
-                    tmp_grid[x][y] = i;
-                }
+        int r, c, h, w, k, damage;
+        tie(r, c, h, w, k, damage) = knights[i];
+        if(k <= 0) continue;              // 살아있는 기사들에 한해서 격자에다가 옮기기
+        for(int x=r; x<r+h; x++){           // 기사들 구성하는 칸에 대해서
+            for(int y=c; y<c+w; y++){
+                tmp_grid[x][y] = i;         // 번호 적기
             }
         }
-        IsMoving[i] = false;
+        
+        IsMoving[i] = false;                    // 현재 움직이는지 여부 초기화하기
     }
-
+    /*
+    for(int i=0; i<L; i++){
+        for(int j=0; j<L; j++){
+            cout << tmp_grid[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    */
     // 2. id와 연결되어 있는 기사 찾기
-    q.push(id);
-    IsMoving[id] = true;
-    while(!q.empty()){
-        int curr = q.front();
+    queue<int> q;   
+    q.push(id);                                 // 현재 명령을 받은 id번 기사 큐에 넣기
+    IsMoving[id] = true;                        // 움직이고 있다고 표시하기
+    while(!q.empty()){                      // 큐가 빌 때까지 진행
+        int curr = q.front();               // 제일 앞에 있는 기사 번호 가져오기
         q.pop();
         
         int r, c, h, w, k, damage;
         tie(r, c, h, w, k, damage) = knights[curr];
-        //cout << r << ' ' << c << ' ' << h << ' ' << w << ' ' << k << '\n';
+
         for(int x=r; x<r+h; x++){
             for(int y=c; y<c+w; y++){
-                //cout << x << ' ' << y << '\n';
                 int a = x, b = y;       // (a,b)는 curr 기사의 한 부분
-                //cout << a << ' ' << b << "는 " << curr << " 일부" << '\n';
                 for(;;){
                     a += dirs[d][0]; b += dirs[d][1];
-                    //cout << a << ' ' << b << "보고 있음" << '\n';
-                    if(!InRange(a, b))
-                        return;         // 만약 범위를 벗어나면 
-                    if(tmp_grid[a][b] == 0){    // 빈칸임
-                        if(grid[a][b] == 2){
-                            // 그런데 벽을 만났다면
+                    if(!InRange(a, b))  // 만약 범위를 벗어나면 움직일 수 없으니 바로 리턴
+                        return;         
+                    if(tmp_grid[a][b] == 0){    // 빈칸 만남
+                        if(grid[a][b] == 2){    // 그런데 벽을 만났다면
                             return;             // 움직이지 못하니 리턴
                         }
-                        //cout << curr << "에서 " << a << ' ' << b << "가 빈칸임" << '\n';
+                        // 벽이 아닌 빈칸을 만났으면 이제 탐색 중단해도 됨
                         break;
                     }   
-                    
-                    if(!IsMoving[tmp_grid[a][b]]){
-                        IsMoving[tmp_grid[a][b]] = true;
-                        q.push(tmp_grid[a][b]);
-                    
+                    if(!IsMoving[tmp_grid[a][b]]){          // 만약 새로운 기사를 만났다면
+                        IsMoving[tmp_grid[a][b]] = true;    // 움직여야 한다고 표시하기
+                        q.push(tmp_grid[a][b]);             // 큐에 번호 넣기
                     }
                 }
             }
         }
     }
-
-    for(int i=1; i<=N; i++){
-
-        if(!IsMoving[i]) continue;
+    // << id << "번 기사 명령 받음" << '\n';
+    for(int i=1; i<=N; i++){        // N명의 기사에 대해
+        //cout << i << "번 움직임: " << IsMoving[i] << '\n';
+        if(!IsMoving[i]) continue;  // 움직이는 기사가 아니라면 넘어가기
        
         int r, c, h, w, k, damage;
         tie(r, c, h, w, k, damage) = knights[i];
-        r += dirs[d][0]; c += dirs[d][1];
-        knights[i] = make_tuple(r, c, h, w, k, damage);
+        r += dirs[d][0]; c += dirs[d][1];               // d방향으로 1만큼 이동시키기
         if(i != id){
             for(int x=r; x<r+h; x++){
                 for(int y=c; y<c+w; y++){
-                    if(grid[x][y] == 1){
-                        damage++;
-                        k--;
+                    if(grid[x][y] == 1){                // 만약 함정이라면
+                        damage++;                       // 데미지 증가
+                        k--;                            // 목숨 감소
                     }
                 }
             }
         }
-        if(k <= 0)
-            IsDead[i] = true;
-        knights[i] = make_tuple(r, c, h, w, k, damage);
+       // cout << i << ' ' << r << ' ' << c << ' ' << h << ' ' << w << ' ' << k << ' ' << damage << '\n';
+        knights[i] = make_tuple(r, c, h, w, k, damage); // 다시 정보 저장하기
     }
-    for(int i=1; i<=N; i++){
-        int r, c, h, w, k, damage;
-        tie(r, c, h, w, k, damage) = knights[i];
-    }
-
 }
 
 void Pro(){
     // Q번의 왕의 명령 수행하기
-    for(int i=0; i<Q; i++){
-        //cout << i << "번째 명령: " << '\n';
+    while(Q--){
         int id, d;
         cin >> id >> d;
-        if(!IsDead[id])     // 아직 사라지지 않았다면
-            Move(id, d);    // 명령 수행하기
+        int r, c, h, w, k, damage;
+        tie(r, c, h, w, k, damage) = knights[id];
+        if(k <= 0) continue;
+        Move(id, d);    // 명령 수행하기
     }
 }
 
 void Output(){
-
-
-int tmp_grid[L][L] = {0,};
-    for(int i=0; i<L; i++){
-        for(int j=0; j<L; j++){
-            tmp_grid[i][j] = 0;
-        }
-    }
-    for(int i=1; i<=N; i++){
-        if(!IsDead[i]){
-            int r, c, h, w;
-            tie(r, c, h, w, ignore, ignore) = knights[i];
-            for(int x=r; x<r+h; x++){
-                for(int y=c; y<c+w; y++){
-                    tmp_grid[x][y] = i;
-                }
-            }
-        }
-        IsMoving[i] = false;
-    }
-
-
-
-
-
     int ans = 0;
     for(int i=1; i<=N; i++){
         int r, c, h, w, k, damage;
         tie(r, c, h, w, k, damage) = knights[i];
-        // << i << "번의 목숨: " << k << '\n';
-        if(k > 0){
-            int damage;
-            tie(ignore, ignore, ignore, ignore, ignore, damage) = knights[i];
+        if(k >= 1){
             ans += damage;
         }
     }
