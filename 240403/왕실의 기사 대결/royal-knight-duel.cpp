1da12945ba0ed23338ec3ba8fbd372305612c489
yj____ <60027712+YJ243@users.noreply.github.com>
2024-04-03 00:58:08 +0900

    Title: 왕실의 기사 대결, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240403/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240403/왕실의 기사 대결/royal-knight-duel.cpp
@@ -57,12 +57,7 @@ void Move(int id, int d){
         }
         IsMoving[i] = false;
     }
-    for(int i=0; i<L; i++){
-        for(int j=0; j<L; j++){
-            cout << tmp_grid[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+
     // 2. id와 연결되어 있는 기사 찾기
     q.push(id);
     IsMoving[id] = true;
@@ -72,11 +67,15 @@ void Move(int id, int d){
         
         int r, c, h, w, k, damage;
         tie(r, c, h, w, k, damage) = knights[curr];
+        //cout << r << ' ' << c << ' ' << h << ' ' << w << ' ' << k << '\n';
         for(int x=r; x<r+h; x++){
-            for(int y=c; y+c+w; y++){
+            for(int y=c; y<c+w; y++){
+                //cout << x << ' ' << y << '\n';
                 int a = x, b = y;       // (a,b)는 curr 기사의 한 부분
-                //for(;;){
+                //cout << a << ' ' << b << "는 " << curr << " 일부" << '\n';
+                for(;;){
                     a += dirs[d][0]; b += dirs[d][1];
+                    //cout << a << ' ' << b << "보고 있음" << '\n';
                     if(!InRange(a, b))
                         return;         // 만약 범위를 벗어나면 
                     if(tmp_grid[a][b] == 0){    // 빈칸임
@@ -84,6 +83,7 @@ void Move(int id, int d){
                             // 그런데 벽을 만났다면
                             return;             // 움직이지 못하니 리턴
                         }
+                        //cout << curr << "에서 " << a << ' ' << b << "가 빈칸임" << '\n';
                         break;
                     }   
                     
@@ -92,15 +92,15 @@ void Move(int id, int d){
                         q.push(tmp_grid[a][b]);
                     
                     }
-                //}
+                }
             }
         }
     }
-    cout << "AAAA" << '\n';
+
     for(int i=1; i<=N; i++){
-        cout << i << "의 움직임 여부: " << IsMoving[i] << '\n';
+
         if(!IsMoving[i]) continue;
-        cout << i << "보고있음" << '\n';
+       
         int r, c, h, w, k, damage;
         tie(r, c, h, w, k, damage) = knights[i];
         r += dirs[d][0]; c += dirs[d][1];
@@ -119,12 +119,17 @@ void Move(int id, int d){
             IsDead[i] = true;
         knights[i] = make_tuple(r, c, h, w, k, damage);
     }
+    for(int i=1; i<=N; i++){
+        int r, c, h, w, k, damage;
+        tie(r, c, h, w, k, damage) = knights[i];
+    }
 
 }
 
 void Pro(){
     // Q번의 왕의 명령 수행하기
     for(int i=0; i<Q; i++){
+        //cout << i << "번째 명령: " << '\n';
         int id, d;
         cin >> id >> d;
         if(!IsDead[id])     // 아직 사라지지 않았다면
@@ -133,11 +138,39 @@ void Pro(){
 }
 
 void Output(){
-    int ans = 0;
+
+
+int tmp_grid[L][L] = {0,};
+    for(int i=0; i<L; i++){
+        for(int j=0; j<L; j++){
+            tmp_grid[i][j] = 0;
+        }
+    }
     for(int i=1; i<=N; i++){
         if(!IsDead[i]){
+            int r, c, h, w;
+            tie(r, c, h, w, ignore, ignore) = knights[i];
+            for(int x=r; x<r+h; x++){
+                for(int y=c; y<c+w; y++){
+                    tmp_grid[x][y] = i;
+                }
+            }
+        }
+        IsMoving[i] = false;
+    }
+
+
+
+
+
+    int ans = 0;
+    for(int i=1; i<=N; i++){
+        int r, c, h, w, k, damage;
+        tie(r, c, h, w, k, damage) = knights[i];
+        // << i << "번의 목숨: " << k << '\n';
+        if(k > 0){
             int damage;
-            tie(ignore, ignore, ignore, ignore, ignore, damage);
+            tie(ignore, ignore, ignore, ignore, ignore, damage) = knights[i];
             ans += damage;
         }
     }
