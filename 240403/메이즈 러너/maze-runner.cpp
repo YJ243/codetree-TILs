4f7cdcdc2e7620dbed22dde194ec1812cab51397
yj____ <60027712+YJ243@users.noreply.github.com>
2024-04-03 17:55:31 +0900

    Title: 메이즈 러너, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240403/메이즈 러너/maze-runner.cpp
+++ 240403/메이즈 러너/maze-runner.cpp
@@ -1,191 +1,198 @@
 #include <iostream>
-#include <cstdlib>
-#define MAX_N 10
+#include <algorithm>
+#define NM 15
 
 using namespace std;
+int maze[NM][NM];
+// maze[i][j]
+// 0 -> 빈칸
+// -1 ~ -9 -> 벽을 의미하고, 내구도는 양수 버전 (예, -5 => 내구도가 5인 벽)
+// -10 -> 출구
+// 1 이상의 자연수 -> 해당 칸에 존재하는 참가자의 수 (예, 8 => 8명의 참가자가 해당 칸에 존재)
+int N, K;       // 미로의 크기, 게임 시간
+int moveCnt;    // 모든 참가자들의 이동 거리 합
 
-int N, M, K; // N: 미로 크기, M: 참가자 수, K: 게임 시간
-int grid[MAX_N][MAX_N]; // 미로 정보
-/*
- grid는 미로
- 0: 빈칸, -1 ~ -9: 벽의 내구도, -10: 출구, 양수: 참가자 수
-*/
-int temp_grid[MAX_N][MAX_N];    // 참가자가 움직이고 난 다음 격자
-int dirs[4][2] = {{-1,0},{1,0},{0,1},{0,-1}};   // 상하 우선순위 -> 좌우
-int total_moved;        // 전체 움직인 거리 합
-
-void Input(){           // 입력을 받는 함수
-    cin >> N >> M >> K;                     // N: 미로 크기, M: 참가자 수, K: 게임 시간
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            cin >> grid[i][j];
-            grid[i][j] = -grid[i][j];       // 벽 내구도 음수로 바꾸기
+void input(){   // 입력 받기
+    int M;
+    cin >> N >> M >> K;
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            int x;
+            cin >> x;
+            maze[i][j] = -x; // 해당 위치에 벽 생성
         }
     }
-    for(int i=0; i<M; i++){                 // M명의 참가자 좌표 입력받기
-        int r, c;
-        cin >> r >> c;
-        grid[r-1][c-1]++;
+    for(int i=1; i<=M; i++){
+        int x, y;
+        cin >> x >> y;
+        maze[x][y]++; // 해당 위치에 참가자 수를 1 증가
     }
-    int r, c;                               // 출구 좌표 입력받기
-    cin >> r >> c;
-    grid[r-1][c-1] = -10;                   // 출구 표시
+
+    int x, y;
+    cin >> x >> y;
+    maze[x][y] = -10;
 }
 
-bool IsFinish(){        // 모든 참가자가 미로를 탈출했는지 확인하는 함수
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(grid[i][j] > 0){     // 만약 참가자가 남아 있다면
-                return false;       // 아직 끝나지 않음
-            }
-        }
-    }
-    return true;                    // 반복문을 빠져나왔다면 끝남을 의미
-}
-
-void Initialize_before_search(){    // 탐색 전 temp_grid 초기화하는 함수
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            temp_grid[i][j] = 0;
-        }
-    }
-}
-
-pair<int, int> FindExit(){          // 출구 좌표를 찾아주는 함수 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(grid[i][j] == -10){
-                return make_pair(i,j);
+pair<int, int> findExit(){  // 출구의 좌표를 돌려주는 함수
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            if(maze[i][j] == -10){ // 만약 특정 격자에 -10이 적혀있다면, 출구를 의미한다.
+                return {i,j};
             }
         }
     }
 }
 
-bool InRange(int x, int y){         // 범위 안에 있는지 확인하는 함수
-    return 0 <= x && x < N && 0 <= y && y < N;
-}
-
-bool CanGo(int x, int y, int dist){ // 다음 칸인 (x,y) 으로 갈 수 있는지 확인하는 함수
-    pair<int, int> exit_loc = FindExit();
-    int curDist = abs(exit_loc.first - x) + abs(exit_loc.second - y);
-    // 범위 안에 있고, 다음 격자가 벽이 아니면서 출구까지의 거리가 더 작아야 갈 수 있음 
-    return InRange(x, y) && (grid[x][y] >= 0 || grid[x][y] == -10) && dist > curDist;
-}
-
-void MoveAll(){                     // 참가자를 모두 이동시키는 함수
-    // 1. 탐색 전 초기화
-    Initialize_before_search();
-    pair<int, int> exit_loc = FindExit();
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(grid[i][j] > 0){     // 참가자가 있는 칸을 만났다면
-                int dist = abs(exit_loc.first - i) + abs(exit_loc.second - j);      // 현재 위치에서 출구까지의 거리 구하기
-                int curX = i, curY = j;         // 다음으로 이동할 위치 저장
-                for(int d=0; d<4; d++){
-                    int nx = i + dirs[d][0], ny = j + dirs[d][1];
-                    if(CanGo(nx,ny, dist)){     // 만약 갈 수 있다면
-                        curX = nx, curY = ny;   // 다음으로 이동할 위치 업데이트
-                        total_moved += grid[i][j];      // 움직인 거리 증가하기
-                        break;
-                    }
-                }
-                if(curX == i && curY == j){     // 만약 네 방향을 모두 탐색했는데도 움직일 수 없는 상황이라면
-                    temp_grid[i][j] += grid[i][j];  // 움직이지 않기
-                }
-                else{   // 다음 칸으로 움직일 수 있다면
-                    if(grid[curX][curY] == -10)     // 그런데 그 칸이 출구라면
-                        continue;                   // 참가자 수 다음 격자에 표시하지 않고 넘어가기
-                    temp_grid[curX][curY] += grid[i][j];    // 그 외의 경우는 참가자 수 이동했다고 표시하기
+int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};    // 상, 하, 좌, 우 순서로 인접한 네 방향
+void moveAll(){         // 1. 모든 참가자를 한 칸씩 이동시키기
+    int newMaze[15][15] = {0,}; // 이동 결과를 저장할 배열
+    auto ex = findExit();
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            if(maze[i][j] < 0){     // 해당 격자에 벽이나 출구가 있다면, 그대로 복사하기
+                newMaze[i][j] = maze[i][j];
+                continue;
+            }
+            if(maze[i][j] == 0)
+                continue;
+            int curDist = abs(i-ex.first) + abs(j-ex.second);
+            int minDist = curDist, minI, minJ;
+            for(int k=0; k<4; k++){
+                int ni = i + dirs[k][0];
+                int nj = j + dirs[k][1];
+                // (i, j) => (ni, nj)
+                if(ni < 1 || nj < 1 || ni > N || nj > N) continue;      // 만약 격자를 벗어나면 무효
+                if(-9 <= maze[ni][nj] && maze[ni][nj] <= -1) continue;  // 만약 벽을 향한 이동이면 무효
+                int dist = abs(ni - ex.first) + abs(nj - ex.second);    // 출구와 (ni, nj) 사이의 거리
+                if(minDist > dist){     // 출구까지의 거리가 "현재보다" 더 가까워졌다면, 갱신
+                    minDist = dist;
+                    minI = ni;
+                    minJ = nj;
                 }
             }
-            else if(grid[i][j] < 0) // 만약 벽이나 출구를 만났다면
-                temp_grid[i][j] = grid[i][j];   // 바로 옮기기
-
+            // 만약 아무 곳도 갈 수가 없는 경우
+            if(minDist == curDist){
+                newMaze[i][j] += maze[i][j];        // 그대로 위치가 유지
+                continue;
+            }
+            moveCnt += maze[i][j];      // 참가자들이 1만큼 움직인다.
+            // 탈출에 성공하는 경우
+            if(maze[minI][minJ] == -10) continue;
+            newMaze[minI][minJ] += maze[i][j];      // (i,j) => (minI, minJ)
         }
-    }       
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            grid[i][j] = temp_grid[i][j];       // temp를 grid로 옮기기 
+    }
+    // 미로 덮어쓰기
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            maze[i][j] = newMaze[i][j];
+        }
+    }
+
+}
+
+void subRotate(int x, int y, int d){    // (x,y)에서 시작해서 한 변의 길이가 d인 정사각형을 회전
+    int a[NM][NM] = {0,};   // 더미 배열
+    int b[NM][NM] = {0,};   // 더미 배열
+    // 회전해야 하는 부분을 a[1][1]부터 시작하도록 옮기기
+    for(int i=x; i<=x+d; i++){
+        for(int j=y; j<=y+d; j++){
+            a[1+i-x][1+j-y] = maze[i][j];
+        }
+    }
+
+    // a 배열을 90도 회전해서 b배열에 저장하기
+    int n = d+1;        // 현재 옮겨야 하는 정사각형의 한 변의 격자 개수
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(-1 <= a[i][j] && a[i][j] <= -1){
+                // 벽이 회전한다면, 내구도 변화
+                a[i][j]++;
+            }
+            b[j][n+1-i] = a[i][j];
+        }
+    }
+
+    // 다시 미로에 복원하기
+    for(int i=x; i<=x+d; i++){
+        for(int j=y; j<=y+d; j++){
+            maze[i][j] = b[i-x+1][j-y+1];
         }
     }
 }
 
-bool CanMakeSquare(int len, int x, int y){      // (x,y)에서 시작하여 len 길이만큼 정사각형을 만들 수 있는지 확인하는 함수
-    bool IsPerson = false, IsExit = false;      // 사람이 존재하는지, 출구가 존재하는지 여부 
-    for(int i=x; i<x+len; i++){
-        for(int j=y; j<y+len; j++){
-            if(grid[i][j] > 0)
-                IsPerson = true;
-            if(grid[i][j] == -10)
-                IsExit = true;
+void rotate(){      // 출구와 참가자를 포함한 회전
+    // 1. 정사각형의 크기를 먼저 결정하자
+    // 가장 작은 정사각형의 크기 = 출구 & (출구와 가장 가까운 참가자)
+    int minDist = 1000000;
+    auto ex = findExit();
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            if(maze[i][j] <= 0) continue;       // 해당 위치에 참가자가 없다면 무시하기
+            int dist = max(abs(i-ex.first), abs(j-ex.second));      // (i,j)와 출구 사이의 거리
+            minDist = min(minDist, dist);
         }
     }
-    return IsPerson && IsExit;
-}
 
-void RotateSquare(int x, int y, int len){
-    // 1. 일단 grid의 정사각형 부분을 tmp로 옮기기
-    int tmp[MAX_N][MAX_N];
-    for(int i=0; i<len; i++){
-        for(int j=0; j<len; j++){
-            tmp[i][j] = grid[i+x][j+y];
-        }
-    }
-    // 2. tmp를 90도 회전하기
-    int rotate_tmp[MAX_N][MAX_N];
-    for(int i=0; i<len; i++){
-        for(int j=0; j<len; j++){
-            rotate_tmp[j][len-1-i] = tmp[i][j];     // 시계방향으로 90도 회전
-            if(-9 <= rotate_tmp[j][len-1-i] && rotate_tmp[j][len-1-i] <= -1)    // 만약 벽이라면
-                rotate_tmp[j][len-1-i]++;       // 내구도 감
-        }
-    }
-    // 3. rotate_tmp를 grid로 넣기
-    for(int i=0; i<len; i++){
-        for(int j=0; j<len; j++){
-            grid[i+x][j+y] = rotate_tmp[i][j];
-        }
-    }
-}
-
-bool Simulate(int turn){
-    // Step 1. 모든 참가자 한 칸 이동
-    MoveAll();
-    // Step 2. 가장 작은 정사각형 잡기
-    if(IsFinish())
-        return false;
-    int firstX, firstY, minLen;
-    bool FindSquare = false;
-    for(int k=1; k<=N; k++){    // 정사각형 한 변의 길이는 1부터 N까지
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(i+k-1 >= N || j+k-1 >= N) continue;      // 정사각형을 만들지 못하면 넘어가기 
-                if(CanMakeSquare(k, i, j)){                 // 한 명 이상의 참가자와 출구를 포함한 정사각형을 만들 수 있다면
-                    firstX = i, firstY = j, minLen = k;     // 좌상단 좌표와 길이 저장하기
-                    FindSquare = true;                      // 정사각형 찾았다고 표시하기
-                    break;
+    // 2. 정사각형의 위치를 그 다음으로 결정하자.
+    int bestRow = 0, bestCol = 0;       // 결정된 정사각형의 좌상단 좌표
+    for(int i=1; i<=N-minDist; i++){
+        for(int j=1; j<=N-minDist; j++){
+            // (i, j) := 이번에 결정한 정사각형의 좌상단 좌표
+            // 행 := i ~ i + minDist;
+            // 열 := j ~ j + minDist
+            bool flagExit = false, flagPerson = false;
+            for(int r=i; r <= i+minDist; r++){
+                for(int c=j; c <= j+minDist; c++){
+                    if(maze[r][c] == -10) flagExit = true;
+                    if(maze[r][c] > 0) flagPerson = true;
                 }
             }
-            if(FindSquare) break;
+            if(flagExit && flagPerson){
+                // 이번에 결정한 정사각형이 사람과 출구를 모두 호팜!
+                bestRow = i;
+                bestCol = j;
+                break;
+            }
         }
-        if(FindSquare) break;
+        if(bestRow != 0) break;
     }
+    // 3. 회전하기
+    subRotate(bestRow, bestCol, minDist);
 
-    // Step 3. 회전하기
-    RotateSquare(firstX, firstY, minLen);
+}
+
+void output(){ // 정답 출력
+    cout << moveCnt << "\n";
+    auto ex = findExit();
+    cout << ex.first << ' ' << ex.second << '\n';
+}
+
+bool isFinish(){
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            if(maze[i][j] > 0){
+                // 한 번이라도 참가자가 발견되면
+                return false;
+            }
+        }
+    }
     return true;
 }
 
-int main() {
+int main(){
     // 입력 받기:
-    Input();
-    // K초 동안 이동하기
-    for(int i=1; i<=K; i++){
-        bool CanGo = Simulate(i);
-        if(!CanGo)  break;
+    input();
+    while(K--){
+        // 시뮬레이션 진행
+        // 1. 모든 참가자를 한 칸씩 이동시키기
+        moveAll();
+        if(isFinish()){     // 게임 종료 조건 확인
+            break;
+        }
+        // 2. 출구와 참가자를 포함한 회전
+        rotate();
     }
-    cout << total_moved << '\n';
-    pair<int, int> exit_loc = FindExit();
-    cout << exit_loc.first+1 << ' ' << exit_loc.second+1 << '\n';
-    return 0;
+    // 정답 출력:
+    output();
+    return 0;  
 }
