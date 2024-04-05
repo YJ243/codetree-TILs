3f6f6c804fea92baf455555e1ceb0f0780ea4462
yj____ <60027712+YJ243@users.noreply.github.com>
2024-04-05 18:17:32 +0900

    Title: 싸움땅, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/싸움땅/battle-ground.cpp
+++ 240405/싸움땅/battle-ground.cpp
@@ -15,10 +15,10 @@ vector<int> gun_power[MAX_N][MAX_N];    // 격자에 존재하는 총의 공격�
 
 int grid[MAX_N][MAX_N];                 
 // 플레이어 번호가 적힌 격자
-// 0: 빈칸, 양수: 번호
-tuple<int, int, int, int, int> players[MAX_M+1];      // 위치 (x,y), 방향 d, 초기능력 s, 총의 공격력 p
-int points[MAX_M+1];                             // 플레이어들의 점수
-int dirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};    // 상, 우, 하, 좌
+// 0: 빈칸, 1이상의 양수: 번호
+tuple<int, int, int, int, int> players[MAX_M+1];        // players[i]: i번 선수의 위치 (x,y), 방향 d, 초기능력 s, 총의 공격력 p
+int points[MAX_M+1];                                    // 플레이어들의 점수
+int dirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};           // 상, 우, 하, 좌
 
 void Input(){       // 입력을 받는 함수 
     cin >> n >> m >> k;                 // n: 격자 크기, m: 플레이어 수, k: 라운드 수
@@ -45,6 +45,7 @@ bool InRange(int x, int y){         // (x,y)가 범위 안에 있는지 확인�
 void GetGun(int x, int y, int idx){  // idx번 플레이어가 (x, y)에서 총 줍기 
     int d, s, p;                     // 방향 d, 초기능력 s, 가지고 있는 총의 공격력 p
     tie(ignore, ignore, d, s, p) = players[idx];     // idx번 플레이어 정보
+    
     int max_power_gun = 0, maxI = 0;                 // 현재 (x,y)에 존재하는 총들 중 가장 공격력이 센 총
     for(int i=0; i<(int)gun_power[x][y].size(); i++){
         if(max_power_gun < gun_power[x][y][i]){
@@ -75,10 +76,10 @@ void MoveLosePlayer(int idx){       // 싸움에서 진 idx번 플레이어를 �
     int x, y, d, s, p;                     // 위치 (x,y), 방향 d, 초기능력 s
     tie(x, y, d, s, p) = players[idx];     // idx번 플레이어 정보
     if(p > 0){      // 총이 있다면 총 내려놓기
-        gun_power[x][y].push_back(p);
-        p = 0;
+        gun_power[x][y].push_back(p);       // 해당 칸에 총 넣고
+        p = 0;      // 가지고 있는 총의 공격력 0으로
     }
-    int nx, ny;
+    int nx, ny;     // 다음으로 이동할 위치
 
     for(int i=0; i<4; i++){
         nx = x + dirs[(d+i)%4][0], ny = y + dirs[(d+i)%4][1];   // idx번 플레이어가 이동할 다음 위치
@@ -111,29 +112,29 @@ void Move(int idx){ // idx번 플레이어 이동시키는 함수
     tie(nx, ny, d) = GetNext(x, y, d);
 
     if(grid[nx][ny] == 0){      // 만약 이동한 방향에 플레이어가 없다면
-        players[idx] = make_tuple(nx, ny, d, s, p);     // 위치, 방향, 초기능력, 총의 공격력 업데이트
-        GetGun(nx, ny, idx);            // 이동한 곳에서 총 줍기 
+        players[idx] = make_tuple(nx, ny, d, s, p);     // idx번 플레이어의 위치, 방향, 초기능력, 총의 공격력 업데이트
+        GetGun(nx, ny, idx);                            // 이동한 곳에서 총 줍기 
     }
-    else{       // 만약 플레이어가 존재한다면
+    else{                       // 만약 플레이어가 존재한다면
         // 현재 (x,y) -> (nx, ny) 방향으로 가려는 원래 idx번 플레이어가 grid[nx][ny]번 사람과 만나게 됨
         int x2, y2, d2, s2, p2;
-        tie(x2, y2, d2, s2, p2) = players[grid[nx][ny]];
-        bool win = false;       // 참: 원래 플레이어 승, 거짓: 원래 플레이어 짐
+        tie(x2, y2, d2, s2, p2) = players[grid[nx][ny]];    // 이동할 격자 칸에 존재하는 플레이어
+        bool win = false;       // 참: idx번 플레이어 승, 거짓: idx번 플레이어 짐
         
-        if(make_tuple(s+p, s) > make_tuple(s2+p2, s2))
-            win = true;
+        if(make_tuple(s+p, s) > make_tuple(s2+p2, s2))  // 만약 idx번 플레이어가 이겼다면
+            win = true;     // 이겼다고 표시
         // 이긴 플레이어는 각 플레이어의 초기 능력치와 가지고 있는 총의 공격력의 합의 차이만큼 포인트를 획득
         int winIdx, loseIdx;
-        if(win){
-            points[idx] += (s+p) - (s2+p2);
-            winIdx = idx, loseIdx = grid[nx][ny];
+        if(win){    // idx번 플레이어가 이겼다면
+            points[idx] += (s+p) - (s2+p2);             // idx번 플레이어 점수 증가
+            winIdx = idx, loseIdx = grid[nx][ny];   
         }
-        else{
-            points[grid[nx][ny]] += (s2+p2) - (s+p);
+        else{       // 격자에 있던 플레이어가 이겼다면
+            points[grid[nx][ny]] += (s2+p2) - (s+p);    // 해당 플레이어 점수 증가 
             winIdx = grid[nx][ny], loseIdx = idx;
         }
-        grid[x][y] = 0;     // 누가 이겼든 원래 있던 자리는 0으로 만들어야 함
-        players[idx] = make_tuple(nx, ny, d, s, p);     // 위치, 방향, 초기능력, 총의 공격력 업데이트
+        grid[x][y] = 0;                                 // 누가 이겼든 원래 있던 자리는 0으로 만들어야 함
+        players[idx] = make_tuple(nx, ny, d, s, p);     // idx번 위치, 방향, 초기능력, 총의 공격력 업데이트
     
         // 진 플레이어는 이동시키기
         MoveLosePlayer(loseIdx);
@@ -143,7 +144,7 @@ void Move(int idx){ // idx번 플레이어 이동시키는 함수
     }
 }
 
-void Initialize(){
+void Initialize(){      // 플레이어 격자를 업데이트하는 함수
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             grid[i][j] = 0;
@@ -159,12 +160,12 @@ void Initialize(){
 void Simulate(){
     // grid 그리기
     for(int i=1; i<=m; i++){    // 첫 번째 플레이어부터 순차적으로 이동하기
-        Initialize();
-        Move(i);
+        Initialize();           // 격자 업데이트
+        Move(i);                // i번 플레이어 움직이기
     }
 }
 
-void Output(){
+void Output(){      // 플레이어들의 점수를 출력하는 함수 
     for(int i=1; i<=m; i++){
         cout << points[i] << ' ';
     }
