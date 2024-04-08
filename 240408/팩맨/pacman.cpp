afbe001bd0d00087ce248efd6a8c36dd9feeb0da
yj____ <60027712+YJ243@users.noreply.github.com>
2024-04-08 21:15:58 +0900

    Title: 팩맨, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/팩맨/pacman.cpp
+++ 240408/팩맨/pacman.cpp
@@ -1,231 +1,227 @@
 #include <iostream>
+#include <tuple>
 #include <vector>
+#include <algorithm>
 
-#define MAX_M 10
+#define MAX_T 25
 #define MAX_N 4
+#define DIR_NUM 8
+#define P_DIR_NUM 4
+#define MAX_DECAY 2
 
 using namespace std;
 
-int n = MAX_N;
-int m, t;                           // m: 몬스터 마리 수, t: 진행되는 턴 수
-vector<int> grid[MAX_N][MAX_N];     // grid[i][j]: (i,j)에 있는 몬스터의 방향(방향 정보만 저장)
-vector<int> egg[MAX_N][MAX_N];      // egg[i][j]: (i,j)에서 부화되는 알의 방향
-vector<int> dead[MAX_N][MAX_N];     // dead[i][j]: (i,j)에 있는 시체의 소멸되는 턴을 저장
-pair<int, int> pacman;              // 팩맨 위치
+int n = 4;
+int m, t;
 
-int dirs[8][2] = {{-1,0},{-1,-1},{0,-1},{1,-1},{1,0},{1,1},{0,1},{-1,1}}; //   ↑, ↖, ←, ↙, ↓, ↘, →, ↗
-int pdirs[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};   // 팩맨 움직이는 방향, 상좌하우
-vector<int> selected;
-int movingDir[3];
-int max_caught;
-void Input(){   // 입력을 받는 함수 
-    cin >> m >> t;      // m: 몬스터 마리 수, t: 진행되는 턴 수
-    int r, c;
-    cin >> r >> c;
-    pacman = make_pair(r-1, c-1);   // 팩맨 위치 입력받기
-    for(int i=0; i<m; i++){
-        int r, c, d;
-        cin >> r >> c >> d;
-        grid[r-1][c-1].push_back(d-1);      // 몬스터 위치와 방향 입력받기
-    }
-}
+// map의 상태를 뜻합니다.
+// t번째 턴에 (x, y) 위치에 방향 move_dir를 바라보고 있는
+// 몬스터의 수를 뜻합니다.
+int monster[MAX_T + 1][MAX_N][MAX_N][DIR_NUM];
 
-void TryCopyMonster(int x, int y){ // (x, y)에 있는 몬스터를 복제하는 함수
-    for(int i=0; i<(int)grid[x][y].size(); i++){
-        int d = grid[x][y][i];
-        egg[x][y].push_back(d);     // 알에다가 d 방향 넣기
-    }
-}
+// 시체를 관리하기 위한 배열입니다.
+// 좀 더 자세하게는,
+// (x, y)위치에서
+// 썩기 t초 전인 시체가
+// 몇 개 있는지를 의미합니다.
+int dead[MAX_N][MAX_N][MAX_DECAY + 1];
 
-bool InRange(int x, int y){     // (x, y)가 범위 안에 있는지 확인하는 함수
+// 문제에서 주어지는 방향 순서대로
+// dx, dy 값들을 정의합니다. 
+// 몬스터를 위한 방향입니다.
+int dx[DIR_NUM] = {-1, -1,  0,  1, 1, 1, 0, -1};
+int dy[DIR_NUM] = { 0, -1, -1, -1, 0, 1, 1,  1};
+
+// 팩맨을 위한
+// dx, dy를 따로 정의합니다.
+// 우선순위에 맞춰
+// 상좌하우 순으로 적어줍니다.
+int p_dx[P_DIR_NUM] = {-1,  0, 1, 0};
+int p_dy[P_DIR_NUM] = { 0, -1, 0, 1};
+
+// 팩맨의 위치를 저장합니다.
+int px, py;
+
+// 현재 몇 번째 턴인지를 저장합니다.
+int t_num = 1;
+
+// 영역 내에 있는지를 확인합니다.
+bool InRange(int x, int y) {
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-bool CanGo(int x, int y){   // 몬스터가 (x, y)로 이동할 수 있는지 확인하는 함수
-    // 범위 안에 있고, 팩맨이 없으며, 시체가 없는 경우에 움직일 수 있다.
-    return InRange(x, y) && make_pair(x, y) != pacman && (int)dead[x][y].size() == 0; 
+// 나아가려고 하는 위치가 영역 내에 있으며 
+// 몬스터 시체가 없고, 팩맨도 없다면
+// 이동이 가능합니다.
+bool CanGo(int x, int y) {
+    return InRange(x, y) && dead[x][y][0] == 0 && dead[x][y][1] == 0 
+           && make_pair(x, y) != make_pair(px, py);
 }
 
-void MoveMonster(){     // 몬스터를 이동하는 함수
-    vector<int> tmp[n][n];  // 다음 몬스터 위치를 저장할 배열
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            tmp[i][j].clear();
+tuple<int, int, int> GetNextPos(int x, int y, int move_dir) {
+    // 현재 위치에서부터
+    // 반시계방향으로 45'씩 회전해보며
+    // 가능한 곳이 보이면 바로 이동합니다.
+    for(int c_dir = 0; c_dir < DIR_NUM; c_dir++) {
+        int n_dir = (move_dir + c_dir + DIR_NUM) % DIR_NUM;
+        int nx = x + dx[n_dir];
+        int ny = y + dy[n_dir];
+        if(CanGo(nx, ny)) {
+            return make_tuple(nx, ny, n_dir);
+        }
+    }
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(grid[i][j].size() >= 1){ // 만약 해당 칸에 몬스터가 있다면
-                for(int k=0; k<(int)grid[i][j].size(); k++){
-                    int cur_d = grid[i][j][k];  // 몬스터의 현재 방향
-                    bool movable = false; // 움직일 수 있는지 여부
-                    int ni, nj;
-                    for(int l=0; l<=8; l++){
-                        ni = i + dirs[(cur_d+l)%8][0], nj = j + dirs[(cur_d+l)%8][1];
-                        if(CanGo(ni, nj)){
-                            movable = true;
-                            cur_d = (cur_d+l) % 8;
-                            break;
-                        }
-                    }
-                    if(movable){    // 만약 k번째 몬스터를 움직일 수 있다면
-                        tmp[ni][nj].push_back(cur_d);
-                    }
-                    else{   // 움직일 수 없다면 해당 자리에 그대로 놓기
-                        tmp[i][j].push_back(cur_d);
-                    }
+    // 이동이 불가능하다면, 움직이지 않고 기존 상태 그대로 반환합니다.
+    return make_tuple(x, y, move_dir);
+}
+
+void MoveM() {
+    // 각 (i, j)칸에 k 방향을 바라보고 있는 몬스터들이
+    // 그 다음으로 이동해야할 위치 및 방향을 구해
+    // 전부 (칸, 방향) 단위로 이동시켜 줍니다.
+    // 일일이 몬스터 마다 위치를 구해 이동시키면 시간초과가 발생합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            for(int k = 0; k < DIR_NUM; k++) {
+                tuple<int, int, int> next_pos = GetNextPos(i, j, k);
+                int x, y, next_dir;
+                tie(x, y, next_dir) = next_pos;
+                monster[t_num][x][y][next_dir] += monster[t_num - 1][i][j][k];
+            }
+}
+
+int GetKilledNum(int dir1, int dir2, int dir3) {
+    int dirs[] = {dir1, dir2, dir3};
+    int x = px, y = py;
+    int killed_num = 0;
+
+    // 방문한적이 있는지를 기록합니다.
+    vector<pair<int, int> > v_pos;
+
+    for(int i = 0; i < 3; i++) {
+        int nx = x + p_dx[dirs[i]], ny = y + p_dy[dirs[i]];
+        // 움직이는 도중에 격자를 벗어나는 경우라면, 선택되면 안됩니다.
+        if(!InRange(nx, ny))
+            return -1;
+		// 이미 계산한 곳에 대해서는, 중복 계산하지 않습니다.
+        if(find(v_pos.begin(), v_pos.end(), make_pair(nx, ny)) == v_pos.end()) {
+            for(int j = 0; j < DIR_NUM; j++)
+                killed_num += monster[t_num][nx][ny][j];
+            
+            v_pos.push_back(make_pair(nx, ny));
+        }
+
+        x = nx; y = ny;
+    }
+    return killed_num;
+}
+
+void DoKill(tuple<int, int, int> best_route) {
+    int dir1, dir2, dir3;
+    tie(dir1, dir2, dir3) = best_route;
+	
+	// 정해진 dir1, dir2, dir3 순서에 맞춰 이동하며
+	// 몬스터를 잡습니다.
+    int dirs[] = {dir1, dir2, dir3};
+    for(int i = 0; i < 3; i++) {
+        int nx = px + p_dx[dirs[i]], ny = py + p_dy[dirs[i]];
+
+        for(int j = 0; j < DIR_NUM; j++) {
+			// 시체가 추가됩니다.
+            dead[nx][ny][MAX_DECAY] += monster[t_num][nx][ny][j];
+            monster[t_num][nx][ny][j] = 0;
+        }
+
+        px = nx; py = ny;
+    }
+}
+
+void MoveP() {
+    int max_cnt = -1;
+    tuple<int, int, int> best_route;
+	
+	// 우선순위 순서대로 수행합니다.
+    for(int i = 0; i < P_DIR_NUM; i++)
+        for(int j = 0; j < P_DIR_NUM; j++)
+            for(int k = 0; k < P_DIR_NUM; k++) {
+                int m_cnt = GetKilledNum(i, j, k);
+				// 가장 많은 수의 몬스터를 잡을 수 있는 경우 중
+				// 우선순위가 가장 높은 것을 고릅니다.
+                if(m_cnt > max_cnt) {
+                    max_cnt = m_cnt;
+                    best_route = make_tuple(i, j, k);
                 }
             }
-        }
-    }
-    // tmp에서 grid로 옮겨주기
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            grid[i][j] = tmp[i][j];
-        }
-    }
-}
-
-bool IsValidRoute(){    // selected에 있는 방향대로 팩맨이 움직일 수 있는지 확인하는 함수
-    int x = pacman.first, y = pacman.second;
-    for(int i=0; i<3; i++){
-        x += pdirs[selected[i]][0]; y += pdirs[selected[i]][1];
-        if(!InRange(x, y))
-            return false;
-    }
-    return true;
-}
-
-int GetMonsterCnt(){    
-    int monsterCnt = 0;
-    bool visited[n][n];
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            visited[i][j] = false;
-        }
-    }
-
-    int x = pacman.first, y = pacman.second;
-    for(int i=0; i<3; i++){
-        x += pdirs[selected[i]][0]; y += pdirs[selected[i]][1];
-        if(!visited[x][y])
-            monsterCnt += (int)grid[x][y].size();        
-        visited[x][y] = true;
-    }
-    return monsterCnt;
-}
-
-void ChooseDirs(int idx){
-    if(idx == 3){
-        if(IsValidRoute()){
-            int curCnt = GetMonsterCnt();
-            if(max_caught < curCnt){
-                max_caught = curCnt;
-                for(int i=0; i<3; i++)
-                    movingDir[i] = selected[i];    // 움직이는 방향 저장하기
-            }
-        }
-        return;
-    }
-    for(int i=0; i<4; i++){
-        selected.push_back(i);
-        ChooseDirs(idx+1);
-        selected.pop_back();
-    }
-}
-
-void EatMonster(int turn){
-    int x = pacman.first, y = pacman.second;
-    for(int i=0; i<3; i++){
-        int d = movingDir[i];
-        x += pdirs[d][0]; y += pdirs[d][1];
-        for(int k=0; k < (int)grid[x][y].size(); k++){
-            dead[x][y].push_back(turn+2); // 시체가 사라지는 턴수 집어넣기
-        }
-        grid[x][y].clear();
-    }
-    pacman = make_pair(x, y);
-}
-
-void MovePacman(int turn){      // 팩맨을 움직이는 함수
-    // 팩맨이 움직이는 방향 구하기
-    max_caught = -1;
-    ChooseDirs(0);  // 0번째 움직일 방향을 선택하러 가기
-    // 이제 movingDir에 있는 방향대로 움직이면서 몬스터를 먹어 치우기 시체 남기기
-    EatMonster(turn);
-}
-
-void DeleteDeadMonster(int turn){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(dead[i][j].size() >= 1){
-                vector<int> tmp;
-                for(int k=0; k<(int)dead[i][j].size(); k++){
-                    // 만약 소멸되는 턴수가 현재 turn보다 작을 때만 남기기
-                    if(dead[i][j][k] > turn)
-                        tmp.push_back(dead[i][j][k]);
-                }
-                dead[i][j] = tmp;
-            }
-        }
-    }
-}
-
-void CopyMonster(){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(egg[i][j].size() > 0){
-                for(int k=0; k<(int)egg[i][j].size(); k++){
-                    grid[i][j].push_back(egg[i][j][k]);
-                }
-                egg[i][j].clear();
-            }
-
-        }
-    }
-}
-void Simulate(int turn){
-    // Step 1. 몬스터 복제 시도
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            egg[i][j] = grid[i][j];
-        }
-    }
-
-    // Step 2. 몬스터 이동
-    MoveMonster();
-
-
-    // Step 3. 팩맨 이동
-    MovePacman(turn);
-
-    // Step 4. 시체 소멸
-    DeleteDeadMonster(turn);
-    // Step 5. 몬스터 복제 완성
-    CopyMonster();
-
     
+    // 최선의 루트에 따라 
+    // 실제 죽이는 것을 진행합니다.
+    DoKill(best_route);
 }
 
-void Output(){  // 살아남은 몬스터를 출력하는 함수
-    int ans = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            ans += (int)grid[i][j].size();
+void DecayM() {
+	// decay를 진행합니다. 턴을 하나씩 깎아주면 됩니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) {
+            for(int k = 0; k < MAX_DECAY; k++)
+                dead[i][j][k] = dead[i][j][k + 1];
+            dead[i][j][MAX_DECAY] = 0;
         }
-    }
-    cout << ans << '\n';
+}
+
+void AddM() {
+	// 몬스터가 복제됩니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            for(int k = 0; k < DIR_NUM; k++)
+                monster[t_num][i][j][k] += monster[t_num - 1][i][j][k];
+}
+
+void Simulate() {
+    // 매 초마다 기록하기 때문에 굳이 copy를 진행할 필요는 없습니다.
+    
+    // 각 칸에 있는 몬스터를 이동시킵니다.
+    MoveM();
+
+    // 팩맨을 이동시킵니다.
+    MoveP();
+
+    // 시체들이 썩어갑니다.
+    DecayM();
+
+    // 몬스터가 복제됩니다.
+    AddM();
+}
+
+int CountMonster() {
+    int cnt = 0;
+
+    // 마지막 턴을 마친 이후의 몬스터 수를 셉니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            for(int k = 0; k < DIR_NUM; k++)
+                cnt += monster[t][i][j][k];
+
+    return cnt;
 }
 
 int main() {
-    // 입력 받기:
-    Input();
-
-    // t개의 턴에 대해 시뮬레이션 진행
-    for(int i=1; i<=t; i++){
-        Simulate(i);
+    // 입력:
+    cin >> m >> t;
+    cin >> px >> py;
+    px--; py--;
+    for(int i = 0; i < m; i++) {
+        int mx, my, mdir;
+        cin >> mx >> my >> mdir;
+        // 첫 번째 턴의 상태를 기록합니다.
+        monster[0][mx - 1][my - 1][mdir - 1]++;
+    }
+    
+	// t번 시뮬레이션을 진행합니다.
+    while(t_num <= t) {
+        Simulate();
+        t_num++;
     }
 
-    // 출력하기:
-    Output();
+    cout << CountMonster();
     return 0;
 }
