ceec2bfb7f928b44a82fffceb9189cd489767db5
yj____ <60027712+YJ243@users.noreply.github.com>
2024-04-12 22:59:17 +0900

    Title: 냉방 시스템, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/냉방 시스템/cooling-system.cpp
+++ 240412/냉방 시스템/cooling-system.cpp
@@ -1,228 +1,249 @@
 #include <iostream>
-#include <queue>
 
 #define MAX_N 20
+#define MAX_M 400
+#define DIR_NUM 4
+#define OFFICE 1
 
 using namespace std;
 
-int n, m, k;    // n: 격자 크기, m: 벽의 개수, k: 사무실 시원함 정도
-int grid[MAX_N][MAX_N];     // 격자 정보
-int walls[MAX_N][MAX_N];    // 벽 표시하기
-int room_cnt;
-vector<pair<int, int> > aircond;    // 에어컨 위치
-int dirs[4][3][2] = {   // 에어컨에서 바람이 나와서 시원하게 만드는 위치, 오위왼아 순서
-    {{0,-1},{-1,-1},{1,-1}},
-    {{-1,0},{-1,-1},{-1,1}},
-    {{0,1},{-1,1},{1,1}},
-    {{1,0},{1,-1},{1,1}}
-};
-int checkdirs[4][4][2] = {  // 대각선 방향 확인하기
-    {{-1,0},{0,-1},{1,0},{0,-1}},
-    {{0,-1},{-1,0},{0,1},{-1,0}},
-    {{-1,0},{0,1},{1,0},{0,1}},
-    {{0,-1},{1,0},{0,1},{1,0}}
-};
-// bfs 탐색을 위한 자료구조
-queue<pair<int, int> > q;   // 탐색을 위한 큐
-bool visited[MAX_N][MAX_N]; // 방문 확인을 위한 배열
-int step[MAX_N][MAX_N];     // 시원함이 퍼져나가는 정도 
-int cool[MAX_N][MAX_N];     // 시원함 정도
-int tempCool[MAX_N][MAX_N];
-void Input(){   // 입력을 받는 함수
-    cin >> n >> m >> k; // n: 격자 크기, m: 벽의 개수, k: 사무실 시원함 정도
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++){
-            cin >> grid[i][j];  // 격자 입력받기
-            if(grid[i][j] == 1)
-                room_cnt++;
-            if(grid[i][j] > 1)
-                aircond.push_back(make_pair(i, j));
-        }
-    int wall_idx = 0;
-    for(int i=0; i<m; i++){
-        int x, y, s;
-        cin >> x >> y >> s;
-        x--; y--;
-        if(s == 0){ // (x, y) 바로 위에 벽
-            int val = 0;
-            if(walls[x][y] > 0 || walls[x-1][y] > 0)
-                val = max(walls[x][y], walls[x-1][y]);
-            else if(walls[x][y] == 0 && walls[x-1][y] == 0)
-                val = ++wall_idx;
-            walls[x][y] = val;
-            if(0 <= x-1 && x-1 < n)
-                walls[x-1][y] = val;
-        }       
-        else{   // (x, y) 바로 왼쪽에 벽
-            int val = 0;
-            if(walls[x][y] > 0 || walls[x][y-1] > 0)
-                val = max(walls[x][y], walls[x][y-1]);
-            else if(walls[x][y] == 0 && walls[x][y-1] == 0)
-                val = ++wall_idx;
-            walls[x][y] = val;
-            if(0 <= y-1 && y-1 < n)
-                walls[x][y-1] = val;
-        }
-    }
-}
+int n, m, k;
+int grid[MAX_N][MAX_N];
 
-bool IsFinish(){    // 끝났는지 확인하는 함수
-    int cnt = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(grid[i][j] == 1 && cool[i][j] >= k)
-                cnt++;
-        }
-    }
-    if(cnt == room_cnt)
-        return true;
-    return false;
-}
+// 각 위치의 시원함의 정도를 관리합니다.
+// 처음에는 전부 0입니다.
+int coolness[MAX_N][MAX_N];
 
-void Initialize_visited(){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            visited[i][j] = false;
-            step[i][j] = 0;
-        }
-    }
-}
+// 시원함을 mix할 때
+// 동시에 일어나는 처리를
+// 편하게 하기 위해 사용될 배열입니다.
+int temp[MAX_N][MAX_N];
 
-bool InRange(int x, int y){
+// dx, dy 순서를 상좌우하로 설정합니다.
+// 입력으로 주어지는 숫자에 맞추며,
+// 4에서 현재 방향을 뺏을 때, 반대 방향이 나오도록 설정한 것입니다.
+int dx[DIR_NUM] = {-1,  0, 0, 1};
+int dy[DIR_NUM] = { 0, -1, 1, 0};
+
+// 현재 위치 (x, y)에서 해당 방향으로
+// 이동한다 했을 때 벽이 있는지를 나타냅니다.
+bool block[MAX_N][MAX_N][DIR_NUM];
+
+// 시원함을 전파할 시
+// 한 에어컨에 대해
+// 겹쳐서 퍼지는 경우를 막기 위해
+// visited 배열을 사용합니다.
+bool visited[MAX_N][MAX_N];
+
+// 현재까지 흐른 시간(분)을 나타냅니다.
+int elapsed_time;
+
+// (x, y)가 격자 내에 있는지를 판단합니다.
+bool InRange(int x, int y) {
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-bool CanGo(int x, int y, int nx, int ny, int s, int d){
-    // 범위 안에 있고, 방문하지 않았고, 벽이 없으면
-    // 범위 안에 없거나 방문했다면 그 곳으로는 갈 수 없
-    if(!InRange(nx, ny) || visited[nx][ny])
-        return false;
-    bool ret = true;
-    if(d == 0){
-        if(walls[x][y] >0 && walls[x][y] == walls[nx][ny]){
-            ret = false;
-        }
-    }
-    else if(d == 1){
-        if((walls[x][y] >0 && walls[x+checkdirs[s][0][0]][y+checkdirs[s][0][1]] == walls[x][y]) ||
-        (walls[x+checkdirs[s][0][0]][y+checkdirs[s][0][1]] >0 && walls[nx][ny] == walls[x+checkdirs[s][0][0]][y+checkdirs[s][0][1]]))
-            ret = false;
-    }
-    else{
-        // (x,y) && (x + checkdirs[s][2][0], y + checkdirs[s][2][1])
-        if((walls[x][y] > 0 && walls[x+checkdirs[s][2][0]][y+checkdirs[s][2][1]] == walls[x][y]) ||
-        (walls[x+checkdirs[s][2][0]][y+checkdirs[s][2][1]] > 0 && walls[nx][ny] == walls[x+checkdirs[s][2][0]][y+checkdirs[s][2][1]]))
-            ret = false;
-    }
-    return ret;
+// (dx, dy) 값으로부터
+// move_dir값을 추출해냅니다.
+int RevDir(int x_diff, int y_diff) {
+    for(int i = 0; i < 4; i++)
+        if(dx[i] == x_diff && dy[i] == y_diff)
+            return i;
 }
 
-void bfs(int s){
-    while(!q.empty()){
-        pair<int, int> curr = q.front();
-        q.pop();
-        int x = curr.first, y = curr.second;
+// (x, y)위치에서 move_dir 방향으로
+// power 만큼의 시원함을 만들어줍니다.
+// 이는 그 다음 칸에게도 영향을 끼칩니다.
+void Spread(int x, int y, int move_dir, int power) {
+    // power가 0이 되면 전파를 멈춥니다.
+    if(power == 0)
+        return;
 
-        for(int d=0; d<3; d++){
-            int nx = x + dirs[s][d][0], ny = y + dirs[s][d][1];
-            ////cout<<nx << ' ' << ny << '\n';
-            if(CanGo(x, y, nx, ny,s, d) && step[x][y] >= 1 && step[x][y] > 0){
-                //cout << "갈 수 있음 " << nx << ' ' << ny << '\n';
-                visited[nx][ny] = true;         // 방문 표시
-                step[nx][ny] = step[x][y]-1;    // 시원함 정도 1 줄이기
-                q.push(make_pair(nx, ny));      // 큐에다가 넣기
+    // 방문 체크를 하고, 해당 위치에 power를 더해줍니다.
+    visited[x][y] = true;
+    coolness[x][y] += power;
+
+    // Case 1. 직진하여 전파되는 경우입니다.
+    int nx = x + dx[move_dir];
+    int ny = y + dy[move_dir];
+    if(InRange(nx, ny) && !visited[nx][ny] && !block[x][y][move_dir])
+        Spread(nx, ny, move_dir, power - 1);
+    
+    // Case 2. 대각선 방향으로 전파되는 경우입니다.
+    if(dx[move_dir] == 0) {
+        int nxs[] = {x + 1, x - 1};
+        for(int i = 0; i < 2; i++) {
+            int nx = nxs[i];
+            int ny = y + dy[move_dir];
+            // 꺾여 들어가는 곳에 전부 벽이 없는 경우에만 전파가 가능합니다.
+            if(InRange(nx, ny) && !visited[nx][ny] && !block[x][y][RevDir(nx - x, 0)] && !block[nx][y][move_dir])
+                Spread(nx, ny, move_dir, power - 1);
+        }
+    }
+    else {
+        int nys[] = {y + 1, y - 1};
+        for(int i = 0; i < 2; i++) {
+            int nx = x + dx[move_dir];
+            int ny = nys[i];
+            // 꺾여 들어가는 곳에 전부 벽이 없는 경우에만 전파가 가능합니다.
+            if(InRange(nx, ny) && !visited[nx][ny] && !block[x][y][RevDir(0, ny - y)] && !block[x][ny][move_dir])
+                Spread(nx, ny, move_dir, power - 1);
+        }
+    }
+}
+
+void ClearVisited() {
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            visited[i][j] = false;
+}
+
+// 에어컨에서 시원함을 발산합니다.
+void Blow() {
+    // 각 에어컨에 대해
+    // 시원함을 발산합니다.
+    for(int x = 0; x < n; x++)
+        for(int y = 0; y < n; y++)
+            // 에어컨에 대해
+            // 해당 방향으로 시원함을
+            // 만들어줍니다.
+            if(grid[x][y] >= 2) {
+                int move_dir = (grid[x][y] <= 3) ? (3 - grid[x][y]) 
+                                                 : (grid[x][y] - 2);
+                int nx = x + dx[move_dir];
+                int ny = y + dy[move_dir];
+
+                // 전파 전에 visited 값을 초기화해줍니다.
+                ClearVisited();
+                // 세기 5에서 시작하여 계속 전파합니다.
+                Spread(nx, ny, move_dir, 5);
             }
-        }
-    }
 }
 
-void BlowWind(int x, int y, int s){
-    // 탐색 전 초기화하기
-    Initialize_visited();
-    int nx = x + dirs[s][0][0], ny = y + dirs[s][0][1];
-    //cout << x << ' ' << y << ' ' << s << ' ' << nx << ' ' << ny << '\n';
-    visited[nx][ny] = true;
-    step[nx][ny] = 5;
-    q.push(make_pair(nx, ny));
-    bfs(s);
+// (x, y) 위치는
+// mix된 이후 시원함이
+// 얼마가 되는지를 계산해줍니다.
+int GetMixedCoolness(int x, int y) {
+    int remaining_c = coolness[x][y];
+    for(int i = 0; i < DIR_NUM; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        // 사이에 벽이 존재하지 않는 경우에만 mix가 일어납니다.
+        if(InRange(nx, ny) && !block[x][y][i]) {
+            // 현재의 시원함이 더 크다면, 그 차이를 4로 나눈 값 만큼 빠져나갑니다.
+            if(coolness[x][y] > coolness[nx][ny])
+                remaining_c -= (coolness[x][y] - coolness[nx][ny]) / 4;
+            // 그렇지 않다면, 반대로 그 차이를 4로 나눈 값만큼 받아오게 됩니다.
+            else
+                remaining_c += (coolness[nx][ny] - coolness[x][y]) / 4;
+        }
+    }
+    return remaining_c;
 }
 
-void MixAir(int x, int y){  // (x, y)에서 공기가 이동할 수 있다면 이동시키기
-    int sdirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
-    
-    for(int d=0; d<4; d++){
-        int nx = x + sdirs[d][0], ny = y + sdirs[d][1];
-        if(!InRange(nx, ny)) continue;
-        if(walls[x][y] > 0 && walls[nx][ny] == walls[x][y]) continue;
-        if(cool[x][y] > cool[nx][ny]){
-            //cout << x << ' ' << y << ' ' <<"에서 " << nx << ' ' << ny << "로 감" << ' ' << abs(cool[x][y] - cool[nx][ny]) / 4<<'\n';
-            tempCool[nx][ny] += abs(cool[x][y] - cool[nx][ny]) / 4;
-            tempCool[x][y] -= abs(cool[x][y] - cool[nx][ny]) / 4;
-        }
-    }
+// 시원함이 mix됩니다.
+void Mix() {
+    // temp 배열을 초기화 해줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            temp[i][j] = 0;
 
+    // 각 칸마다 시원함이 mix된 이후의 결과를 받아옵니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) 
+            temp[i][j] = GetMixedCoolness(i, j);
+    
+    // temp 값을 coolness 배열에 다시 옮겨줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            coolness[i][j] = temp[i][j];
 }
 
-void Simulate(){    // 시뮬레이션을 진행하는 함수
-    // Step 1. 에어컨의 바람이 나와서 공기를 시원하게 한다.
-    for(int i=0; i<(int)aircond.size(); i++){
-        int x, y, s;
-        x = aircond[i].first, y = aircond[i].second;
-        s = grid[x][y];     // 에어컨 방향 번호, 2: 왼, 3: 위, 4:오, 5: 아
-        BlowWind(x, y, s-2);
-
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-                cool[i][j] += step[i][j];
-    }
-
-    // Step 2. 시원한 공기들이 섞이기 시작한다.
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            tempCool[i][j] = cool[i][j];
-        }
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            MixAir(i, j);
-        }
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cool[i][j] = tempCool[i][j];
-            //cout << cool[i][j] << ' ';
-        }
-        //cout << '\n';
-    }
-    
-    // Step 3. 외벽에 있는 칸에 대해서 시원함이 1씩 감소한다.
-    for(int j=0; j<n; j++){
-        cool[0][j] = max(cool[0][j]-1, 0);
-        cool[n-1][j] = max(cool[n-1][j]-1, 0);
-    }
-    for(int i=1; i<=n-2; i++){
-        cool[i][0] = max(cool[i][0]-1, 0);
-        cool[i][n-1] = max(cool[i][n-1]-1, 0);
-    }
-
+// 외벽에 해당하는 칸인지를 판단합니다.
+bool IsOuterWall(int x, int y) {
+    return x == 0 || x == n - 1 || y == 0 || y == n - 1;
 }
 
-int main(){
-    // 입력 받기:
-    Input();
-    
-    // 시뮬레이션 진행하기
-    int elapsed_time = 0;
-    while(true){
-        if(IsFinish() || elapsed_time > 100)
-            break;
-        elapsed_time++;
-        Simulate();
-    }
-    
-    // 모든 사무실의 시원함 정도가 k 이상이 되는 최초의 시간 출력
+// 외벽과 인접한 칸 중
+// 시원함이 있는 곳에 대해서만
+// 1만큼 시원함을 하락시킵니다.
+void Drop() {
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(IsOuterWall(i, j) && coolness[i][j] > 0)
+                coolness[i][j]--;
+}
+
+// 시원함이 생기는 과정을 반복합니다.
+void Simulate() {
+    // Step1. 에어컨에서 시원함을 발산합니다.
+    Blow();
+
+    // Step2. 시원함이 mix됩니다.
+    Mix();
+
+    // Step3. 외벽과 인접한 칸의 시원함이 떨어집니다.
+    Drop();
+
+    // 시간이 1분씩 증가합니다.
+    elapsed_time++;
+}
+
+// 종료해야 할 순간인지를 판단합니다.
+// 모든 사무실의 시원함의 정도가 k 이상이거나,
+// 흐른 시간이 100분을 넘게 되는지를 살펴봅니다.
+bool End() {
+    // 100분이 넘게 되면 종료해야 합니다.
     if(elapsed_time > 100)
-        elapsed_time = -1;
-    cout << elapsed_time;       
+        return true;
+    
+    // 사무실 중에
+    // 시원함의 정도가 k 미만인 곳이
+    // 단 하나라도 있으면, 아직 끝내면 안됩니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(grid[i][j] == OFFICE && 
+               coolness[i][j] < k)
+               return false;
+
+    // 모두 k 이상이므로, 종료해야 합니다.
+    return true;
+}
+
+int main() {
+    // 입력:
+    cin >> n >> m >> k;
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+
+    for(int i = 0; i < m; i++) {
+        int bx, by, bdir;
+        cin >> bx >> by >> bdir;
+        bx--; by--;
+        // 현재 위치 (bx, by)에서
+        // bdir 방향으로 나아가려고 했을 때
+        // 벽이 있음을 표시해줍니다.
+        block[bx][by][bdir] = true;
+
+        int nx = bx + dx[bdir];
+        int ny = by + dy[bdir];
+        // 격자를 벗어나지 않는 칸과 벽을 사이에 두고 있다면,
+        // 해당 칸에서 반대 방향(3-bdir)으로 진입하려고 할 때도
+        // 벽이 있음을 표시해줍니다.
+        if(InRange(nx, ny))
+            block[nx][ny][3 - bdir] = true;
+    }
+
+    // 종료조건이 만족되기 전까지
+    // 계속 시뮬레이션을 반복합니다.
+    while(!End())
+        Simulate();
+
+    // 출력:
+    if(elapsed_time <= 100)
+        cout << elapsed_time;
+    else
+        cout << -1;
     return 0;
 }
